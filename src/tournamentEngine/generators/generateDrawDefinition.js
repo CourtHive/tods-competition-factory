206ed6b7efc7a8ab5090ccb214c627421fabc44e
Charles Allen <charles@courtHive.com>
2020-10-19 14:05:58 +0200

    add seedingProfile for generateDrawDefinition helper

--- src/tournamentEngine/generators/generateDrawDefinition.js
+++ src/tournamentEngine/generators/generateDrawDefinition.js
@@ -19,6 +19,7 @@ export function generateDrawDefinition(props) {
   const {
     groupSize,
     customName,
+    seedingProfile,
     automated = true,
     qualifyingRound,
     policyDefinitions,
@@ -83,6 +84,7 @@ export function generateDrawDefinition(props) {
   drawEngine.generateDrawType({
     stage,
     drawType,
+    seedingProfile,
     structureOptions,
     qualifyingRound,
     qualifyingPositions,
