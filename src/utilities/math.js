d08f34464641a58cd3a5c606c654264327b3cb6b
Charles Allen <charles@courtHive.com>
2020-10-26 07:17:52 +0100

    add randomInt generator

--- src/utilities/math.js
+++ src/utilities/math.js
@@ -18,3 +18,9 @@ export function nextPowerOf2(n) {
   }
   return n;
 }
+
+export function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
