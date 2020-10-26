ea9f62a5b94f1daaba0f3944acffb451405aa4c2
Charles Allen <charles@courtHive.com>
2020-10-26 10:45:59 +0100

    add tests

---
+++ src/drawEngine/tests/primitives/generateMatchUpOutcome.test.js
@@ -0,0 +1,41 @@
+import { generateMatchUpOutcome } from '../primitives/generateMatchUpOutcome';
+
+it('can reliably generate matchUp outcomes', () => {
+  let setValues = [
+    [0, 6],
+    [6, 0],
+  ];
+  let outcome = generateMatchUpOutcome({ setValues });
+  expect(outcome.winningSide).toEqual(undefined);
+
+  setValues = [
+    [6, 0],
+    [6, 0],
+  ];
+  outcome = generateMatchUpOutcome({ setValues });
+  expect(outcome.winningSide).toEqual(1);
+
+  setValues = [
+    [0, 6],
+    [6, 6],
+  ];
+  outcome = generateMatchUpOutcome({ setValues });
+  expect(outcome.winningSide).toEqual(undefined);
+
+  setValues = [
+    [0, 6],
+    [6, 7, 3, 7],
+  ];
+  outcome = generateMatchUpOutcome({ setValues });
+  expect(outcome.winningSide).toEqual(2);
+  expect(outcome.score).toEqual('0-6 6-7(3)');
+
+  setValues = [
+    [0, 6],
+    [6, 0],
+    [6, 7, 3, 7],
+  ];
+  outcome = generateMatchUpOutcome({ setValues });
+  expect(outcome.winningSide).toEqual(2);
+  expect(outcome.score).toEqual('0-6 6-0 6-7(3)');
+});
