fb834ae963f753fa4731b86ef4f68748a5e8e8c3
Charles Allen <charles@courtHive.com>
2020-11-01 16:00:07 +0100

    test with two avoidances (pair, nationalityCode)

--- src/tournamentEngine/tests/integration/pairAvoidance.test.js
+++ src/tournamentEngine/tests/integration/pairAvoidance.test.js
@@ -9,7 +9,7 @@ const pairAvoidancePolicy = {
   roundsToSeparate: undefined,
   policyName: 'Doubles Partner Avoidance',
   policyAttributes: [{ directive: 'pairParticipants' }],
-  candidatesCount: 1, // force generation of only one candidate for logging during testing
+  // candidatesCount: 1, // force generation of only one candidate for logging during testing
 };
 
 it('can generate ELIMINATION drawDefinition using pair avoidance with Doubles participants', () => {
@@ -33,7 +33,7 @@ it('can generate ELIMINATION drawDefinition using pair avoidance with Doubles pa
   if (conflicts?.unseededConflicts) console.log(conflicts);
 });
 
-it.only('can generate ELIMINATION drawDefinition using pair avoidance with Doubles participants and seeding', () => {
+it('can generate ELIMINATION drawDefinition using pair avoidance with Doubles participants and seeding', () => {
   const result = avoidanceTest({
     seedsCount: 4,
     eventType: SINGLES,
@@ -53,5 +53,37 @@ it.only('can generate ELIMINATION drawDefinition using pair avoidance with Doubl
     });
   });
   console.log(result.report);
+  if (conflicts?.unseededConflicts) console.log(conflicts);
+});
+
+it('can generate ELIMINATION drawDefinition using pair and nationality avoidance with Doubles participants and seeding', () => {
+  const pairAvoidancePolicy = {
+    roundsToSeparate: undefined,
+    policyName: 'Doubles Partner Avoidance',
+    policyAttributes: [
+      { key: 'person.nationalityCode' },
+      { key: 'individualParticipants.person.nationalityCode' },
+      { directive: 'pairParticipants' },
+    ],
+  };
+
+  const result = avoidanceTest({
+    seedsCount: 4,
+    eventType: SINGLES,
+    participantType: PAIR,
+    participantsCount: 16,
+    avoidance: pairAvoidancePolicy,
+  });
+  const { conflicts, report, participants } = result || {};
+  const pairedParticipants = participants
+    .filter(p => p.participantType === PAIR)
+    .map(p => p.individualParticipants.map(ip => ip.name));
+  const pairedOpponents = report.map(r => r.names);
+  pairedParticipants.forEach(pair => {
+    pairedOpponents.forEach(opponents => {
+      const overlap = intersection(pair, opponents);
+      if (overlap.length > 1) console.log({ pair, opponents, overlap });
+    });
+  });
   if (conflicts?.unseededConflicts) console.log(conflicts);
 });
