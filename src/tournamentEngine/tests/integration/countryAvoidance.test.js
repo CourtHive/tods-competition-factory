765df3b1bf638835723135b2cc1c0feb2022283e
Charles Allen <charles@courtHive.com>
2020-11-16 15:50:41 +0100

    add tests with BYEs

--- src/tournamentEngine/tests/integration/countryAvoidance.test.js
+++ src/tournamentEngine/tests/integration/countryAvoidance.test.js
@@ -57,6 +57,57 @@ it('can generate ELIMINATION drawDefinition using country avoidance with INDIVID
   if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
 });
 
+it('can generate ELIMINATION drawDefinition using country avoidance with INDIVIDUAL participants and BYEs', () => {
+  let { conflicts, error } = avoidanceTest({
+    eventType: SINGLES,
+    participantsCount: 17,
+    participantType: INDIVIDUAL,
+    avoidance: avoidancePolicy,
+  });
+  if (error) console.log({ error });
+  if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
+
+  ({ conflicts, error } = avoidanceTest({
+    eventType: SINGLES,
+    participantsCount: 17,
+    participantType: INDIVIDUAL,
+    avoidance: avoidancePolicy,
+    valuesCount: 4,
+  }));
+  if (error) console.log({ error });
+  if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
+
+  ({ conflicts, error } = avoidanceTest({
+    eventType: SINGLES,
+    participantsCount: 23,
+    participantType: INDIVIDUAL,
+    avoidance: avoidancePolicy,
+    valuesCount: 4,
+  }));
+  if (error) console.log({ error });
+  if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
+
+  ({ conflicts, error } = avoidanceTest({
+    eventType: SINGLES,
+    participantsCount: 27,
+    participantType: INDIVIDUAL,
+    avoidance: avoidancePolicy,
+    valuesCount: 4,
+  }));
+  if (error) console.log({ error });
+  if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
+
+  ({ conflicts, error } = avoidanceTest({
+    eventType: SINGLES,
+    participantsCount: 17,
+    participantType: INDIVIDUAL,
+    avoidance: avoidancePolicy,
+    valuesCount: 4,
+  }));
+  if (error) console.log({ error });
+  if (conflicts?.unseededConflicts) console.log(conflicts.unseededConflicts);
+});
+
 it('can generate ROUND_ROBIN drawDefinition using country avoidance with INDIVIDUAL participants', () => {
   const { conflicts, error } = avoidanceTest({
     eventType: SINGLES,
