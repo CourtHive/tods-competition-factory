5f8af9a1a29b501594fc2f66e1663e63dcee2a85
Charles Allen <charles@courtHive.com>
2020-12-09 07:43:36 +0100

    error avoidance

--- src/tournamentEngine/getters/structureGetter.js
+++ src/tournamentEngine/getters/structureGetter.js
@@ -1,15 +1,16 @@
 import { PLAY_OFF } from '../../constants/drawDefinitionConstants';
+import { MISSING_DRAW_DEFINITION } from '../../constants/errorConditionConstants';
 
 export function getPlayoffStructures({ drawDefinition, structureId }) {
-  const targetStructureIds = drawDefinition.links
+  const targetStructureIds = (drawDefinition?.links || [])
     .filter(link => link.source?.structureId === structureId)
     .map(link => link.target?.structureId);
 
-  const playoffStructures = drawDefinition.structures?.filter(
+  const playoffStructures = drawDefinition?.structures?.filter(
     structure =>
       targetStructureIds.includes(structure.structureId) &&
       structure.stage === PLAY_OFF
   );
 
-  return playoffStructures;
+  return playoffStructures || [];
 }
