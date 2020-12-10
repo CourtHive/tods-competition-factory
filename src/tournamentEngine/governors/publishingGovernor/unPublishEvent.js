b2016782da2e061a104fedef7a0759bb0648ca44
Charles Allen <charles@courtHive.com>
2020-12-10 14:05:41 +0100

    add unpublish timeItem

--- src/tournamentEngine/governors/publishingGovernor/unPublishEvent.js
+++ src/tournamentEngine/governors/publishingGovernor/unPublishEvent.js
@@ -1,13 +1,22 @@
+import { getEventData } from './getEventData';
+
 import {
   MISSING_EVENT,
   MISSING_TOURNAMENT_RECORD,
 } from '../../../constants/errorConditionConstants';
 import { SUCCESS } from '../../../constants/resultConstants';
-import { getEventData } from './getEventData';
+import { HIDDEN } from '../../../constants/timeItemConstants';
 
 export function unPublishEvent({ tournamentRecord, event }) {
   if (!tournamentRecord) return { error: MISSING_TOURNAMENT_RECORD };
   if (!event) return { error: MISSING_EVENT };
+
+  const timeItem = {
+    itemSubject: PUBLISH,
+    itemValue: HIDDEN,
+  };
+  const result = addEventTimeItem({ event, timeItem });
+  if (result.error) return { error };
 
   return Object.assign({}, SUCCESS);
 }
