8c41604418ba373b65ecc03cc135d10abcd3191f
CourtHive <charles@courthive.com>
2020-12-14 10:49:05 +0100

    optimize

--- src/tournamentEngine/generators/regenerateDrawDefinition.js
+++ src/tournamentEngine/generators/regenerateDrawDefinition.js
@@ -37,10 +37,6 @@ export function regenerateDrawDefinition({
     // TODO: write test to insure that appliedPolicies are copied faithfully
 
     if (newDrawDefinition) {
-      event.drawDefinitions = event.drawDefinitions.map((dd) => {
-        return dd.drawId === drawId ? newDrawDefinition : dd;
-      });
-
       Object.assign(auditData, {
         action: 'Regenerate Draw',
         rejectedDrawDefinition: drawDefinition,
--- src/tournamentEngine/governors/eventGovernor/addDrawEntries.js
+++ src/tournamentEngine/governors/eventGovernor/addDrawEntries.js
@@ -1,4 +1,6 @@
+import { addDrawEntries as addEntries } from '../../../drawEngine/governors/entryGovernor/addingDrawEntries';
 import { getDrawDefinition } from '../../getters/eventGetter';
+
 import {
   DRAW_DEFINITION_NOT_FOUND,
   EVENT_NOT_FOUND,
@@ -7,7 +9,6 @@ import { SUCCESS } from '../../../constants/resultConstants';
 
 export function addDrawEntries({
   tournamentRecord,
-  drawEngine,
 
   participantIds,
   entryStatus,
@@ -21,17 +22,5 @@ export function addDrawEntries({
   if (!event) return { error: EVENT_NOT_FOUND };
   if (!drawDefinition) return { error: DRAW_DEFINITION_NOT_FOUND };
 
-  const result = drawEngine
-    .setState(drawDefinition)
-    .addDrawEntries({ participantIds, stage: entryStage, entryStatus });
-  if (result.error) return result;
-
-  const { drawDefinition: updatedDrawDefinition } = drawEngine.getState();
-  event.drawDefinitions = event.drawDefinitions.map((drawDefinition) => {
-    return drawDefinition.drawId === drawId
-      ? updatedDrawDefinition
-      : drawDefinition;
-  });
-
-  return SUCCESS;
+  return addEntries({ drawDefinition, participantIds, entryStatus, stage });
 }
