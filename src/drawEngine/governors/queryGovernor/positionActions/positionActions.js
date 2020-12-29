84658774980f879cd18e84725b0a8dd019562301
CourtHive <charles@courthive.com>
2020-12-29 06:59:47 +0100

    reorganize

--- src/drawEngine/governors/queryGovernor/positionActions/positionActions.js
+++ src/drawEngine/governors/queryGovernor/positionActions/positionActions.js
@@ -1,5 +1,4 @@
 import { stageEntries } from '../../../getters/stageGetter';
-import { getNextSeedBlock } from '../../../getters/seedGetter';
 import { findStructure } from '../../../getters/findStructure';
 import { structureAssignedDrawPositions } from '../../../getters/positionsGetter';
 import { structureActiveDrawPositions } from '../../../getters/structureActiveDrawPositions';
@@ -166,42 +165,4 @@ export function positionActions({
   }
 
   return { validActions, isDrawPosition: true, isByePosition };
-}
-
-export function getNextUnfilledDrawPositions({ drawDefinition, structureId }) {
-  if (!drawDefinition) {
-    const error = MISSING_DRAW_DEFINITION;
-    return { error, nextUnfilledDrawPositions: [] };
-  }
-  if (!structureId) {
-    const error = MISSING_STRUCTURE_ID;
-    return { error, nextUnfilledDrawPositions: [] };
-  }
-
-  const { structure, error } = findStructure({ drawDefinition, structureId });
-
-  if (error) return { error };
-  if (!structure) return { error: STRUCTURE_NOT_FOUND };
-
-  const result = structureAssignedDrawPositions({ structure });
-  const positionAssignments = result?.positionAssignments || [];
-  const { unfilledPositions } = getNextSeedBlock({
-    drawDefinition,
-    structureId,
-    randomize: true,
-  });
-
-  const unfilledDrawPositions = positionAssignments
-    .filter((assignment) => {
-      return (
-        !assignment.participantId && !assignment.bye && !assignment.qualifier
-      );
-    })
-    .map((assignment) => assignment.drawPosition);
-
-  if (unfilledPositions?.length) {
-    return { nextUnfilledDrawPositions: unfilledPositions };
-  } else {
-    return { nextUnfilledDrawPositions: unfilledDrawPositions };
-  }
 }
