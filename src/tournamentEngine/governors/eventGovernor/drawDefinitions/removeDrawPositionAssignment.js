2dd2e59d6ab8a981735e31c3d85d2ebc7bc9ad78
CourtHive <charles@courthive.com>
2020-12-29 07:01:13 +0100

    extend method to include bye replacement

--- src/tournamentEngine/governors/eventGovernor/drawDefinitions/removeDrawPositionAssignment.js
+++ src/tournamentEngine/governors/eventGovernor/drawDefinitions/removeDrawPositionAssignment.js
@@ -1,4 +1,12 @@
+import {
+  assignDrawPositionBye,
+  getByesData,
+} from '../../../../drawEngine/governors/positionGovernor/positionByes';
 import { clearDrawPosition } from '../../../../drawEngine/governors/positionGovernor/positionClear';
+import { modifyEntriesStatus } from '../entries/modifyEntriesStatus';
+
+import { ALTERNATE } from '../../../../constants/entryStatusConstants';
+import { findStructure } from '../../../../drawEngine/getters/findStructure';
 
 /**
  *
@@ -8,5 +16,41 @@ import { clearDrawPosition } from '../../../../drawEngine/governors/positionGove
  *
  */
 export function removeDrawPositionAssignment(props) {
-  return clearDrawPosition(props);
+  const { replaceWithBye, setAlternateEntryStatus } = props;
+  if (setAlternateEntryStatus) console.log({ setAlternateEntryStatus });
+
+  const result = clearDrawPosition(props);
+  if (result.error) return result;
+
+  const { participantId } = result;
+
+  if (replaceWithBye) {
+    const { drawDefinition, drawPosition, event, structureId } = props;
+    if (participantId) {
+      const { structure } = findStructure({ drawDefinition, structureId });
+      const { byesCount, placedByes } = getByesData({
+        drawDefinition,
+        structure,
+      });
+      const unplacedByes = placedByes < byesCount;
+
+      if (!unplacedByes) {
+        modifyEntriesStatus({
+          participantIds: [participantId],
+          entryStatus: ALTERNATE,
+          drawDefinition,
+          event,
+        });
+      }
+    }
+
+    const result = assignDrawPositionBye({
+      drawDefinition,
+      structureId,
+      drawPosition,
+    });
+    if (result.error) return result;
+  }
+
+  return result;
 }
--- src/tournamentEngine/governors/eventGovernor/entries/modifyEntriesStatus.js
+++ src/tournamentEngine/governors/eventGovernor/entries/modifyEntriesStatus.js
@@ -2,34 +2,43 @@ import {
   INVALID_ENTRY_STATUS,
   INVALID_PARTICIPANT_ID,
   MISSING_EVENT,
-  MISSING_TOURNAMENT_RECORD,
 } from '../../../../constants/errorConditionConstants';
 import { VALID_ENTERED_TYPES } from '../../../../constants/entryStatusConstants';
 import { SUCCESS } from '../../../../constants/resultConstants';
 
 export function modifyEntriesStatus({
-  tournamentRecord,
-  event,
+  drawDefinition,
   participantIds,
   entryStatus,
+  event,
 }) {
-  if (!tournamentRecord) return { error: MISSING_TOURNAMENT_RECORD };
   if (!participantIds || !Array.isArray(participantIds))
     return {
       error: INVALID_PARTICIPANT_ID,
       participantIds,
       method: 'modifyEntriesStatus',
     };
-  if (!event) return { error: MISSING_EVENT };
   if (!VALID_ENTERED_TYPES.includes(entryStatus))
     return { error: INVALID_ENTRY_STATUS };
 
+  if (!drawDefinition && !event) return { error: MISSING_EVENT };
   // TODO: check that entries are not present in any drawDefinitions/structures
-  event.entries.forEach((entry) => {
-    if (participantIds.includes(entry.participantId)) {
-      entry.entryStatus = entryStatus;
-    }
-  });
+
+  if (event) {
+    event.entries.forEach((entry) => {
+      if (participantIds.includes(entry.participantId)) {
+        entry.entryStatus = entryStatus;
+      }
+    });
+  }
+
+  if (drawDefinition) {
+    drawDefinition.entries.forEach((entry) => {
+      if (participantIds.includes(entry.participantId)) {
+        entry.entryStatus = entryStatus;
+      }
+    });
+  }
 
   return SUCCESS;
 }
