6c64dd5be91c26c29665a59f5ffef4b186e8aeb6
CourtHive <charles@courthive.com>
2021-01-05 15:42:27 +0100

    extend tests

--- src/drawEngine/tests/positions/actions/positionActions.test.js
+++ src/drawEngine/tests/positions/actions/positionActions.test.js
@@ -1,14 +1,79 @@
-import drawEngine from '../../..';
 import mocksEngine from '../../../../mocksEngine';
 import tournamentEngine from '../../../../tournamentEngine';
 
 import { INVALID_DRAW_POSITION } from '../../../../constants/errorConditionConstants';
+import { COMPLETED } from '../../../../constants/matchUpStatusConstants';
+import {
+  PENALTY,
+  NICKNAME,
+} from '../../../../constants/matchUpActionConstants';
 
 it('can return accurate position details when requesting positionActions', () => {
   const drawProfiles = [
     {
       drawSize: 32,
       participantsCount: 30,
+      outcomes: [[1, 3, '6-2 6-1', 1]],
+    },
+  ];
+  const {
+    drawIds: [drawId],
+    tournamentRecord,
+  } = mocksEngine.generateTournamentRecord({
+    drawProfiles,
+    inContext: true,
+  });
+
+  tournamentEngine.setState(tournamentRecord);
+
+  const {
+    drawDefinition: { structures },
+  } = tournamentEngine.getEvent({ drawId });
+  const structureId = structures[0].structureId;
+
+  let drawPosition = 1;
+  let result = tournamentEngine.positionActions({
+    drawId,
+    structureId,
+    drawPosition,
+  });
+  expect(result.isActiveDrawPosition).toEqual(false);
+  expect(result.isDrawPosition).toEqual(true);
+  expect(result.isByePosition).toEqual(false);
+
+  drawPosition = 2;
+  result = tournamentEngine.positionActions({
+    drawId,
+    structureId,
+    drawPosition,
+  });
+  expect(result.isActiveDrawPosition).toEqual(false);
+  expect(result.isDrawPosition).toEqual(true);
+  expect(result.isByePosition).toEqual(true);
+
+  drawPosition = 0;
+  result = tournamentEngine.positionActions({
+    drawId,
+    structureId,
+    drawPosition,
+  });
+  expect(result.error).toEqual(INVALID_DRAW_POSITION);
+
+  drawPosition = 40;
+  result = tournamentEngine.positionActions({
+    drawId,
+    structureId,
+    drawPosition,
+  });
+  expect(result.error).toEqual(INVALID_DRAW_POSITION);
+});
+
+it('returns correct positionActions for participants in completed matchUps', () => {
+  const drawProfiles = [
+    {
+      drawSize: 32,
+      participantsCount: 32,
+      outcomes: [[1, 1, '6-2 6-1', 1]],
     },
   ];
   const { drawIds, tournamentRecord } = mocksEngine.generateTournamentRecord({
@@ -24,37 +89,24 @@ it('can return accurate position details when requesting positionActions', () =>
   } = tournamentEngine.getEvent({ drawId });
   const structureId = structures[0].structureId;
 
-  let drawPosition = 1;
-  let result = drawEngine.positionActions({
+  const { matchUps } = tournamentEngine.allDrawMatchUps({ drawId });
+
+  const drawPosition = 1;
+  const targetMatchUp = matchUps.find((matchUp) =>
+    matchUp.drawPositions.includes(drawPosition)
+  );
+  expect(targetMatchUp.matchUpStatus).toEqual(COMPLETED);
+
+  let result = tournamentEngine.positionActions({
     drawId,
     structureId,
     drawPosition,
   });
+  expect(result.isActiveDrawPosition).toEqual(true);
   expect(result.isDrawPosition).toEqual(true);
   expect(result.isByePosition).toEqual(false);
 
-  drawPosition = 2;
-  result = drawEngine.positionActions({
-    drawId,
-    structureId,
-    drawPosition,
-  });
-  expect(result.isDrawPosition).toEqual(true);
-  expect(result.isByePosition).toEqual(true);
-
-  drawPosition = 0;
-  result = drawEngine.positionActions({
-    drawId,
-    structureId,
-    drawPosition,
-  });
-  expect(result.error).toEqual(INVALID_DRAW_POSITION);
-
-  drawPosition = 40;
-  result = drawEngine.positionActions({
-    drawId,
-    structureId,
-    drawPosition,
-  });
-  expect(result.error).toEqual(INVALID_DRAW_POSITION);
+  const options = result.validActions?.map((validAction) => validAction.type);
+  expect(options.includes(PENALTY)).toEqual(true);
+  expect(options.includes(NICKNAME)).toEqual(true);
 });
