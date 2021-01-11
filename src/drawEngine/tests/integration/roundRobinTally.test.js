cf8b9b253c46a7de6b0fb6505dfb50563f04f6a1
CourtHive <charles@courthive.com>
2021-01-11 17:51:18 +0100

    rough-in tally test

--- src/drawEngine/tests/integration/roundRobinTally.test.js
+++ src/drawEngine/tests/integration/roundRobinTally.test.js
@@ -1,3 +1,39 @@
+import { ROUND_ROBIN } from '../../../constants/drawDefinitionConstants';
+import { SINGLES } from '../../../constants/eventConstants';
+import mocksEngine from '../../../mocksEngine';
+import tournamentEngine from '../../../tournamentEngine';
+
 it('properly orders round robin participants', () => {
-  expect('foo');
+  const drawProfiles = [
+    {
+      drawSize: 3,
+      eventType: SINGLES,
+      participantsCount: 3,
+      drawType: ROUND_ROBIN,
+      outcomes: [
+        {
+          roundNumber: 1,
+          structureIndex: 1,
+          scoreString: '6-2 6-2',
+          winningSide: 1,
+        },
+        {
+          roundNumber: 2,
+          structureIndex: 1,
+          scoreString: '6-2 6-2',
+          winningSide: 2,
+        },
+      ],
+    },
+  ];
+  let {
+    tournamentRecord,
+    drawIds: [drawId],
+  } = mocksEngine.generateTournamentRecord({
+    drawProfiles,
+  });
+  tournamentEngine.setState(tournamentRecord);
+
+  let { drawDefinition } = tournamentEngine.getEvent({ drawId });
+  console.log(drawDefinition.structures[0].structures[0].matchUps);
 });
