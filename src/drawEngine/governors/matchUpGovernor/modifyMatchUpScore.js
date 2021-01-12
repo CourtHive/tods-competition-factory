d61a758c24c71667724ebc5b3bd86df9850d40c9
CourtHive <charles@courthive.com>
2021-01-12 13:09:09 +0100

    enable RR tally middleware

--- src/drawEngine/governors/matchUpGovernor/modifyMatchUpScore.js
+++ src/drawEngine/governors/matchUpGovernor/modifyMatchUpScore.js
@@ -6,6 +6,15 @@
  * @param {*} param0
  */
 
+import { CONTAINER } from '../../../constants/drawDefinitionConstants';
+import { addExtension } from '../../../tournamentEngine/governors/tournamentGovernor/addRemoveExtensions';
+import { addTimeItem } from '../../../tournamentEngine/governors/tournamentGovernor/addTimeItem';
+import {
+  findMatchUp,
+  getAllStructureMatchUps,
+} from '../../getters/getMatchUps';
+import { tallyParticipantResults } from '../scoreGovernor/roundRobinTally';
+
 export function modifyMatchUpScore({
   drawDefinition,
   matchUp,
@@ -20,8 +29,45 @@ export function modifyMatchUpScore({
   if (matchUpStatus) matchUp.matchUpStatus = matchUpStatus;
   if (matchUpStatusCodes) matchUp.matchUpStatusCodes = matchUpStatusCodes;
   if (winningSide) matchUp.winningSide = winningSide;
+
+  // middleware methods
   if (drawDefinition) {
-    console.log({ matchUp, score });
-    // middleware methods
+    const { matchUpId } = matchUp;
+    const { structure } = findMatchUp({
+      drawDefinition,
+      matchUpId,
+    });
+
+    if (structure?.structureType === CONTAINER) {
+      matchUpFormat =
+        matchUpFormat ||
+        matchUp.matchUpFormat ||
+        structure?.matchUpFormat ||
+        drawDefinition.matchUpFormat;
+
+      const itemStructure = structure.structures.find((itemStructure) => {
+        return itemStructure?.matchUps.find(
+          (matchUp) => matchUp.matchUpId === matchUpId
+        );
+      });
+      const { matchUps } = getAllStructureMatchUps({
+        structure: itemStructure,
+        inContext: true,
+      });
+      const { participantResults } = tallyParticipantResults({
+        matchUps,
+        matchUpFormat,
+      });
+      Object.keys(participantResults).forEach((participantId) => {
+        const assignment = itemStructure.positionAssignments.find(
+          (assignment) => assignment.participantId === participantId
+        );
+        const extension = {
+          name: 'tally',
+          value: participantResults[participantId],
+        };
+        addExtension({ element: assignment, extension });
+      });
+    }
   }
 }
