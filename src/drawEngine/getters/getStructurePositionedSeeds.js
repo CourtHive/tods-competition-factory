7f353d3a85d4880017ce3c2203825e8a7f2905c2
CourtHive <charles@courthive.com>
2021-01-18 06:22:51 +0100

    breakout function into separate module

---
+++ src/drawEngine/getters/getStructurePositionedSeeds.js
@@ -0,0 +1,27 @@
+import { getStructureSeedAssignments } from './getStructureSeedAssignments';
+import { structureAssignedDrawPositions } from './positionsGetter';
+
+export function getStructurePositionedSeeds({ drawDefinition, structure }) {
+  const { positionAssignments } = structureAssignedDrawPositions({ structure });
+  const { seedAssignments } = getStructureSeedAssignments({
+    drawDefinition,
+    structure,
+  });
+  const seedMap = Object.assign(
+    {},
+    ...seedAssignments
+      .filter((assignment) => assignment.participantId)
+      .map((assignment) => ({ [assignment.participantId]: assignment }))
+  );
+  const positionedSeeds = positionAssignments
+    .map((assignment) => {
+      return !seedMap[assignment.participantId]
+        ? ''
+        : Object.assign(assignment, {
+            seedNumber: seedMap[assignment.participantId].seedNumber,
+            seedValue: seedMap[assignment.participantId].seedValue,
+          });
+    })
+    .filter((f) => f);
+  return positionedSeeds;
+}
