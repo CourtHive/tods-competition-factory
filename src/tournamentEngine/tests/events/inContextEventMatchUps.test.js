773ec33685a45cf4ca75ebe6e34e5a4be5b37fab
CourtHive <charles@courthive.com>
2021-01-18 21:00:28 +0100

    extend tests

--- src/tournamentEngine/tests/events/inContextEventMatchUps.test.js
+++ src/tournamentEngine/tests/events/inContextEventMatchUps.test.js
@@ -1,5 +1,6 @@
 import tournamentEngine from '../..';
 import { generateTournamentRecord } from '../../../mocksEngine/generators/generateTournamentRecord';
+import { FEED_IN_CHAMPIONSHIP } from '../../../constants/drawDefinitionConstants';
 
 it('can return event matchUps with context and upcoming matchUps', () => {
   const drawProfiles = [
@@ -44,6 +45,53 @@ it('can return event matchUps with context and upcoming matchUps', () => {
   checkExpectation({
     matchUps: relevantMatchUps,
     expectation: [1, 3, [2, 2]],
+  });
+});
+
+it('can return event matchUps with context and upcoming matchUps for FEED_IN_CHAMPIONSHIP', () => {
+  const drawProfiles = [
+    {
+      drawSize: 8,
+      drawType: FEED_IN_CHAMPIONSHIP,
+    },
+  ];
+  const { drawIds, tournamentRecord } = generateTournamentRecord({
+    drawProfiles,
+    inContext: true,
+  });
+
+  const drawId = drawIds[0];
+
+  tournamentEngine.setState(tournamentRecord);
+
+  const { matchUps } = tournamentEngine.allDrawMatchUps({
+    drawId,
+    nextMatchUps: true,
+  });
+
+  // convenience function for testing, expectation:
+  // [roundNumber, roundPosition, [winnerToRoundNumber, winnerToRoundPosition], [loserToRoundNumber, loserToRoundPosition]]
+  checkExpectation({ matchUps, expectation: [1, 1, [2, 1]] });
+  checkExpectation({ matchUps, expectation: [1, 3, [2, 2]] });
+
+  const {
+    matchUps: tournamentMatchUps,
+  } = tournamentEngine.allTournamentMatchUps({
+    nextMatchUps: true,
+  });
+
+  const relevantMatchUps = tournamentMatchUps.filter(
+    (matchUp) => matchUp.drawId === drawId
+  );
+  // convenience function for testing, expectation:
+  // [roundNumber, roundPosition, [winnerToRoundNumber, winnerToRoundPosition], [loserToRoundNumber, loserToRoundPosition]]
+  checkExpectation({
+    matchUps: relevantMatchUps,
+    expectation: [1, 1, [2, 1], [1, 1]],
+  });
+  checkExpectation({
+    matchUps: relevantMatchUps,
+    expectation: [1, 3, [2, 2], [1, 2]],
   });
 });
 
