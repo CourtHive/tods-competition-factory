9dfbd623b4c85ae013a827992728bdc74b74d5d6
CourtHive <charles@courthive.com>
2021-01-19 09:38:01 +0100

    cleanup

--- src/drawEngine/governors/positionGovernor/positionClear.js
+++ src/drawEngine/governors/positionGovernor/positionClear.js
@@ -3,14 +3,18 @@ import { getAllDrawMatchUps } from '../../getters/getMatchUps/drawMatchUps';
 import { structureAssignedDrawPositions } from '../../getters/positionsGetter';
 import { structureActiveDrawPositions } from '../../getters/structureActiveDrawPositions';
 import { getAllStructureMatchUps } from '../../getters/getMatchUps/getAllStructureMatchUps';
-import { removeByeAndCleanUp } from './byePositioning/checkByeAndCleanUp';
+import { removeMatchUpDrawPosition } from '../matchUpGovernor/matchUpDrawPosition';
+import { positionTargets } from './positionTargets';
+import { numericSort } from '../../../utilities';
+
+import { MISSING_DRAW_POSITIONS } from '../../../constants/errorConditionConstants';
+import { BYE, TO_BE_PLAYED } from '../../../constants/matchUpStatusConstants';
 
 import {
   DRAW_POSITION_ACTIVE,
   MISSING_DRAW_POSITION,
   DRAW_POSITION_NOT_CLEARED,
 } from '../../../constants/errorConditionConstants';
-import { BYE } from '../../../constants/matchUpStatusConstants';
 import { SUCCESS } from '../../../constants/resultConstants';
 
 /**
@@ -112,4 +116,88 @@ export function clearDrawPosition({
   if (!drawPositionCleared) return { error: DRAW_POSITION_NOT_CLEARED };
 
   return Object.assign({}, SUCCESS, { participantId });
+}
+
+function removeByeAndCleanUp({
+  drawDefinition,
+  matchUp,
+  structure,
+  drawPosition,
+  inContextDrawMatchUps,
+}) {
+  const { matchUpId } = matchUp;
+
+  matchUp.matchUpStatus = TO_BE_PLAYED;
+  matchUp.matchUpStatusCodes = [];
+
+  // if there is a linked draw then BYE must also be placed there
+  // This must be propagated through compass draw, for instance
+  const pairedDrawPosition = matchUp.drawPositions?.reduce(
+    (pairedDrawPosition, currentDrawPosition) => {
+      return currentDrawPosition !== drawPosition
+        ? currentDrawPosition
+        : pairedDrawPosition;
+    },
+    undefined
+  );
+
+  const sourceMatchUpWinnerDrawPositionIndex = matchUp.drawPositions?.indexOf(
+    drawPosition
+  );
+
+  const {
+    targetLinks: { loserTargetLink, winnerTargetLink },
+    targetMatchUps: { loserMatchUp, winnerMatchUp },
+  } = positionTargets({
+    matchUpId,
+    structure,
+    drawDefinition,
+    inContextDrawMatchUps,
+    sourceMatchUpWinnerDrawPositionIndex,
+  });
+
+  // clear Directed Byes
+  if (loserMatchUp && loserMatchUp.matchUpStatus === BYE) {
+    // loserMatchUp must have both drawPositions defined
+    const loserMatchUpDrawPositionsCount = loserMatchUp.drawPositions?.filter(
+      (f) => f
+    ).length;
+    if (loserMatchUpDrawPositionsCount !== 2)
+      return { error: MISSING_DRAW_POSITIONS };
+    // drawPositions must be in numerical order
+    loserMatchUp.drawPositions = (loserMatchUp.drawPositions || []).sort(
+      numericSort
+    );
+    // loser drawPosition in target structure is determined bye even/odd
+    const targetDrawPositionIndex = 1 - (matchUp.roundPosition % 2);
+
+    const structureId = loserTargetLink.target.structureId;
+    const targetDrawPosition =
+      loserMatchUp.drawPositions[targetDrawPositionIndex];
+    clearDrawPosition({
+      drawDefinition,
+      structureId,
+      drawPosition: targetDrawPosition,
+    });
+  }
+
+  if (winnerMatchUp) {
+    if (
+      winnerTargetLink &&
+      matchUp.structureId !== winnerTargetLink.target.structureId
+    ) {
+      // TODO: if the winnerMatchUp structureId is different than the matchUp structureId
+      // => the winner participantId should be removed from the positionAssignments of target structure
+      console.log(
+        '%c ALERT: remove winner participantId from target structure positionAssignments',
+        'color: red'
+      );
+    }
+
+    removeMatchUpDrawPosition({
+      drawDefinition,
+      matchUpId: winnerMatchUp.matchUpId,
+      drawPosition: pairedDrawPosition,
+    });
+  }
 }
