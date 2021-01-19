7454707544f0bb4cf16830649fbbe1bbdef37e82
CourtHive <charles@courthive.com>
2021-01-19 07:10:46 +0100

    refactor

--- src/drawEngine/governors/positionGovernor/positionClear.js
+++ src/drawEngine/governors/positionGovernor/positionClear.js
@@ -1,22 +1,17 @@
-import { numericSort } from '../../../utilities';
 import { findStructure } from '../../getters/findStructure';
-import { setMatchUpStatus } from '../matchUpGovernor/setMatchUpStatus';
 import { getAllDrawMatchUps } from '../../getters/getMatchUps/drawMatchUps';
 import { structureAssignedDrawPositions } from '../../getters/positionsGetter';
-import { positionTargets } from '../../governors/positionGovernor/positionTargets';
 import { structureActiveDrawPositions } from '../../getters/structureActiveDrawPositions';
 import { getAllStructureMatchUps } from '../../getters/getMatchUps/getAllStructureMatchUps';
-import { removeMatchUpDrawPosition } from '../../governors/matchUpGovernor/matchUpDrawPosition';
+import { removeByeAndCleanUp } from './byePositioning/removeByeAndCleanUp';
 
 import {
   DRAW_POSITION_ACTIVE,
   MISSING_DRAW_POSITION,
   DRAW_POSITION_NOT_CLEARED,
-  MISSING_DRAW_POSITIONS,
 } from '../../../constants/errorConditionConstants';
-import { BYE, TO_BE_PLAYED } from '../../../constants/matchUpStatusConstants';
+import { BYE } from '../../../constants/matchUpStatusConstants';
 import { SUCCESS } from '../../../constants/resultConstants';
-// import { attemptToSetMatchUpStatusToBePlayed } from '../matchUpGovernor/attemptToSetMatchUpStatusToBePlayed';
 
 /**
  *
@@ -117,99 +112,4 @@ export function clearDrawPosition({
   if (!drawPositionCleared) return { error: DRAW_POSITION_NOT_CLEARED };
 
   return Object.assign({}, SUCCESS, { participantId });
-}
-
-function removeByeAndCleanUp({
-  drawDefinition,
-  matchUp,
-  structure,
-  drawPosition,
-  inContextDrawMatchUps,
-}) {
-  const { matchUpId } = matchUp;
-  setMatchUpStatus({
-    drawDefinition,
-    matchUpId,
-    matchUpStatus: TO_BE_PLAYED,
-    matchUpStatusCodes: [],
-  });
-
-  /*
-    const result = attemptToSetMatchUpStatusToBePlayed({
-      drawDefinition,
-      matchUp,
-      structure,
-    });
-    */
-
-  // if there is a linked draw then BYE must also be placed there
-  // This must be propagated through compass draw, for instance
-  const pairedDrawPosition = matchUp.drawPositions?.reduce(
-    (pairedDrawPosition, currentDrawPosition) => {
-      return currentDrawPosition !== drawPosition
-        ? currentDrawPosition
-        : pairedDrawPosition;
-    },
-    undefined
-  );
-
-  const sourceMatchUpWinnerDrawPositionIndex = matchUp.drawPositions?.indexOf(
-    drawPosition
-  );
-
-  const {
-    targetLinks: { loserTargetLink, winnerTargetLink },
-    targetMatchUps: { loserMatchUp, winnerMatchUp },
-  } = positionTargets({
-    matchUpId,
-    structure,
-    drawDefinition,
-    inContextDrawMatchUps,
-    sourceMatchUpWinnerDrawPositionIndex,
-  });
-
-  // clear Directed Byes
-  if (loserMatchUp && loserMatchUp.matchUpStatus === BYE) {
-    // loserMatchUp must have both drawPositions defined
-    const loserMatchUpDrawPositionsCount = loserMatchUp.drawPositions?.filter(
-      (f) => f
-    ).length;
-    if (loserMatchUpDrawPositionsCount !== 2)
-      return { error: MISSING_DRAW_POSITIONS };
-    // drawPositions must be in numerical order
-    loserMatchUp.drawPositions = (loserMatchUp.drawPositions || []).sort(
-      numericSort
-    );
-    // loser drawPosition in target structure is determined bye even/odd
-    const targetDrawPositionIndex = 1 - (matchUp.roundPosition % 2);
-
-    const structureId = loserTargetLink.target.structureId;
-    const targetDrawPosition =
-      loserMatchUp.drawPositions[targetDrawPositionIndex];
-    clearDrawPosition({
-      drawDefinition,
-      structureId,
-      drawPosition: targetDrawPosition,
-    });
-  }
-
-  if (winnerMatchUp) {
-    if (
-      winnerTargetLink &&
-      matchUp.structureId !== winnerTargetLink.target.structureId
-    ) {
-      // TODO: if the winnerMatchUp structureId is different than the matchUp structureId
-      // => the winner participantId should be removed from the positionAssignments of target structure
-      console.log(
-        '%c ALERT: remove winner participantId from target structure positionAssignments',
-        'color: red'
-      );
-    }
-
-    removeMatchUpDrawPosition({
-      drawDefinition,
-      matchUpId: winnerMatchUp.matchUpId,
-      drawPosition: pairedDrawPosition,
-    });
-  }
 }
