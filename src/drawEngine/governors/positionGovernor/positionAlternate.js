24b15ec7a0a6b32d35ae4410d3fb971003f4489a
CourtHive <charles@courthive.com>
2021-01-21 12:49:54 +0100

    swap assigned participantId with alternateId without clearPosition

--- src/drawEngine/governors/positionGovernor/positionAlternate.js
+++ src/drawEngine/governors/positionGovernor/positionAlternate.js
@@ -1,6 +1,8 @@
-import { SUCCESS } from '../../../constants/resultConstants';
 import { assignDrawPosition } from './positionAssignment';
 import { clearDrawPosition } from './positionClear';
+
+import { SUCCESS } from '../../../constants/resultConstants';
+import { getPositionAssignments } from '../../getters/positionsGetter';
 
 export function alternateDrawPositionAssignment({
   alternateParticipantId,
@@ -8,6 +10,29 @@ export function alternateDrawPositionAssignment({
   drawPosition,
   structureId,
 }) {
+  const { positionAssignments } = getPositionAssignments({
+    drawDefinition,
+    structureId,
+  });
+  const positionAssignment = positionAssignments.find(
+    (assignment) => assignment.drawPosition === drawPosition
+  );
+  if (positionAssignment.participantId) {
+    let result = assignDrawPosition({
+      drawDefinition,
+      structureId,
+      drawPosition,
+      participantId: alternateParticipantId,
+    });
+    if (!result.success) {
+      console.log({ result });
+    }
+    return Object.assign({}, SUCCESS, {
+      removedParticipantId: positionAssignment.participantId,
+    });
+  } else {
+    console.log('deal with bye replacement');
+  }
   let result = clearDrawPosition({
     drawDefinition,
     drawPosition,
