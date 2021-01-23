129d3b99bcca1e1f306f7c27b129fd8ea765ad8a
CourtHive <charles@courthive.com>
2021-01-23 16:41:45 +0100

    handle outcome change when participants directed by win ratio

--- src/drawEngine/governors/matchUpGovernor/attemptToSetWinningSide.js
+++ src/drawEngine/governors/matchUpGovernor/attemptToSetWinningSide.js
@@ -1,14 +1,36 @@
 import { removeDirectedParticipants } from './removeDirectedParticipants';
+import { isCompletedStructure } from '../queryGovernor/structureActions';
 import { isDirectingMatchUpStatus } from './checkStatusType';
 import { directParticipants } from './directParticipants';
+import { getAffectedTarget } from './getAffectedTarget';
 
 import { COMPLETED } from '../../../constants/matchUpStatusConstants';
+import { WIN_RATIO } from '../../../constants/drawDefinitionConstants';
 
 export function attemptToSetWinningSide(props) {
-  const { matchUp, matchUpStatus, matchUpStatusCodes, winningSide } = props;
+  const {
+    drawDefinition,
+    matchUpStatus,
+    matchUpStatusCodes,
+    winningSide,
+    structure,
+    matchUp,
+  } = props;
   let errors = [];
 
   if (matchUp.winningSide && matchUp.winningSide !== winningSide) {
+    // check whether player movement is dependent on win ratio
+    if (structure.finishingPosition === WIN_RATIO) {
+      const structureIsComplete = isCompletedStructure({
+        drawDefinition,
+        structure,
+      });
+      if (structureIsComplete) {
+        // if structure is complete then a changed outcome will have downstream effects
+        getAffectedTarget({ drawDefinition, structure, matchUp });
+      }
+    }
+
     const { errors: participantDirectionErrors } = removeDirectedParticipants(
       props
     );
