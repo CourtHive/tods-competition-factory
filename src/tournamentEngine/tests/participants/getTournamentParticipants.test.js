fde6e4d7348c912b198480df5a723220ae6d992d
CourtHive <charles@courthive.com>
2021-01-27 06:13:48 +0100

    update test to reflect participants calc modification

--- src/tournamentEngine/tests/participants/getTournamentParticipants.test.js
+++ src/tournamentEngine/tests/participants/getTournamentParticipants.test.js
@@ -15,28 +15,28 @@ it('can retrieve tournament participants', () => {
   tournamentEngine.setState(tournamentRecord);
 
   let { tournamentParticipants } = tournamentEngine.getTournamentParticipants();
-  expect(tournamentParticipants.length).toEqual(300);
+  expect(tournamentParticipants.length).toEqual(150);
 
   ({ tournamentParticipants } = tournamentEngine.getTournamentParticipants({
     participantFilters: { participantTypes: [INDIVIDUAL] },
   }));
-  expect(tournamentParticipants.length).toEqual(200);
+  expect(tournamentParticipants.length).toEqual(100);
 
   ({ tournamentParticipants } = tournamentEngine.getTournamentParticipants({
     participantFilters: { participantTypes: [PAIR] },
   }));
-  expect(tournamentParticipants.length).toEqual(100);
+  expect(tournamentParticipants.length).toEqual(50);
   expect(tournamentParticipants[0].individualParticipants).toBeUndefined();
 
   ({ tournamentParticipants } = tournamentEngine.getTournamentParticipants({
     participantFilters: { participantTypes: [PAIR] },
     inContext: true,
   }));
-  expect(tournamentParticipants.length).toEqual(100);
+  expect(tournamentParticipants.length).toEqual(50);
   expect(tournamentParticipants[0].individualParticipants.length).toEqual(2);
 
   ({ tournamentParticipants } = tournamentEngine.getTournamentParticipants({
     participantFilters: { participantRoles: [COMPETITOR] },
   }));
-  expect(tournamentParticipants.length).toEqual(300);
+  expect(tournamentParticipants.length).toEqual(150);
 });
