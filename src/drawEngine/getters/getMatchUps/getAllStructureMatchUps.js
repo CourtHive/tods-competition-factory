74fc2a028f269d5fb045c356656ef6c5ff591d8d
CourtHive <charles@courthive.com>
2021-02-02 06:42:00 +0100

    add displayOrder; displaySideNumber;

--- src/drawEngine/getters/getMatchUps/getAllStructureMatchUps.js
+++ src/drawEngine/getters/getMatchUps/getAllStructureMatchUps.js
@@ -53,15 +53,16 @@ export function getAllStructureMatchUps({
 
   const thisEvent =
     !context?.eventId ||
-    (!matchUpFilters?.eventIds?.length && !contextFilters?.eventIds?.length) ||
+    (!matchUpFilters?.eventIds?.filter((f) => f).length &&
+      !contextFilters?.eventIds?.filter((f) => f).length) ||
     matchUpFilters?.eventIds?.includes(context.eventId) ||
     contextFilters?.eventIds?.includes(context.eventId);
   const thisStructure =
-    !matchUpFilters?.structureIds?.length ||
+    !matchUpFilters?.structureIds?.filter((f) => f).length ||
     matchUpFilters.structureIds.includes(structure.structureId);
   const thisDraw =
     !drawDefinition ||
-    !matchUpFilters?.drawIds?.length ||
+    !matchUpFilters?.drawIds?.filter((f) => f).length ||
     matchUpFilters.drawIds.includes(drawDefinition.drawId);
 
   // don't process this structure if filters and filters don't include eventId, drawId or structureId
@@ -253,7 +254,7 @@ export function getAllStructureMatchUps({
     }
 
     if (Array.isArray(drawPositions)) {
-      const orderedDrawPositions = getOrderedDrawPositions({
+      const { orderedDrawPositions, displayOrder } = getOrderedDrawPositions({
         drawPositions,
         roundProfile,
         roundPosition,
@@ -263,6 +264,7 @@ export function getAllStructureMatchUps({
       const sides = orderedDrawPositions.map((drawPosition, index) => {
         const sideNumber = index + 1;
         const side = getSide({
+          displaySideNumber: displayOrder[index],
           seedAssignments,
           positionAssignments,
           drawPosition,
