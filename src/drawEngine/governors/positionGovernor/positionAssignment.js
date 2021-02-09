c5b6409f83dbf34fcc4f7f4c28d19b4831fd066d
CourtHive <charles@courthive.com>
2021-02-09 17:02:38 +0100

    cleanup

--- src/drawEngine/governors/positionGovernor/positionAssignment.js
+++ src/drawEngine/governors/positionGovernor/positionAssignment.js
@@ -164,17 +164,6 @@ function addDrawPositionToMatchUps({
     matchUp.drawPositions.includes(drawPosition)
   );
 
-  /*
-  const positionAssignments = structure.positionAssignments;
-  const pairedDrawPoaition = matchUp.drawPositions.find(
-    (currentDrawPosition) => currentDrawPosition !== drawPosition
-  );
-  const pairedDrawPositionAssignment = positionAssignments.find(
-    (assignment) => assignment.drawPosition === pairedDrawPoaition
-  );
-  const pairedDrawPositionIsBye = pairedDrawPositionAssignment?.bye;
-  */
-
   if (matchUp) {
     const result = assignMatchUpDrawPosition({
       drawDefinition,
