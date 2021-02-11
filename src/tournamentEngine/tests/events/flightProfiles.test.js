f0eebfe966aa99e3f5e381af81eaa0341251441e
CourtHive <charles@courthive.com>
2021-02-11 16:47:04 +0100

    update flight attributes

--- src/tournamentEngine/tests/events/flightProfiles.test.js
+++ src/tournamentEngine/tests/events/flightProfiles.test.js
@@ -73,3 +73,46 @@ it('can create and return flighProfiles', () => {
   ]);
   expect(flightProfile.flights.every(({ drawId }) => drawId));
 });
+
+it.only('can create and return flighProfiles with drawDefinitions', () => {
+  mocksEngine.generateTournamentRecord({});
+  const eventName = 'Test Event';
+  const event = { eventName };
+  let result = tournamentEngine.addEvent({ event });
+  let { event: eventResult } = result;
+  const { eventId } = eventResult;
+  expect(result.success).toEqual(true);
+
+  let { flightProfile } = tournamentEngine.getFlightProfile({ eventId });
+  expect(flightProfile).toBeUndefined();
+
+  const { tournamentParticipants } = tournamentEngine.getTournamentParticipants(
+    {
+      participantFilters: { participantTypes: [INDIVIDUAL] },
+    }
+  );
+  const participantIds = tournamentParticipants.map((p) => p.participantId);
+  result = tournamentEngine.addEventEntries({ eventId, participantIds });
+  expect(result.success).toEqual(true);
+
+  ({ flightProfile } = tournamentEngine.generateFlightProfile({
+    eventId,
+    flightsCount: 3,
+  }));
+
+  flightProfile.flights?.forEach((flight) => {
+    const { drawDefinition } = tournamentEngine.generateDrawDefinition({
+      eventId,
+      drawId: flight.drawId,
+      drawEntries: flight.drawEntries,
+    });
+    result = tournamentEngine.addDrawDefinition({ eventId, drawDefinition });
+    expect(result.success).toEqual(true);
+  });
+
+  ({ flightProfile } = tournamentEngine.getFlightProfile({ eventId }));
+  expect(
+    flightProfile.flights.every(({ drawDefinition }) => drawDefinition)
+  ).toEqual(true);
+  console.log(flightProfile.flights);
+});
