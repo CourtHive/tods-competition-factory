34b8f505e8d87c7634b7b021a051027995b24698
CourtHive <charles@courthive.com>
2021-02-11 09:37:25 +0100

    normalize policy exports

--- src/fixtures/policies/POLICY_SCORING_DEFAULT.js
+++ src/fixtures/policies/POLICY_SCORING_DEFAULT.js
@@ -5,7 +5,7 @@ import { POLICY_TYPE_SCORING } from '../../constants/policyConstants';
  * default SCORING_POLICY requires all stage:MAIN, stageSequence:1 drawPositions to be assigned **BEFORE** scoring is enabled,
  * while allowing scoring in consolation and compass/playoff structures when not all drawPositions have been filled
  */
-export const SCORING_POLICY = {
+export const POLICY_SCORING_DEFAULT = {
   [POLICY_TYPE_SCORING]: {
     defaultMatchUpFormat: 'SET3-S:6/TB7',
     requireAllPositionsAssigned: false,
@@ -21,4 +21,4 @@ export const SCORING_POLICY = {
   },
 };
 
-export default SCORING_POLICY;
+export default POLICY_SCORING_DEFAULT;
