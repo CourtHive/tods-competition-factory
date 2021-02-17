f8a97b474d7c8a586b13369b5b263911698544fe
CourtHive <charles@courthive.com>
2021-02-17 07:04:22 +0100

    additional tests

--- src/drawEngine/tests/positions/actions/positionActionsFIC.test.js
+++ src/drawEngine/tests/positions/actions/positionActionsFIC.test.js
@@ -4,6 +4,14 @@ import { FEED_IN_CHAMPIONSHIP } from '../../../../constants/drawDefinitionConsta
 test.each([
   [8, FEED_IN_CHAMPIONSHIP, [2, 4, 5, 6]],
   [8, FEED_IN_CHAMPIONSHIP, [7, 5, 6, 8]],
+  [8, FEED_IN_CHAMPIONSHIP, [1, 2, 4, 3]],
+  [8, FEED_IN_CHAMPIONSHIP, [7, 3, 1, 8]],
+  [8, FEED_IN_CHAMPIONSHIP, [1, 7, 8, 5]],
+  [8, FEED_IN_CHAMPIONSHIP, [3, 2, 5, 6]],
+  [8, FEED_IN_CHAMPIONSHIP, [4, 6, 5, 3]],
+  [8, FEED_IN_CHAMPIONSHIP, [3, 2, 7, 8]],
+  [16, FEED_IN_CHAMPIONSHIP, [15, 7, 1, 10, 11, 13, 16, 8]],
+  [16, FEED_IN_CHAMPIONSHIP, [6, 15, 4, 1, 3, 12, 5, 9]],
 ])(
   'pass specific bye replaceent scenarios',
   (drawSize, drawType, positionsToReplaceWithBye) => {
