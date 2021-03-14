ed15fd998ceb7761224291a727ea739ef1fc9344
CourtHive <charles@courthive.com>
2021-03-14 10:51:56 +0100

    restrict lucky losers by source round in certain conditions

--- src/drawEngine/governors/queryGovernor/positionActions/getValidLuckyLoserAction.js
+++ src/drawEngine/governors/queryGovernor/positionActions/getValidLuckyLoserAction.js
@@ -1,10 +1,14 @@
-import { getDrawMatchUps } from '../../../getters/getMatchUps/drawMatchUps';
+import { getAllStructureMatchUps } from '../../../getters/getMatchUps/getAllStructureMatchUps';
+import { getStructureMatchUps } from '../../../getters/getMatchUps/getStructureMatchUps';
+import { getInitialRoundNumber } from '../../../getters/getInitialRoundNumber';
+import { findStructure } from '../../../getters/findStructure';
 
 import { MISSING_DRAW_ID } from '../../../../constants/errorConditionConstants';
 import {
   LUCKY_PARTICIPANT,
   LUCKY_PARTICIPANT_METHOD,
 } from '../../../../constants/positionActionConstants';
+import { ROUND_OUTCOME } from '../../../../constants/drawDefinitionConstants';
 
 export function getValidLuckyLosersAction({
   drawPosition,
@@ -25,20 +29,64 @@ export function getValidLuckyLosersAction({
   /*
   Available Lucky Losers are those participants who are assigned drawPositions
   in source draw structures and have already lost
+
+  If links are by ROUND_OUTCOME, and...
+  If there is only one source structure and only one target structure, then no round restrictions;
+  otherwise restrict the aviailable lucky losers by the source round in the source structure
   */
 
-  const relevantLink = drawDefinition.links?.find(
+  const { sourceStructureIds, targetStructureIds } =
+    drawDefinition.links?.reduce(
+      (ids, link) => {
+        const sourceStructureId = link.source.structureId;
+        const targetStructureId = link.target.structureId;
+        if (!ids.sourceStructureIds.includes(sourceStructureId))
+          ids.sourceStructureIds.push(sourceStructureId);
+        if (!ids.targetStructureIds.includes(targetStructureId))
+          ids.targetStructureIds.push(targetStructureId);
+        return ids;
+      },
+      { sourceStructureIds: [], targetStructureIds: [] }
+    ) || {};
+
+  let relevantLink = drawDefinition.links?.find(
     (link) => link.target?.structureId === structure.structureId
   );
-  const sourceStructureIds = [relevantLink?.source?.structureId].filter(
-    (f) => f
-  );
+  const sourceStructureId = relevantLink?.source?.structureId;
 
-  const contextFilters = { structureIds: sourceStructureIds };
-  let { completedMatchUps } = getDrawMatchUps({
+  const { structure: sourceStructure } = findStructure({
     drawDefinition,
-    contextFilters,
+    structureId: sourceStructureId,
+  });
+
+  const restrictBySourceRound =
+    sourceStructure?.finishingPosition === ROUND_OUTCOME &&
+    (sourceStructureIds?.length !== 1 || targetStructureIds?.length !== 1);
+
+  const matchUpFilters = {};
+  if (restrictBySourceRound) {
+    const { matchUps } = getAllStructureMatchUps({
+      structure,
+      drawDefinition,
+    });
+    const { initialRoundNumber } = getInitialRoundNumber({
+      matchUps,
+      drawPosition,
+    });
+    relevantLink = drawDefinition.links?.find(
+      (link) =>
+        link.target?.structureId === structure.structureId &&
+        link.target.roundNumber === initialRoundNumber
+    );
+    const sourceRoundNumber = relevantLink.source.roundNumber;
+    matchUpFilters.roundNumbers = [sourceRoundNumber];
+  }
+
+  const { completedMatchUps } = getStructureMatchUps({
+    structureId: sourceStructureId,
     inContext: true,
+    matchUpFilters,
+    drawDefinition,
   });
 
   const assignedParticipantIds = positionAssignments
@@ -46,22 +94,22 @@ export function getValidLuckyLosersAction({
     .filter((f) => f);
 
   const availableLuckyLoserParticipantIds = completedMatchUps
-    .map(({ winningSide, sides }) => sides[1 - (winningSide - 1)])
+    ?.map(({ winningSide, sides }) => sides[1 - (winningSide - 1)])
     .map(({ participantId }) => participantId)
     .filter((participantId) => !assignedParticipantIds.includes(participantId));
 
   const availableLuckyLosers = tournamentParticipants?.filter((participant) =>
-    availableLuckyLoserParticipantIds.includes(participant.participantId)
+    availableLuckyLoserParticipantIds?.includes(participant.participantId)
   );
 
-  availableLuckyLosers.forEach((luckyLoser) => {
+  availableLuckyLosers?.forEach((luckyLoser) => {
     const entry = (drawDefinition.entries || []).find(
       (entry) => entry.participantId === luckyLoser.participantId
     );
     luckyLoser.entryPosition = entry?.entryPosition;
   });
 
-  if (availableLuckyLoserParticipantIds.length) {
+  if (availableLuckyLoserParticipantIds?.length) {
     const validLuckyLosersAction = {
       type: LUCKY_PARTICIPANT,
       method: LUCKY_PARTICIPANT_METHOD,
