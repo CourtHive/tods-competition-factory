967b2fef2853ec8d21f0b8a8a412a003a0b2fce0
CourtHive <charles@courthive.com>
2021-03-16 09:09:45 +0100

    rough in for `addDrawDefinition` test

---
+++ src/tournamentEngine/tests/events/addDrawDefinition.test.js
@@ -0,0 +1,30 @@
+import mocksEngine from '../../../mocksEngine';
+import tournamentEngine from '../../sync';
+
+import { INDIVIDUAL } from '../../../constants/participantTypes';
+
+it('can create flightProfile on addDrawDefinition', () => {
+  mocksEngine.generateTournamentRecord({});
+  const eventName = 'Test Event';
+  const event = { eventName };
+  let result = tournamentEngine.addEvent({ event });
+  let { event: eventResult } = result;
+  expect(result.success).toEqual(true);
+
+  const { eventId } = eventResult;
+
+  const { tournamentParticipants } = tournamentEngine.getTournamentParticipants(
+    {
+      participantFilters: { participantTypes: [INDIVIDUAL] },
+    }
+  );
+  const participantIds = tournamentParticipants.map((p) => p.participantId);
+  result = tournamentEngine.addEventEntries({ eventId, participantIds });
+  expect(result.success).toEqual(true);
+
+  const { drawDefinition } = tournamentEngine.generateDrawDefinition({
+    eventId,
+  });
+  result = tournamentEngine.addDrawDefinition({ eventId, drawDefinition });
+  expect(result.success).toEqual(true);
+});
