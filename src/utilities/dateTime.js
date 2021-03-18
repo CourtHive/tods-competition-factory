5ba875694194486a001ac8d0c87f36870ba95cef
CourtHive <charles@courthive.com>
2021-03-18 07:23:00 +0100

    cleanup

--- src/utilities/dateTime.js
+++ src/utilities/dateTime.js
@@ -159,8 +159,8 @@ function splitTime(value) {
   return time;
 }
 
-export function militaryTime(value, env) {
-  const time = splitTime(value || (env && env.schedule.default_time));
+export function militaryTime(value) {
+  const time = splitTime(value);
   if (time.ampm && time.hours) {
     if (time.ampm.toLowerCase() === 'pm' && parseInt(time.hours) < 12)
       time.hours = ((time.hours && parseInt(time.hours)) || 0) + 12;
@@ -171,8 +171,8 @@ export function militaryTime(value, env) {
   return timeString.split(':').map(zeroPad).join(':');
 }
 
-export function regularTime(value, env) {
-  const time = splitTime(value || (env && env.schedule.default_time));
+export function regularTime(value) {
+  const time = splitTime(value);
   if (time.ampm) return value;
   if (time.hours > 12) {
     time.hours -= 12;
@@ -192,10 +192,8 @@ export function regularTime(value, env) {
   return `${time.hours || '12'}:${time.minutes || '00'} ${time.ampm}`;
 }
 
-export function convertTime(value, env) {
-  return !env || env.schedule.time24
-    ? militaryTime(value, env)
-    : regularTime(value, env);
+export function convertTime(value, time24) {
+  return time24 ? militaryTime(value) : regularTime(value);
 }
 
 export function futureDate(days = 1) {
@@ -313,9 +311,10 @@ export function ymd2date(ymd) {
 
 export function timeToDate(time, date = undefined) {
   const [hours, minutes] = (time || '00:00').split(':');
-  return date
+  const milliseconds = date
     ? new Date(date).setHours(hours, minutes, 0, 0)
     : new Date().setHours(hours, minutes, 0, 0);
+  return new Date(milliseconds);
 }
 
 export function zeroPad(number) {
@@ -398,6 +397,7 @@ export const dateTime = {
   offsetDate,
   offsetTime,
   futureDate,
+  timeToDate,
   convertTime,
   getDateByWeek,
   currentUTCDate,
