7144d0f98317882389302566c0b9bfa82af050a0
CourtHive <charles@courthive.com>
2021-04-12 10:04:55 +0200

    tidy/cleanup

--- src/drawEngine/governors/scoreGovernor/roundRobinTally/tests/getFinishingPosition.test.js
+++ src/drawEngine/governors/scoreGovernor/roundRobinTally/tests/getFinishingPosition.test.js
@@ -61,6 +61,8 @@ it('can breat 3 way ties bye matchUpsWon ratio', () => {
   // drawPosition 3 is therefore 4th
   // drawPosition 5 had only one win and is 5th
 
+  // idsFilter needs to be used to scope the winsRatios to the tied group
+
   const { participantResults } = generateScenario({ outcomeValues });
   const drawPositions = participantResults.map(
     ({ drawPosition }) => drawPosition
@@ -109,7 +111,7 @@ it('can recognize unbreakable 3 way ties', () => {
   ).toEqual([1, 2, 2, 2, 5]);
 });
 
-it('can break 3 way ties bye setsRatio', () => {
+it('can break 3 way ties bye setsRatio without idsFilter', () => {
   const outcomeValues = [
     [[4, 5], '6-0 6-0', 1], // win for drawPosition: 4
     [[2, 4], '6-0 6-0', 2], // win for drawPosition: 4
@@ -133,6 +135,43 @@ it('can break 3 way ties bye setsRatio', () => {
   // drawPosition 3 is therefore 3rd
   // drawPosition 5 had only one win and is 5th
 
+  // does not need to use the idsFilter
+
+  const { participantResults } = generateScenario({ outcomeValues });
+  const drawPositions = participantResults.map(
+    ({ drawPosition }) => drawPosition
+  );
+  expect(drawPositions).toEqual([4, 2, 3, 1, 5]);
+
+  expect(
+    participantResults.map(
+      ({ participantResult }) => participantResult.finishingPosition
+    )
+  ).toEqual([1, 2, 3, 4, 5]);
+});
+
+it('can breake 3 way ties by gamesRatio', () => {
+  const outcomeValues = [
+    [[4, 5], '6-0 6-0', 1], // win for drawPosition: 4
+    [[2, 4], '6-0 6-0', 2], // win for drawPosition: 4
+    [[3, 4], '6-0 6-0', 2], // win for drawPosition: 4
+
+    [[1, 2], '6-0 6-1', 1], // win for drawPosition: 1
+    [[1, 4], '6-0 6-0', 1], // win for drawPosition: 1
+    [[2, 3], '6-0 6-0', 1], // win for drawPosition: 2
+    [[2, 5], '6-0 6-0', 1], // win for drawPosition: 2
+    [[1, 3], '6-0 6-0', 2], // win for drawPosition: 3
+    [[3, 5], '6-0 6-0', 1], // win for drawPosition: 3
+
+    [[1, 5], '6-0 6-0', 2], // win for drawPosition: 5
+  ];
+
+  // drawPosition 4 has 3 wins and is 1st
+  // drawPositions 1, 2 & 3 each have 2 wins
+  // drawPositions 1 beat 2, 2 beat 3 and 3 beat 1
+  // drawPosition 1 lost one more game than 2 and 3 and is therefore last in tied group due to gamesRatio
+  // drawPosition 5 had only one win and is 5th
+
   const { participantResults } = generateScenario({ outcomeValues });
   const drawPositions = participantResults.map(
     ({ drawPosition }) => drawPosition
@@ -152,18 +191,21 @@ it('can break h2h ties between 2 participants', () => {
   // drawPositions 1 & 2 have 3 wins each; 1 beat 2, so 1 is 1st, 2 is 2nd
   // drawPositions 1 & 2 have 3 wins each; 1 beat 2, so 1 is 1st, 2 is 2nd
   // drawPosition 4 has 2 wins, is 3rd
-  // drawPositions 3 & 5 have 2 wins each; 3 beat 5, so 3 is 4th, 5 is 5th
+  // drawPositions 3 & 5 have 1 win each; 3 beat 5, so 3 is 4th, 5 is 5th
   const outcomeValues = [
     [[1, 2], '6-0 6-0', 1], // win for drawPosition: 1
     [[1, 3], '6-0 6-0', 1], // win for drawPosition: 1
     [[1, 4], '6-0 6-0', 1], // win for drawPosition: 1
-    [[1, 5], '6-0 6-0', 2], // win for drawPosition: 5
+
     [[2, 3], '6-0 6-0', 1], // win for drawPosition: 2
     [[2, 4], '6-0 6-0', 1], // win for drawPosition: 2
     [[2, 5], '6-0 6-0', 1], // win for drawPosition: 2
+
     [[3, 4], '6-0 6-0', 2], // win for drawPosition: 4
-    [[3, 5], '6-0 6-0', 1], // win for drawPosition: 3
     [[4, 5], '6-0 6-0', 2], // win for drawPosition: 4
+
+    [[3, 5], '6-0 6-0', 1], // win for drawPosition: 3
+    [[1, 5], '6-0 6-0', 2], // win for drawPosition: 5
   ];
   const expectation = [1, 2, 3, 4, 5];
 
