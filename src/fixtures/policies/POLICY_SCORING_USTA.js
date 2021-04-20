a27d92413ede5ce89e601b5563803ed4a9c553ea
CourtHive <charles@courthive.com>
2021-04-20 14:41:58 +0200

    added `getEventMatchUpFormatTiming` method

--- src/fixtures/policies/POLICY_SCORING_USTA.js
+++ src/fixtures/policies/POLICY_SCORING_USTA.js
@@ -16,81 +16,88 @@ export const POLICY_SCORING_USTA = {
     defaultMatchUpFormat: 'SET3-S:6/TB7',
     matchUpFormats: [
       {
-        name: 'Best of 3 tiebreak sets',
+        categoryNames: [],
+        categoryTypes: [],
+        description: 'Best of 3 tiebreak sets',
         matchUpFormat: 'SET3-S:6/TB7',
       },
       {
-        name: 'Two tiebreak sets, 7-point match tiebreak at one set all',
+        description: 'Two tiebreak sets, 7-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:6/TB7-F:TB7',
       },
       {
-        name: 'Two tiebreak sets, 10-point match tiebreak at one set all',
+        description:
+          'Two tiebreak sets, 10-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:6/TB7-F:TB10',
       },
       {
-        name: 'One standard tiebreak set to 6, 7-point tiebreak at 6 games all',
+        description:
+          'One standard tiebreak set to 6, 7-point tiebreak at 6 games all',
         matchUpFormat: 'SET1-S:6/TB7',
       },
       {
-        name: 'Best of 3 sets to 4',
+        description: 'Best of 3 sets to 4',
         matchUpFormat: 'SET3-S:4/TB7',
       },
       {
-        name:
+        description:
           'Two out of three short sets to 4 with 5-point tiebreak at 3 games all',
         matchUpFormat: 'SET3-S:4/TB5@3',
       },
       {
-        name: 'One short set to 4, 7-point tiebreak at 4 games all',
+        description: 'One short set to 4, 7-point tiebreak at 4 games all',
         matchUpFormat: 'SET1-S:4/TB7',
       },
       {
-        name: 'One short set to 4, 5-point tiebreak at 3 games all',
+        description: 'One short set to 4, 5-point tiebreak at 3 games all',
         matchUpFormat: 'SET1-S:4/TB5@3',
       },
       {
-        name: 'Two short sets to 4, 10-point match tiebreak at one set all',
+        description:
+          'Two short sets to 4, 10-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:4/TB7-F:TB10',
       },
       {
-        name: 'Two short sets to 4, 7-point match tiebreak at one set all',
+        description:
+          'Two short sets to 4, 7-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:4/TB7-F:TB7',
       },
       {
-        name: 'One no advantage set to 5, tiebreak to 9 at 4-4',
+        description: 'One no advantage set to 5, tiebreak to 9 at 4-4',
         matchUpFormat: 'SET1-S:5/TB9@4',
       },
       {
-        name: 'One set to 6 with deciding game at 5 games all',
+        description: 'One set to 6 with deciding game at 5 games all',
         matchUpFormat: 'SET1-S:6NOAD',
       },
       {
-        name: 'One short set to 4, deciding game is played at 3 games all',
+        description:
+          'One short set to 4, deciding game is played at 3 games all',
         matchUpFormat: 'SET1-S:4NOAD',
       },
       {
-        name:
+        description:
           'Two short sets to 4 with deciding game at 3-3, 7-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:4NOAD-F:TB7',
       },
       {
-        name: '8 game pro-set with 7 point tiebreak at 8 games all',
+        description: '8 game pro-set with 7 point tiebreak at 8 games all',
         matchUpFormat: 'SET1-S:8/TB7',
       },
       {
-        name: '8 game pro-set with 7 point tiebreak at 7 games all',
+        description: '8 game pro-set with 7 point tiebreak at 7 games all',
         matchUpFormat: 'SET1-S:8/TB7@7',
       },
       {
-        name: 'Best of 3 10-point tiebreak games',
+        description: 'Best of 3 10-point tiebreak games',
         matchUpFormat: 'SET3-S:TB10',
       },
       {
-        name: 'One 10-point tiebreak game',
+        description: 'One 10-point tiebreak game',
         matchUpFormat: 'SET1-S:TB10',
       },
       {
-        name: 'Timed 20 minute game - game based',
+        description: 'Timed 20 minute game - game based',
         matchUpFormat: 'SET1-S:T20',
       },
     ],
