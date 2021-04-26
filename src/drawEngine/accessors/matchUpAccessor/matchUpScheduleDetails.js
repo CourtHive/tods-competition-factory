2c8abade33933ae8a789317ae9989fd930c70823
CourtHive <charles@courthive.com>
2021-04-26 19:13:09 +0200

    set scheduledDate from scheduledTime if date present and scheduledDate undefined

--- src/drawEngine/accessors/matchUpAccessor/matchUpScheduleDetails.js
+++ src/drawEngine/accessors/matchUpAccessor/matchUpScheduleDetails.js
@@ -5,15 +5,19 @@ import { scheduledMatchUpTime } from './scheduledMatchUpTime';
 import { scheduledMatchUpDate } from './scheduledMatchUpDate';
 import { matchUpAssignedCourtId } from './courtAssignment';
 import { matchUpAssignedVenueId } from './venueAssignment';
+import { extractDate } from '../../../utilities/dateTime';
 
 export function getMatchUpScheduleDetails({ matchUp }) {
   const { milliseconds, time } = matchUpDuration({ matchUp });
-  const { scheduledDate } = scheduledMatchUpDate({ matchUp });
+  let { scheduledDate } = scheduledMatchUpDate({ matchUp });
   const { scheduledTime } = scheduledMatchUpTime({ matchUp });
   const { startTime } = matchUpStartTime({ matchUp });
   const { endTime } = matchUpEndTime({ matchUp });
   const { courtId } = matchUpAssignedCourtId({ matchUp });
   const { venueId } = matchUpAssignedVenueId({ matchUp });
+
+  if (!scheduledDate && scheduledTime)
+    scheduledDate = extractDate(scheduledTime);
 
   const schedule = {
     time,
