f3b024f0660554222b5276f9349d7b87ea0e7718
CourtHive <charles@courthive.com>
2021-05-11 18:32:26 +0200

    cleanup

--- src/competitionEngine/governors/scheduleGovernor/garman/getCourtsAvailableAtPeriodStart.js
+++ src/competitionEngine/governors/scheduleGovernor/garman/getCourtsAvailableAtPeriodStart.js
@@ -20,8 +20,6 @@ export function courtsAvailableAtPeriodStart({
   });
 
   const availableCourts = courts.filter((court) => {
-    // add any matches already scheduled for this court
-    // court.dateAvailbility.bookings.push(...(courtBookings[court.courtId] || []))
     const available =
       Array.isArray(court.dateAvailability) &&
       court.dateAvailability.filter(sameDate).filter(enoughTime);
