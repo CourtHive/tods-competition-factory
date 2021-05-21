d03d3fc9b7d1761707e238a925b23178ebcb3624
CourtHive <charles@courthive.com>
2021-05-21 09:19:00 +0200

    tests for `getEventMatchUpFormatTiming` and `removeEventMatchUpFormatTiming`

---
+++ src/tournamentEngine/tests/scheduling/modifyEventMatchUpFormatTiming.test.js
@@ -0,0 +1,99 @@
+import mocksEngine from '../../../mocksEngine';
+import tournamentEngine from '../../sync';
+
+import POLICY_SCHEDULING_USTA from '../../../fixtures/policies/POLICY_SCHEDULING_USTA';
+import POLICY_SCORING_USTA from '../../../fixtures/policies/POLICY_SCORING_USTA';
+
+// categoryTypes
+// const JUNIOR = 'JUNIOR';
+// const ADULT = 'ADULT';
+
+it('can modify event timing for matchUpFormat codes', () => {
+  const {
+    tournamentRecord,
+    eventIds: [eventId],
+  } = mocksEngine.generateTournamentRecord({
+    drawProfiles: [{ drawSize: 32 }],
+  });
+
+  tournamentEngine.setState(tournamentRecord);
+
+  tournamentEngine.attachPolicy({
+    policyDefinition: POLICY_SCHEDULING_USTA,
+  });
+
+  let result = tournamentEngine.modifyEventMatchUpFormatTiming({
+    eventId,
+    matchUpFormat: 'SET3-S:6/TB7',
+    averageMinutes: 127,
+  });
+  expect(result.success).toEqual(true);
+
+  result = tournamentEngine.modifyEventMatchUpFormatTiming({
+    eventId,
+    matchUpFormat: 'SET1-S:4/TB10',
+    averageMinutes: 137,
+  });
+  expect(result.success).toEqual(true);
+
+  // overwriting value of 137 with 117
+  result = tournamentEngine.modifyEventMatchUpFormatTiming({
+    eventId,
+    matchUpFormat: 'SET1-S:4/TB10',
+    averageMinutes: 117,
+  });
+  expect(result.success).toEqual(true);
+
+  let { eventMatchUpFormatTiming } =
+    tournamentEngine.getEventMatchUpFormatTiming({
+      eventId,
+    });
+  expect(eventMatchUpFormatTiming.map((t) => t.averageMinutes)).toEqual([
+    127, 117,
+  ]);
+
+  ({ eventMatchUpFormatTiming } = tournamentEngine.getEventMatchUpFormatTiming({
+    eventId,
+    matchUpFormats: ['SET3-S:6/TB7', 'SET1-S:4/TB10', 'SET1-S:4/TB10'],
+  }));
+  // expect duplicated matchUpFormat to be filtered out
+  expect(eventMatchUpFormatTiming.map((t) => t.averageMinutes)).toEqual([
+    127, 117,
+  ]);
+
+  let { methods } = tournamentEngine.getMatchUpFormatTimingUpdate();
+  expect(methods.length).toEqual(1);
+  expect(methods[0].method).toEqual('addEventExtension');
+  expect(methods[0].params.extension.value.matchUpAverageTimes.length).toEqual(
+    2
+  );
+
+  result = tournamentEngine.removeEventMatchUpFormatTiming({ eventId });
+  expect(result.success).toEqual(true);
+
+  ({ methods } = tournamentEngine.getMatchUpFormatTimingUpdate());
+  expect(methods.length).toEqual(0);
+
+  ({ eventMatchUpFormatTiming } = tournamentEngine.getEventMatchUpFormatTiming({
+    eventId,
+    matchUpFormats: ['SET3-S:6/TB7', 'SET1-S:4/TB10'],
+  }));
+  expect(eventMatchUpFormatTiming.map((t) => t.averageMinutes)).toEqual([
+    90, 90,
+  ]);
+
+  ({ eventMatchUpFormatTiming } = tournamentEngine.getEventMatchUpFormatTiming({
+    eventId,
+  }));
+  expect(eventMatchUpFormatTiming.length).toEqual(0);
+
+  const policyDefinition = POLICY_SCORING_USTA;
+  tournamentEngine.attachPolicy({ policyDefinition });
+
+  ({ eventMatchUpFormatTiming } = tournamentEngine.getEventMatchUpFormatTiming({
+    eventId,
+  }));
+  expect(policyDefinition.scoring.matchUpFormats.length).toEqual(
+    eventMatchUpFormatTiming.length
+  );
+});
