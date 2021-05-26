b7d99688881edeb6f1277fa437e2fcd8b2ac0e13
CourtHive <charles@courthive.com>
2021-05-26 10:46:33 +0200

    complete use of profileHash for including schedulingProfile in matchUps sort

--- src/global/sorting/scheduledSortedMatchUps.js
+++ src/global/sorting/scheduledSortedMatchUps.js
@@ -1,30 +1,35 @@
-import {
-  extractDate,
-  extractTime /*, timeToDate */,
-} from '../../utilities/dateTime';
+import { extractDate, extractTime } from '../../utilities/dateTime';
 
 export function scheduledSortedMatchUps({ matchUps = [], schedulingProfile }) {
   const profileHash = {};
-  const sortedMatchUps = [];
+
+  // hash is used to store a sort order value for scheduled rounds
+  const getHash = ({ eventId, drawId, structureId, roundNumber }) => {
+    return `${eventId}|${drawId}|${structureId}${roundNumber}`;
+  };
 
   if (schedulingProfile?.length) {
-    /*
-    // 1. Segregate matchUps by date (date can come from schedule.scheduledDate or schedule.scheduledTime)
-    // 2. For each date group, sort all matchUps by time
-    // 3. for each time group, sort all matchUps by `${eventId}|${drawId}|${structureId}`
-    const dateSchedulingProfiles = schedulingProfile.sort((a, b) => {
-      new Date(a.scheduleDate).getTime() - new Date(b.scheduleDate).getTime();
-    });
-    dateSchedulingProfiles.forEach((dateProfile) => {
-      const sortedRounds = rounds.sort(
+    const roundsGroupings = schedulingProfile
+      .map(({ venues }) => venues.map(({ rounds }) => rounds))
+      .flat();
+
+    roundsGroupings.forEach((grouping) => {
+      const sortedRounds = grouping.sort(
         (a, b) => (a.sortOrder || 0) - (b.sortOrder || 0)
       );
-    })
-    */
+      sortedRounds.forEach(
+        ({ eventId, drawId, structureId, roundNumber }, index) => {
+          const hash = getHash({ eventId, drawId, structureId, roundNumber });
+          profileHash[hash] = index;
+        }
+      );
+    });
   }
 
+  const sortedMatchUps = [];
   const dateGroups = { noScheduledDate: [] };
 
+  // 1. Segregate matchUps by date (date can come from schedule.scheduledDate or schedule.scheduledTime)
   for (const matchUp of matchUps) {
     const schedule = matchUp.schedule || {};
     const scheduledDate =
@@ -39,6 +44,7 @@ export function scheduledSortedMatchUps({ matchUps = [], schedulingProfile }) {
   // this works because all dates are strings and 'noScheduledDate' sorts to last after numeric strings
   const sortedDateKeys = Object.keys(dateGroups).sort();
 
+  // 2. For each date group, sort all matchUps by time
   for (const scheduledDate of sortedDateKeys) {
     const dateGroup = dateGroups[scheduledDate];
 
@@ -57,12 +63,13 @@ export function scheduledSortedMatchUps({ matchUps = [], schedulingProfile }) {
     // this works because all times are strings '00:00' and 'noScheduledTime' sorts to last after numeric strings
     const sortedTimeKeys = Object.keys(timeGroups).sort();
 
+    // 3. for each time group, sort all matchUps by hash values derived from rounds group order
     for (const scheduledTime of sortedTimeKeys) {
       const timeGroup = timeGroups[scheduledTime];
 
       timeGroup.sort((a, b) => {
-        const sortValueA = profileHash[matchUpHash(a)] || 0;
-        const sortValueB = profileHash[matchUpHash(b)] || 0;
+        const sortValueA = profileHash[getHash(a)] || 0;
+        const sortValueB = profileHash[getHash(b)] || 0;
         return sortValueA - sortValueB;
       });
 
@@ -71,33 +78,4 @@ export function scheduledSortedMatchUps({ matchUps = [], schedulingProfile }) {
   }
 
   return sortedMatchUps;
-  /*
-  return matchUps.sort((a, b) => {
-    return (
-      getComparisionTime(a?.scheduledTime) -
-      getComparisionTime(b?.scheduledTime)
-    );
-  });
-  */
 }
-
-function matchUpHash(matchUp) {
-  const { eventId, drawId, structureId, roundNumber } = matchUp;
-  return `${eventId}|${drawId}|${structureId}${roundNumber}`;
-}
-
-/*
-function getComparisionTime(scheduledTime) {
-  return timeToDate(extractTime(scheduledTime)).getTime();
-}
-*/
-
-/*
-    dateGroup.sort((a, b) => {
-      const splitA = extractTime(a?.schedule?.scheduledTime || '').split(':');
-      const splitB = extractTime(b?.schedule?.scheduledTime || '').split(':');
-      parseInt(splitA[0]) - parseInt(splitB[0]) === 0
-        ? parseInt(splitA[1]) - parseInt(splitB[1])
-        : parseInt(splitA[0]) - parseInt(splitB[0]);
-    });
-    */
