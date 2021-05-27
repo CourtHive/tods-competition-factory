20a0c1b58f7f4886c1ebc05297aa7c3e2bf9eadd
CourtHive <charles@courthive.com>
2021-05-27 14:39:37 +0200

    add `competitionEngine.modifyEventMatchUpFormatTiming` function

---
+++ src/competitionEngine/governors/scheduleGovernor/matchUpFormatTiming/modifyEventMatchUpFormatTiming.js
@@ -0,0 +1,46 @@
+import { modifyEventMatchUpFormatTiming as modifyEventTiming } from '../../../../tournamentEngine/governors/scheduleGovernor/matchUpFormatTiming/modifyEventMatchUpFormatTiming';
+import { findEvent } from '../../../../tournamentEngine/getters/eventGetter';
+
+import {
+  EVENT_NOT_FOUND,
+  INVALID_VALUES,
+  MISSING_EVENT,
+  MISSING_TOURNAMENT_RECORDS,
+} from '../../../../constants/errorConditionConstants';
+
+export function modifyEventMatchUpFormatTiming({
+  tournamentRecords,
+  tournamentId,
+  eventId,
+
+  matchUpFormat,
+  averageMinutes,
+  recoveryMinutes,
+}) {
+  if (!tournamentRecords) return { error: MISSING_TOURNAMENT_RECORDS };
+  if (!eventId) return { error: MISSING_EVENT };
+
+  const tournamentIds = Object.keys(tournamentRecords).filter(
+    (currentTournamentId) =>
+      !tournamentId || tournamentId === currentTournamentId
+  );
+
+  if (tournamentId && !tournamentIds.includes(tournamentId))
+    return { error: INVALID_VALUES };
+
+  for (const currentTournamentId of tournamentIds) {
+    const tournamentRecord = tournamentRecords[currentTournamentId];
+    const { event } = findEvent({ tournamentRecord, eventId });
+    if (event) {
+      return modifyEventTiming({
+        tournamentRecord,
+        eventId,
+        matchUpFormat,
+        averageMinutes,
+        recoveryMinutes,
+      });
+    }
+  }
+
+  return { error: EVENT_NOT_FOUND };
+}
