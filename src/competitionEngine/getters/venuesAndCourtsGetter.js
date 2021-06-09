8af51c1e5c703bccd56c3a5e9717f8965884e12b
CourtHive <charles@courthive.com>
2021-06-09 13:40:55 +0200

    add chained conditional for undefined venues

--- src/competitionEngine/getters/venuesAndCourtsGetter.js
+++ src/competitionEngine/getters/venuesAndCourtsGetter.js
@@ -16,11 +16,11 @@ export function getVenuesAndCourts({ tournamentRecords }) {
   const tournamentIds = Object.keys(tournamentRecords);
   tournamentIds.forEach((tournamentId) => {
     const tournamentRecord = tournamentRecords[tournamentId];
-    tournamentRecord.venues.forEach((venue) => {
+    tournamentRecord.venues?.forEach((venue) => {
       if (!uniqueVenueIds.includes(venue.venueId)) {
         venues.push(makeDeepCopy(venue));
       }
-      venue.courts.forEach((court) => {
+      venue.courts?.forEach((court) => {
         if (!uniqueCourtIds.includes(court.courtId)) {
           const inContextCourt = Object.assign({}, makeDeepCopy(court), {
             venueId: venue.venueId,
