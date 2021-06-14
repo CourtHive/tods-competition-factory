84d996f39f14c71fadde2ee866b25d6db82a0c42
CourtHive <charles@courthive.com>
2021-06-14 11:14:11 +0200

    add chained conditional and check for `venue` object in `addVenue`

--- src/competitionEngine/governors/competitionsGovernor/venueManagement/addVenue.js
+++ src/competitionEngine/governors/competitionsGovernor/venueManagement/addVenue.js
@@ -1,17 +1,21 @@
 import { addVenue as venueAdd } from '../../../../tournamentEngine/governors/venueGovernor/addVenue';
 
-import { MISSING_TOURNAMENT_RECORDS } from '../../../../constants/errorConditionConstants';
+import {
+  INVALID_VALUES,
+  MISSING_TOURNAMENT_RECORDS,
+} from '../../../../constants/errorConditionConstants';
 import { SUCCESS } from '../../../../constants/resultConstants';
 
 export function addVenue({ tournamentRecords, venue }) {
   if (!tournamentRecords) return { error: MISSING_TOURNAMENT_RECORDS };
+  if (typeof venue !== 'object') return { error: INVALID_VALUES };
 
   let venueId = venue?.venueId;
   for (const tournamentRecord of Object.values(tournamentRecords)) {
     Object.assign(venue, { venueId });
     const result = venueAdd({ tournamentRecord, venue, returnDetails: true });
     venueId = venueId || result.venue?.venueId;
-    if (result.error) return result;
+    if (result?.error) return result;
   }
 
   return SUCCESS;
