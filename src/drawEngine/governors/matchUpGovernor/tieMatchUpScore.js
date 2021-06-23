ac364762a0156c12f19d5445f1625eba1393972d
CourtHive <charles@courthive.com>
2021-06-23 07:46:04 +0200

    space

--- src/drawEngine/governors/matchUpGovernor/tieMatchUpScore.js
+++ src/drawEngine/governors/matchUpGovernor/tieMatchUpScore.js
@@ -12,12 +12,15 @@ import { SUCCESS } from '../../../constants/resultConstants';
 export function updateTieMatchUpScore({ drawDefinition, matchUpId }) {
   if (!drawDefinition) return { error: MISSING_DRAW_DEFINITION };
   if (!matchUpId) return { error: MISSING_MATCHUP };
+
   const matchUp = findMatchUp({ drawDefinition, matchUpId });
   if (!matchUp) return { error: MATCHUP_NOT_FOUND };
+
   const scoreString = generateTieMatchUpScoreString({ matchUp });
   const scoreObject = {};
   const { winningSide } = matchUp;
   const reverseScoreString = scoreString.split('-').reverse().join('-');
+
   if (winningSide) {
     const winnerPerspective = scoreString;
     const loserPerspective = reverseScoreString;
@@ -29,10 +32,12 @@ export function updateTieMatchUpScore({ drawDefinition, matchUpId }) {
     scoreObject.scoreStringSide1 = scoreString;
     scoreObject.scoreStringSide2 = reverseScoreString;
   }
+
   modifyMatchUpScore({
     drawDefinition,
     matchUp,
     score: scoreObject,
   });
+
   return SUCCESS;
 }
