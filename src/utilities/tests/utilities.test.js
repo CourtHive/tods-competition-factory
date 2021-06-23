c0496db665dc82b0a010757310dcf48c0296fc84
CourtHive <charles@courthive.com>
2021-06-23 06:03:14 +0200

    test coverage

--- src/utilities/tests/utilities.test.js
+++ src/utilities/tests/utilities.test.js
@@ -1,4 +1,5 @@
 import { countValues } from '..';
+import { JSON2CSV } from '../json';
 
 it('can count values and determine active drawPositions', () => {
   const drawPositions = [1, 1, 2, 3, 4, 5, 5, 6];
@@ -9,4 +10,9 @@ it('can count values and determine active drawPositions', () => {
     }, [])
     .map((p) => parseInt(p));
   expect(activeDrawPositions).toMatchObject([1, 5]);
+});
+
+it('can create CSV from shallow JSON objects', () => {
+  const csv = JSON2CSV([{ a: '1', b: '2' }]);
+  expect(csv).not.toBeUndefined;
 });
