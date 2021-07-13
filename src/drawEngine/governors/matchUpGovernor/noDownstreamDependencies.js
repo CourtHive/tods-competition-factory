82b3682f718ce399277cb5aedd9336ce4645124d
CourtHive <charles@courthive.com>
2021-07-13 09:52:03 +0200

    `setMatchUpStatus` / `noDownstreamDependencies` code cleanup for clarity

--- src/drawEngine/governors/matchUpGovernor/noDownstreamDependencies.js
+++ src/drawEngine/governors/matchUpGovernor/noDownstreamDependencies.js
@@ -7,25 +7,16 @@ import { removeDoubleWalkover } from './removeDoubleWalkover';
 import { modifyMatchUpScore } from './modifyMatchUpScore';
 import { scoreHasValue } from './scoreHasValue';
 
+import { SUCCESS } from '../../../constants/resultConstants';
 import {
   ABANDONED,
   DOUBLE_WALKOVER,
   INCOMPLETE,
   TO_BE_PLAYED,
 } from '../../../constants/matchUpStatusConstants';
-import { SUCCESS } from '../../../constants/resultConstants';
 
 export function noDownstreamDependencies(params) {
-  const {
-    matchUp,
-    matchUpId,
-    matchUpFormat,
-    score,
-    winningSide,
-    tournamentRecord,
-    event,
-  } = params;
-  let { matchUpStatus } = params;
+  const { matchUp, matchUpStatus, score, winningSide } = params;
 
   const doubleWalkoverCleanup =
     matchUp?.matchUpStatus === DOUBLE_WALKOVER &&
@@ -34,7 +25,19 @@ export function noDownstreamDependencies(params) {
     const result = removeDoubleWalkover(params);
     if (result.error) return result;
   }
+
   const doubleWalkover = matchUpStatus === DOUBLE_WALKOVER;
+  const scoreWithNoWinningSide =
+    !winningSide && scoreHasValue({ score }) && !doubleWalkover;
+  const removeScore = ![INCOMPLETE, ABANDONED].includes(
+    matchUpStatus || INCOMPLETE
+  );
+
+  const removeWinningSide =
+    matchUp.winningSide && !winningSide && !scoreHasValue({ score });
+
+  const existingWOWO = matchUp?.matchUpStatus === DOUBLE_WALKOVER;
+  const statusNotTBP = matchUpStatus && matchUpStatus !== TO_BE_PLAYED;
 
   const removeDirected = ({ removeScore } = {}) => {
     const { structure, drawDefinition } = params;
@@ -43,37 +46,13 @@ export function noDownstreamDependencies(params) {
     return removeDirectedParticipants(params);
   };
 
-  if (winningSide) {
-    const result = attemptToSetWinningSide(params);
-    if (result.error) return result;
-  } else if (!winningSide && scoreHasValue({ score }) && !doubleWalkover) {
-    if (!matchUpStatus) matchUpStatus = INCOMPLETE;
-    const removeScore = ![INCOMPLETE, ABANDONED].includes(matchUpStatus);
-    const result = removeDirected({ removeScore });
-    if (result.error) return result;
-  } else if (matchUpStatus && matchUpStatus !== TO_BE_PLAYED) {
-    const { error } = attemptToSetMatchUpStatus(params);
-    if (error) return { error };
-  } else if (!winningSide && matchUp.winningSide && !scoreHasValue({ score })) {
-    const result = removeDirected();
-    if (result.error) return result;
-  } else if (matchUp) {
-    if (matchUp.matchUpStatus === DOUBLE_WALKOVER) {
-      const result = checkDoubleWalkoverPropagation(params);
-      if (result.error) return result;
-    }
-    modifyMatchUpScore({
-      drawDefinition: params.drawDefinition,
-      removeScore: true,
-      matchUpFormat,
-      matchUpId,
-      matchUp,
-      tournamentRecord,
-      event,
-    });
-  } else {
-    console.log('unknown condition');
-  }
-
-  return SUCCESS;
+  return (
+    (winningSide && attemptToSetWinningSide(params)) ||
+    (scoreWithNoWinningSide && removeDirected({ removeScore })) ||
+    (statusNotTBP && attemptToSetMatchUpStatus(params)) ||
+    (removeWinningSide && removeDirected()) ||
+    (existingWOWO && checkDoubleWalkoverPropagation(params)) ||
+    (matchUp && modifyMatchUpScore({ ...params, removeScore: true })) ||
+    (console.log('unknown condition') && { ...SUCCESS })
+  );
 }
