7e5991d7ba478595a847152206fffed90f559deb
CourtHive <charles@courthive.com>
2021-07-15 11:20:42 +0200

    functional decomposition / rationalization for reuse

---
+++ src/drawEngine/governors/matchUpGovernor/getUpdatedDrawPositions.js
@@ -0,0 +1,32 @@
+import { numericSort } from '../../../utilities';
+
+/**
+ * Insures that a matchUp has 2 and only 2 drawPositions
+ * Checks to see whether drawPosition is already present in drawPositions
+ * Does NOT allow a position to be assigned if there are already 2 drawPositions
+ *
+ * @param {number} drawPosition - drawPosition which is to be assigned to a matchUp
+ * @param {number[]} drawPositions - existing drawPositions
+ *
+ */
+export function getUpdatedDrawPositions({ drawPosition, drawPositions }) {
+  let positionAdded;
+  let positionAssigned = drawPositions.includes(drawPosition);
+  const updatedDrawPositions = positionAssigned
+    ? drawPositions
+    : []
+        .concat(...drawPositions, undefined, undefined)
+        .slice(0, 2) // accounts for empty array, should always have length 2
+        .map((position) => {
+          if (!position && !positionAssigned) {
+            positionAssigned = true;
+            positionAdded = true;
+            return drawPosition;
+          } else {
+            return position;
+          }
+        })
+        .sort(numericSort);
+
+  return { updatedDrawPositions, positionAdded, positionAssigned };
+}
