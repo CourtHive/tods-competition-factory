0695e8eb92c7298058121a49c2e92049d3cb8818
CourtHive <charles@courthive.com>
2021-07-18 09:22:54 +0200

    disable SCORE matchUpAction for active DOUBLE_WALKOVER

--- src/drawEngine/governors/queryGovernor/matchUpActions.js
+++ src/drawEngine/governors/queryGovernor/matchUpActions.js
@@ -1,6 +1,10 @@
 import { structureAssignedDrawPositions } from '../../getters/positionsGetter';
 import { isDirectingMatchUpStatus } from '../matchUpGovernor/checkStatusType';
+import { getAllDrawMatchUps } from '../../getters/getMatchUps/drawMatchUps';
+import { isActiveDownstream } from '../matchUpGovernor/isActiveDownstream';
 import { getAppliedPolicies } from '../policyGovernor/getAppliedPolicies';
+import { getMatchUpsMap } from '../../getters/getMatchUps/getMatchUpsMap';
+import { positionTargets } from '../positionGovernor/positionTargets';
 import { findMatchUp } from '../../getters/getMatchUps/findMatchUp';
 import { isCompletedStructure } from './structureActions';
 
@@ -12,7 +16,10 @@ import {
   MISSING_DRAW_DEFINITION,
   MISSING_MATCHUP_ID,
 } from '../../../constants/errorConditionConstants';
-import { BYE } from '../../../constants/matchUpStatusConstants';
+import {
+  BYE,
+  DOUBLE_WALKOVER,
+} from '../../../constants/matchUpStatusConstants';
 import {
   END,
   REFEREE,
@@ -30,7 +37,13 @@ import {
  * @param {string} matchUpId
  *
  */
-export function matchUpActions({ drawDefinition, matchUpId }) {
+export function matchUpActions({
+  drawDefinition,
+  matchUpId,
+
+  matchUpsMap,
+  inContextDrawMatchUps,
+}) {
   if (!drawDefinition) return { error: MISSING_DRAW_DEFINITION };
   if (!matchUpId) return { error: MISSING_MATCHUP_ID };
 
@@ -68,6 +81,8 @@ export function matchUpActions({ drawDefinition, matchUpId }) {
         return byeAssignedDrawPositions.includes(drawPosition) || isByeMatchUp;
       }, false));
 
+  if (isByeMatchUp) return { validActions, isByeMatchUp };
+
   const matchUpDrawPositionsAreAssigned = drawPositions?.reduce(
     (assignedBoolean, drawPosition) =>
       participantAssignedDrawPositions.includes(drawPosition) &&
@@ -75,83 +90,102 @@ export function matchUpActions({ drawDefinition, matchUpId }) {
     drawPositions?.length === 2
   );
 
-  if (isByeMatchUp) {
-    return { validActions, isByeMatchUp };
-  } else {
-    validActions.push({ type: REFEREE });
-    const isInComplete = !isDirectingMatchUpStatus({
-      matchUpStatus: matchUp.matchUpStatus,
+  validActions.push({ type: REFEREE });
+  const isInComplete = !isDirectingMatchUpStatus({
+    matchUpStatus: matchUp.matchUpStatus,
+  });
+
+  const { appliedPolicies } = getAppliedPolicies({ drawDefinition });
+  const structureScoringPolicies = appliedPolicies?.scoring?.structures;
+  const stageSpecificPolicies =
+    structureScoringPolicies?.stage &&
+    structureScoringPolicies?.stage[structure.stage];
+  const sequenceSpecificPolicies =
+    stageSpecificPolicies?.stageSequence &&
+    stageSpecificPolicies.stageSequence[structure.stageSequence];
+  const requireAllPositionsAssigned =
+    appliedPolicies?.scoring?.requireAllPositionsAssigned ||
+    stageSpecificPolicies?.requireAllPositionsAssigned ||
+    sequenceSpecificPolicies?.requireAllPositionsAssigned;
+  const scoringActive = !requireAllPositionsAssigned || allPositionsAssigned;
+
+  const hasParticipants =
+    matchUp.sides &&
+    matchUp.sides.filter((side) => side && side.participantId).length === 2;
+
+  let activeDownstream;
+  const isDoubleWalkover = matchUp.matchUpStatus === DOUBLE_WALKOVER;
+  if (isDoubleWalkover) {
+    matchUpsMap = matchUpsMap || getMatchUpsMap({ drawDefinition });
+
+    if (!inContextDrawMatchUps) {
+      ({ matchUps: inContextDrawMatchUps } = getAllDrawMatchUps({
+        drawDefinition,
+        inContext: true,
+        includeByeMatchUps: true,
+
+        matchUpsMap,
+      }));
+    }
+
+    const targetData = positionTargets({
+      matchUpId,
+      drawDefinition,
+      inContextDrawMatchUps,
     });
-    const { appliedPolicies } = getAppliedPolicies({ drawDefinition });
-    const structureScoringPolicies = appliedPolicies?.scoring?.structures;
-    const stageSpecificPolicies =
-      structureScoringPolicies?.stage &&
-      structureScoringPolicies?.stage[structure.stage];
-    const sequenceSpecificPolicies =
-      stageSpecificPolicies?.stageSequence &&
-      stageSpecificPolicies.stageSequence[structure.stageSequence];
-    const requireAllPositionsAssigned =
-      appliedPolicies?.scoring?.requireAllPositionsAssigned ||
-      stageSpecificPolicies?.requireAllPositionsAssigned ||
-      sequenceSpecificPolicies?.requireAllPositionsAssigned;
-    const scoringActive = !requireAllPositionsAssigned || allPositionsAssigned;
-
-    const hasParticipants =
-      matchUp.sides &&
-      matchUp.sides.filter((side) => side && side.participantId).length === 2;
-
-    const readyToScore = matchUpDrawPositionsAreAssigned || hasParticipants;
-
-    const { drawId } = drawDefinition;
-    const addPenaltyAction = {
-      type: ADD_PENALTY,
-      method: ADD_PENALTY_METHOD,
-      payload: {
-        drawId,
-        matchUpId,
-        penaltyCode: undefined,
-        penaltyType: undefined,
-        participantIds: [],
-        notes: undefined,
-      },
-    };
-    if (isInComplete && !isByeMatchUp) {
-      validActions.push({ type: SCHEDULE });
-    }
-    if (readyToScore && !isByeMatchUp) {
-      validActions.push(addPenaltyAction);
-    }
-    if (isInComplete && readyToScore && !isByeMatchUp) {
-      validActions.push({ type: STATUS });
-    }
-    if (scoringActive && readyToScore && !isByeMatchUp) {
-      const { matchUpId, matchUpFormat } = matchUp;
-      const payload = {
-        drawId,
-        matchUpId,
-        matchUpFormat,
-        outcome: {
-          scoreStringSide1: undefined,
-          scoreStringSide2: undefined,
-          sets: [],
-        },
-        winningSide: undefined,
-      };
-      validActions.push({
-        type: SCORE,
-        method: 'setMatchUpStatus',
-        message: 'set outcome and winningSide',
-        payload,
-      });
-      validActions.push({ type: START });
-      validActions.push({ type: END });
-    }
+    activeDownstream = isActiveDownstream({
+      inContextDrawMatchUps,
+      targetData,
+      drawDefinition,
+    });
   }
 
-  const result = {
-    isByeMatchUp,
+  const readyToScore =
+    (matchUpDrawPositionsAreAssigned || hasParticipants) &&
+    !(isDoubleWalkover && activeDownstream);
+
+  const { drawId } = drawDefinition;
+  const addPenaltyAction = {
+    type: ADD_PENALTY,
+    method: ADD_PENALTY_METHOD,
+    payload: {
+      drawId,
+      matchUpId,
+      penaltyCode: undefined,
+      penaltyType: undefined,
+      participantIds: [],
+      notes: undefined,
+    },
+  };
+  if (isInComplete) validActions.push({ type: SCHEDULE });
+  if (readyToScore) validActions.push(addPenaltyAction);
+  if (isInComplete && readyToScore) validActions.push({ type: STATUS });
+  if (scoringActive && readyToScore) {
+    const { matchUpId, matchUpFormat } = matchUp;
+    const payload = {
+      drawId,
+      matchUpId,
+      matchUpFormat,
+      outcome: {
+        scoreStringSide1: undefined,
+        scoreStringSide2: undefined,
+        sets: [],
+      },
+      winningSide: undefined,
+    };
+    validActions.push({
+      type: SCORE,
+      method: 'setMatchUpStatus',
+      message: 'set outcome and winningSide',
+      payload,
+    });
+    validActions.push({ type: START });
+    validActions.push({ type: END });
+  }
+
+  return {
     validActions,
+    isDoubleWalkover,
     structureIsComplete,
   };
-  return result;
 }
