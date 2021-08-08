851ce55b536c1e3c3b6934c4b067afffcfb09526
CourtHive <charles@courthive.com>
2021-08-08 19:31:35 +0200

    cleanup

--- src/drawEngine/accessors/matchUpAccessor/courtAssignment.js
+++ src/drawEngine/accessors/matchUpAccessor/courtAssignment.js
@@ -3,18 +3,6 @@ import { latestVisibleTimeItemValue } from './latestVisibleTimeItemValue';
 import { ASSIGN_COURT } from '../../../constants/timeItemConstants';
 
 export function matchUpAssignedCourtId({ matchUp, visibilityThreshold }) {
-  /*
-  const timeItems = matchUp?.timeItems || [];
-  const getTimeStamp = (item) =>
-    !item.createdAt ? 0 : new Date(item.createdAt).getTime();
-  const courtAssignmentItems = timeItems
-    .filter((timeItem) => timeItem?.itemType === ASSIGN_COURT)
-    .sort((a, b) => getTimeStamp(a) - getTimeStamp(b));
-
-  const lastCourtAssignmentItem = courtAssignmentItems.pop();
-  const courtId = lastCourtAssignmentItem && lastCourtAssignmentItem.itemValue;
-  */
-
   const itemValue = latestVisibleTimeItemValue(
     matchUp?.timeItems || [],
     ASSIGN_COURT,
--- src/drawEngine/accessors/matchUpAccessor/venueAssignment.js
+++ src/drawEngine/accessors/matchUpAccessor/venueAssignment.js
@@ -3,18 +3,6 @@ import { latestVisibleTimeItemValue } from './latestVisibleTimeItemValue';
 import { ASSIGN_VENUE } from '../../../constants/timeItemConstants';
 
 export function matchUpAssignedVenueId({ matchUp, visibilityThreshold }) {
-  /*
-  const timeItems = matchUp?.timeItems || [];
-  const getTimeStamp = (item) =>
-    !item.createdAt ? 0 : new Date(item.createdAt).getTime();
-  const lastCourtAssignmentItem = timeItems
-    .filter((timeItem) => timeItem?.itemType === ASSIGN_VENUE)
-    .sort((a, b) => getTimeStamp(a) - getTimeStamp(b))
-    .pop();
-
-  const venueId = lastCourtAssignmentItem && lastCourtAssignmentItem.itemValue;
-  */
-
   const itemValue = latestVisibleTimeItemValue(
     matchUp?.timeItems || [],
     ASSIGN_VENUE,
