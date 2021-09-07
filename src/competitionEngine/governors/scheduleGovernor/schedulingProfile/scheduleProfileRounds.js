6f40cc5226c43c1c87a28d0725b15cd21f56d2f3
CourtHive <charles@courthive.com>
2021-09-07 08:51:43 +0200

    decomposition: extract `getScheduledRoundDetails` from `scheduleProfileRounts`

--- src/competitionEngine/governors/scheduleGovernor/schedulingProfile/scheduleProfileRounds.js
+++ src/competitionEngine/governors/scheduleGovernor/schedulingProfile/scheduleProfileRounds.js
@@ -1,17 +1,12 @@
 import { getContainedStructures } from '../../../../tournamentEngine/governors/tournamentGovernor/getContainedStructures';
 import { addTournamentTimeItem } from '../../../../tournamentEngine/governors/tournamentGovernor/addTimeItem';
-import { filterMatchUps } from '../../../../drawEngine/getters/getMatchUps/filterMatchUps';
-import { findMatchUpFormatTiming } from '../matchUpFormatTiming/findMatchUpFormatTiming';
-import { getMatchUpFormat } from '../../../../tournamentEngine/getters/getMatchUpFormat';
 import { extractDate, isValidDateString } from '../../../../utilities/dateTime';
-import { findEvent } from '../../../../tournamentEngine/getters/eventGetter';
 import { allCompetitionMatchUps } from '../../../getters/matchUpsGetter';
 import { scheduleMatchUps } from '../scheduleMatchUps/scheduleMatchUps';
+import { getScheduledRoundDetails } from './getScheduledRoundDetails';
 import { addNotice, getTopics } from '../../../../global/globalState';
-import { isConvertableInteger } from '../../../../utilities/math';
 import { getMatchUpDailyLimits } from '../getMatchUpDailyLimits';
 import { getSchedulingProfile } from './schedulingProfile';
-import { isPowerOf2 } from '../../../../utilities';
 
 import { SUCCESS } from '../../../../constants/resultConstants';
 import { AUDIT } from '../../../../constants/topicConstants';
@@ -20,10 +15,6 @@ import {
   MISSING_TOURNAMENT_RECORDS,
   NO_VALID_DATES,
 } from '../../../../constants/errorConditionConstants';
-import {
-  BYE,
-  completedMatchUpStatuses,
-} from '../../../../constants/matchUpStatusConstants';
 
 export function scheduleProfileRounds({
   tournamentRecords,
@@ -100,105 +91,16 @@ export function scheduleProfileRounds({
     for (const venue of venues) {
       const { rounds = [], venueId } = venue;
 
-      const hashes = [];
-      const sortedRounds = rounds.sort(
-        (a, b) => (a.sortOrder || 0) - (b.sortOrder || 0)
-      );
-
-      const recoveryMinutesMap = {};
-      let greatestAverageMinutes = 0;
-      const scheduledRoundsDetails = sortedRounds.map((round) => {
-        const roundPeriodLength =
-          round.periodLength ||
-          dateSchedulingProfile?.periodLength ||
-          periodLength;
-        const structureIds = containedStructureIds[round.structureId] || [
-          round.structureId,
-        ];
-        const roundMatchUpFilters = {
-          tournamentIds: [round.tournamentId],
-          roundNumbers: [round.roundNumber],
-          matchUpIds: round.matchUpIds,
-          eventIds: [round.eventId],
-          drawIds: [round.drawId],
-          structureIds,
-        };
-        let roundMatchUps = filterMatchUps({
-          matchUps,
-          processContext: true,
-          ...roundMatchUpFilters,
-        });
-
-        // filter by roundSegment
-        const { segmentNumber, segmentsCount } = round.roundSegment || {};
-        if (
-          isConvertableInteger(segmentNumber) &&
-          isPowerOf2(roundMatchUps?.length) &&
-          isPowerOf2(segmentsCount) &&
-          segmentNumber > 0 &&
-          segmentNumber <= segmentsCount &&
-          segmentsCount < roundMatchUps?.length &&
-          !round.matchUpIds?.length
-        ) {
-          const segmentSize = roundMatchUps.length / segmentsCount;
-          const firstSegmentIndex = segmentSize * (segmentNumber - 1);
-          roundMatchUps = roundMatchUps.slice(
-            firstSegmentIndex,
-            firstSegmentIndex + segmentSize
-          );
-        }
-
-        const tournamentRecord = tournamentRecords[round.tournamentId];
-        const { drawDefinition, event } = findEvent({
-          tournamentRecord,
-          drawId: round.drawId,
-        });
-        const { matchUpFormat } = getMatchUpFormat({
-          tournamentRecord,
-          structureId: round.structureId,
-          drawDefinition,
-          event,
-        });
-
-        const { eventType, category } = event || {};
-        const { categoryName, ageCategoryCode } = category || {};
-        const { averageMinutes, recoveryMinutes, error } =
-          findMatchUpFormatTiming({
-            tournamentRecords,
-            categoryName: categoryName || ageCategoryCode,
-            tournamentId: round.tournamentId,
-            eventId: round.eventId,
-            matchUpFormat,
-            eventType,
-          });
-        if (error) return { error, round };
-
-        const matchUpIds = roundMatchUps
-          .filter(
-            ({ matchUpStatus }) =>
-              // don't attempt to scheduled completed matchUpstatuses
-              !completedMatchUpStatuses.includes(matchUpStatus) &&
-              matchUpStatus !== BYE
-          )
-          .map(({ matchUpId }) => matchUpId);
-        matchUpIds.forEach(
-          (matchUpId) => (recoveryMinutesMap[matchUpId] = recoveryMinutes)
-        );
-
-        greatestAverageMinutes = Math.max(
-          averageMinutes || 0,
-          greatestAverageMinutes
-        );
-        const hash = `${averageMinutes}|${roundPeriodLength}`;
-        if (!hashes.includes(hash)) hashes.push(hash);
-
-        return {
-          hash,
-          matchUpIds,
-          averageMinutes,
-          recoveryMinutes,
-          roundPeriodLength,
-        };
+      const {
+        recoveryMinutesMap,
+        scheduledRoundsDetails,
+        greatestAverageMinutes,
+      } = getScheduledRoundDetails({
+        tournamentRecords,
+        containedStructureIds,
+        periodLength,
+        matchUps,
+        rounds,
       });
 
       let lastHash;
