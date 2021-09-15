fdd05001a90ca76ea1dd10d3891689bae5b0ff2e
CourtHive <charles@courthive.com>
2021-09-15 20:47:59 +0200

    update JSON2CSV to flatten all objects

--- src/utilities/json.js
+++ src/utilities/json.js
@@ -1,15 +1,38 @@
 import { INVALID_VALUES } from '../constants/errorConditionConstants';
 
-export function JSON2CSV(arrayOfJSON) {
+export function JSON2CSV(arrayOfJSON, targetAttributes, joiner = '.') {
   if (!Array.isArray(arrayOfJSON)) return { error: INVALID_VALUES };
-  const replacer = (_, value) => (value === null ? '' : value);
-  const header = Object.keys(arrayOfJSON[0]);
-  return [
-    header.join(','),
-    ...arrayOfJSON.map((row) =>
-      header
-        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
-        .join(',')
-    ),
-  ].join('\r\n');
+
+  const flattened = arrayOfJSON
+    .filter(Boolean)
+    .map((obj) => flatten(obj, joiner));
+
+  const headerRow = flattened
+    .reduce(
+      (aggregator, row) =>
+        Object.keys(row).every(
+          (key) => (!aggregator.includes(key) && aggregator.push(key)) || true
+        ) && aggregator,
+      []
+    )
+    .filter(
+      (key) => !targetAttributes?.length || targetAttributes.includes(key)
+    );
+
+  const process = (row) => headerRow.map((attribute) => row[attribute] || '');
+
+  return [headerRow.join(','), ...flattened.map(process)].join('\r\n');
+}
+
+function flatten(obj, joiner = '.', path = []) {
+  return Object.keys(obj || {}).reduce((result, attribute) => {
+    if (typeof obj[attribute] !== 'object') {
+      result[path.concat(attribute).join(joiner)] = obj[attribute];
+      return result;
+    }
+    return Object.assign(
+      result,
+      flatten(obj[attribute], joiner, path.concat(attribute), result)
+    );
+  }, {});
 }
