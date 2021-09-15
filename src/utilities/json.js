d53cac94f1338541bff54d6aae2c1b46869ba0bc
CourtHive <charles@courthive.com>
2021-09-15 21:34:22 +0200

    add support for `columnMap` to JSON2CSV

--- src/utilities/json.js
+++ src/utilities/json.js
@@ -1,7 +1,14 @@
 import { INVALID_VALUES } from '../constants/errorConditionConstants';
 
-export function JSON2CSV(arrayOfJSON, targetAttributes, joiner = '.') {
-  if (!Array.isArray(arrayOfJSON)) return { error: INVALID_VALUES };
+export function JSON2CSV(arrayOfJSON, config) {
+  const { targetKeys = [], columnMap = {}, joiner = '.' } = config;
+  if (
+    !Array.isArray(arrayOfJSON) ||
+    !Array.isArray(targetKeys) ||
+    !Array.isArray(Object.keys(columnMap || {})) ||
+    typeof joiner !== 'string'
+  )
+    return { error: INVALID_VALUES };
 
   const flattened = arrayOfJSON
     .filter(Boolean)
@@ -15,24 +22,23 @@ export function JSON2CSV(arrayOfJSON, targetAttributes, joiner = '.') {
         ) && aggregator,
       []
     )
-    .filter(
-      (key) => !targetAttributes?.length || targetAttributes.includes(key)
-    );
+    .filter((key) => !targetKeys?.length || targetKeys.includes(key));
 
-  const process = (row) => headerRow.map((attribute) => row[attribute] || '');
+  const mappedHeaderRow = headerRow.map((key) => columnMap[key] || key);
+  const process = (row) => headerRow.map((key) => row[key] || '');
 
-  return [headerRow.join(','), ...flattened.map(process)].join('\r\n');
+  return [mappedHeaderRow.join(','), ...flattened.map(process)].join('\r\n');
 }
 
 function flatten(obj, joiner = '.', path = []) {
-  return Object.keys(obj || {}).reduce((result, attribute) => {
-    if (typeof obj[attribute] !== 'object') {
-      result[path.concat(attribute).join(joiner)] = obj[attribute];
+  return Object.keys(obj || {}).reduce((result, key) => {
+    if (typeof obj[key] !== 'object') {
+      result[path.concat(key).join(joiner)] = obj[key];
       return result;
     }
     return Object.assign(
       result,
-      flatten(obj[attribute], joiner, path.concat(attribute), result)
+      flatten(obj[key], joiner, path.concat(key), result)
     );
   }, {});
 }
