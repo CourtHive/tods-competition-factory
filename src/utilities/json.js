09962976a21e7c323a0d4a408ec01be61b5c5219
CourtHive <charles@courthive.com>
2021-09-16 10:27:14 +0200

    extend JSON2CSV; add tests;

--- src/utilities/json.js
+++ src/utilities/json.js
@@ -1,19 +1,51 @@
 import { INVALID_VALUES } from '../constants/errorConditionConstants';
 
+/**
+ *
+ * @param {object[]} arrayOfJSON - JSON objects array
+ * @param {object} config - object which configures processing (see below)
+ * @returns {string} - joined by '\r\n' or specified line separator
+ *
+ * config {
+ *  {boolean} transformAccesorFilter, // transform accessors are included with columnAccessors
+ *  {string[]} columnAccessors, // [ 'includeThis', 'andThis' ]
+ *  {object} columnTransform, // e.g. { 'newColumnName': ['oldColumn1', 'oldColumn2' ]}
+ *  {object} columnMap, // e.g. { 'columnName': 'newColumnName' }
+ *  {string} columnJoiner, // defaults to ',' // defines how CSV columns are joined
+ *  {string} rowJoiner, // defaults to '\r\n' // defines how CSV lines are joined
+ *  {string} keyJoiner, // defaults to '.' // defines how flattened column names are constructed
+ * }
+ *
+ * NOTE: `columnMap` should not contain newColumnName(s) that are `columnTransform` keys
+ * NOTE: `columnTransform` mapped array elements are sensitive to order and will resolve to the first matching value
+ */
 export function JSON2CSV(arrayOfJSON, config) {
   if (config && typeof config !== 'object') return { error: INVALID_VALUES };
-  const { targetKeys = [], columnMap = {}, joiner = '.' } = config || {};
+  const {
+    transformAccesorFilter,
+    columnAccessors = [],
+    columnTransform = {},
+    columnMap = {},
+
+    columnJoiner = ',',
+    rowJoiner = '\r\n',
+    keyJoiner = '.',
+  } = config || {};
+
   if (
     !Array.isArray(arrayOfJSON) ||
-    !Array.isArray(targetKeys) ||
+    !Array.isArray(columnAccessors) ||
     !Array.isArray(Object.keys(columnMap || {})) ||
-    typeof joiner !== 'string'
+    typeof keyJoiner !== 'string'
   )
     return { error: INVALID_VALUES };
 
   const flattened = arrayOfJSON
     .filter(Boolean)
-    .map((obj) => flatten(obj, joiner));
+    .map((obj) => flatten(obj, keyJoiner));
+
+  const transformColumns = Object.values(columnTransform).flat();
+  if (transformAccesorFilter) columnAccessors.push(...transformColumns);
 
   const headerRow = flattened
     .reduce(
@@ -23,23 +55,72 @@ export function JSON2CSV(arrayOfJSON, config) {
         ) && aggregator,
       []
     )
-    .filter((key) => !targetKeys?.length || targetKeys.includes(key));
+    .filter((key) => !columnAccessors?.length || columnAccessors.includes(key));
 
-  const mappedHeaderRow = headerRow.map((key) => columnMap[key] || key);
-  const process = (row) => headerRow.map((key) => row[key] || '');
+  const accessorMap = Object.assign(
+    {},
+    ...Object.keys(columnTransform)
+      .map((transform) =>
+        columnTransform[transform]
+          .map((value) => ({ [value]: transform }))
+          .flat()
+      )
+      .flat()
+  );
 
-  return [mappedHeaderRow.join(','), ...flattened.map(process)].join('\r\n');
+  const tranformedHeaderRow = headerRow.reduce((def, key) => {
+    const transform = accessorMap[key];
+    if (transform) {
+      if (!def.includes(transform)) def.push(transform);
+    } else {
+      def.push(key);
+    }
+    return def;
+  }, []);
+
+  Object.keys(columnMap).forEach(
+    (columnName) =>
+      !tranformedHeaderRow.includes(columnName) &&
+      tranformedHeaderRow.unshift(columnName)
+  );
+
+  Object.keys(columnTransform).forEach(
+    (columnName) =>
+      !tranformedHeaderRow.includes(columnName) &&
+      tranformedHeaderRow.unshift(columnName)
+  );
+
+  const mappedHeaderRow = tranformedHeaderRow.map(
+    (key) => columnMap[key] || key
+  );
+  const processRow = (row) => {
+    const valueMap = Object.values(
+      tranformedHeaderRow.reduce((valueMap, key) => {
+        const accessors = columnTransform[key];
+        valueMap[key] =
+          (accessors?.length
+            ? row[accessors.find((accessor) => row[accessor])]
+            : row[key]) || '';
+
+        return valueMap;
+      }, {})
+    );
+    return valueMap;
+  };
+  const rows = flattened.map(processRow);
+
+  return [mappedHeaderRow.join(columnJoiner), ...rows].join(rowJoiner);
 }
 
-function flatten(obj, joiner = '.', path = []) {
+function flatten(obj, keyJoiner = '.', path = []) {
   return Object.keys(obj || {}).reduce((result, key) => {
     if (typeof obj[key] !== 'object') {
-      result[path.concat(key).join(joiner)] = obj[key];
+      result[path.concat(key).join(keyJoiner)] = obj[key];
       return result;
     }
     return Object.assign(
       result,
-      flatten(obj[key], joiner, path.concat(key), result)
+      flatten(obj[key], keyJoiner, path.concat(key), result)
     );
   }, {});
 }
