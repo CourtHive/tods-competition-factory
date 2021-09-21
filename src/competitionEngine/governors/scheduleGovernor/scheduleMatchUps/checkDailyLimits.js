a773c887a11c840be7015f1b74de976d2387e7ac
CourtHive <charles@courthive.com>
2021-09-21 11:25:01 +0200

    add default values to avoid errors

--- src/competitionEngine/governors/scheduleGovernor/scheduleMatchUps/checkDailyLimits.js
+++ src/competitionEngine/governors/scheduleGovernor/scheduleMatchUps/checkDailyLimits.js
@@ -16,7 +16,7 @@ import { TOTAL } from '../../../../constants/scheduleConstants';
 export function checkDailyLimits(
   individualParticipantProfiles,
   matchUpPotentialParticipantIds,
-  matchUpDailyLimits,
+  matchUpDailyLimits = {},
   scheduleDate,
   matchUp
 ) {
@@ -41,7 +41,7 @@ export function checkDailyLimits(
         const limitReached = [matchUpType, TOTAL].find((counterName) => {
           const participantCount =
             (profile.counters && profile.counters[counterName]) || 0;
-          const dailyLimit = matchUpDailyLimits[counterName];
+          const dailyLimit = matchUpDailyLimits[counterName] || 0;
           return (
             participantCount && dailyLimit && participantCount >= dailyLimit
           );
