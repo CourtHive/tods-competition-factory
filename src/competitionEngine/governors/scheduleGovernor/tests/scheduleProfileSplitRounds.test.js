d9e72498f2f71997e9d2ba787c89ef46452ca4d2
CourtHive <charles@courthive.com>
2021-09-22 09:14:10 +0200

    add test for splitting rounds across venues

---
+++ src/competitionEngine/governors/scheduleGovernor/tests/scheduleProfileSplitRounds.test.js
@@ -0,0 +1,205 @@
+import { visualizeScheduledMatchUps } from '../../../../global/testHarness/testUtilities/visualizeScheduledMatchUps';
+import tournamentEngine from '../../../../tournamentEngine/sync';
+import { hasSchedule } from '../scheduleMatchUps/hasSchedule';
+import mocksEngine from '../../../../mocksEngine';
+
+import POLICY_SCHEDULING_NO_DAILY_LIMITS from '../../../../fixtures/policies/POLICY_SCHEDULING_NO_DAILY_LIMITS';
+import { APPLIED_POLICIES } from '../../../../constants/extensionConstants';
+
+const showGlobalLog = false;
+
+it('Can split rounds across multiple venues', () => {
+  const startDate = '2022-01-01';
+  const endDate = '2022-01-07';
+  const firstVenueId = 'first';
+  const secondVenueId = 'second';
+  const venueProfiles = [
+    {
+      venueId: firstVenueId,
+      venueName: 'venue 1',
+      startTime: '08:00',
+      endTime: '20:00',
+      courtsCount: 4,
+    },
+    {
+      venueId: secondVenueId,
+      venueName: 'venue 2',
+      startTime: '08:00',
+      endTime: '20:00',
+      courtsCount: 2,
+    },
+  ];
+
+  const drawProfiles = [
+    {
+      drawName: 'U16 Boys Singles',
+      participantsCount: 32,
+      idPrefix: 'M16',
+      drawId: 'idM16',
+      drawSize: 32,
+    },
+  ];
+  const schedulingProfile = [
+    {
+      scheduleDate: startDate,
+      venues: [
+        {
+          venueId: firstVenueId,
+          rounds: [
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-16',
+              roundSegment: { segmentNumber: 1, segmentsCount: 2 },
+            },
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-8',
+              roundSegment: { segmentNumber: 1, segmentsCount: 2 },
+            },
+          ],
+        },
+        {
+          venueId: secondVenueId,
+          rounds: [
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-16',
+              roundSegment: { segmentNumber: 2, segmentsCount: 2 },
+            },
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-8',
+              roundSegment: { segmentNumber: 2, segmentsCount: 2 },
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  let result = mocksEngine.generateTournamentRecord({
+    policyDefinitions: POLICY_SCHEDULING_NO_DAILY_LIMITS,
+    autoSchedule: true,
+    schedulingProfile,
+    venueProfiles,
+    drawProfiles,
+    startDate,
+    endDate,
+  });
+
+  tournamentEngine.setState(result.tournamentRecord);
+
+  const { extension } = tournamentEngine.findTournamentExtension({
+    name: APPLIED_POLICIES,
+  });
+  expect(extension.name).toEqual(APPLIED_POLICIES);
+
+  const { schedulingProfile: attachedSchedulingProfile } =
+    tournamentEngine.getSchedulingProfile();
+
+  expect(attachedSchedulingProfile[0].venues[0].rounds[0].roundNumber).toEqual(
+    1
+  );
+  expect(attachedSchedulingProfile[0].venues[1].rounds[0].roundNumber).toEqual(
+    1
+  );
+  expect(
+    attachedSchedulingProfile[0].venues[0].rounds[0].roundSegment.segmentNumber
+  ).toEqual(1);
+  expect(
+    attachedSchedulingProfile[0].venues[1].rounds[0].roundSegment.segmentNumber
+  ).toEqual(2);
+
+  const { matchUps } = tournamentEngine.allTournamentMatchUps();
+  const scheduledMatchUps = matchUps.filter(hasSchedule);
+
+  visualizeScheduledMatchUps({ scheduledMatchUps, showGlobalLog });
+});
+
+it('Can split rounds across multiple venues', () => {
+  const startDate = '2022-01-01';
+  const endDate = '2022-01-07';
+  const firstVenueId = 'first';
+  const secondVenueId = 'second';
+  const venueProfiles = [
+    {
+      venueId: firstVenueId,
+      venueName: 'venue 1',
+      startTime: '08:00',
+      endTime: '20:00',
+      courtsCount: 4,
+    },
+    {
+      venueId: secondVenueId,
+      venueName: 'venue 2',
+      startTime: '08:00',
+      endTime: '20:00',
+      courtsCount: 2,
+    },
+  ];
+
+  const drawProfiles = [
+    {
+      drawName: 'U16 Boys Singles',
+      participantsCount: 32,
+      idPrefix: 'M16',
+      drawId: 'idM16',
+      drawSize: 32,
+    },
+  ];
+  const schedulingProfile = [
+    {
+      scheduleDate: startDate,
+      venues: [
+        {
+          venueId: firstVenueId,
+          rounds: [
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-16',
+            },
+          ],
+        },
+        {
+          venueId: secondVenueId,
+          rounds: [
+            {
+              drawId: 'idM16',
+              winnerFinishingPositionRange: '1-8',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  let result = mocksEngine.generateTournamentRecord({
+    policyDefinitions: POLICY_SCHEDULING_NO_DAILY_LIMITS,
+    autoSchedule: true,
+    schedulingProfile,
+    venueProfiles,
+    drawProfiles,
+    startDate,
+    endDate,
+  });
+
+  tournamentEngine.setState(result.tournamentRecord);
+
+  const { matchUps } = tournamentEngine.allTournamentMatchUps();
+  const scheduledMatchUps = matchUps.filter(hasSchedule);
+
+  visualizeScheduledMatchUps({ scheduledMatchUps, showGlobalLog });
+
+  expect(
+    Object.keys(result.schedulerResult.matchUpScheduleTimes).length
+  ).toEqual(24);
+  expect(
+    result.schedulerResult.skippedScheduleTimes[startDate].second.length
+  ).toEqual(0);
+  expect(
+    result.schedulerResult.scheduleTimesRemaining[startDate].first.length
+  ).toEqual(14);
+  expect(
+    result.schedulerResult.scheduleTimesRemaining[startDate].second.length
+  ).toEqual(7);
+});
