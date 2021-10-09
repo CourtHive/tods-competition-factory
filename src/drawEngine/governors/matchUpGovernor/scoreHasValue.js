840931bd4755db453d0dba0e1b132c0177b6b210
CourtHive <charles@courthive.com>
2021-10-09 17:20:48 +0200

    refactor

--- src/drawEngine/governors/matchUpGovernor/scoreHasValue.js
+++ src/drawEngine/governors/matchUpGovernor/scoreHasValue.js
@@ -1,6 +1,10 @@
-export function scoreHasValue({ score }) {
-  if (!score) return false;
+export function scoreHasValue({ score } = {}) {
   const { sets, scoreStringSide1, scoreStringSide2 } = score || {};
-  if (scoreStringSide1 || scoreStringSide2) return true;
-  return sets?.length ? Object.values(sets[0]).some((value) => value) : false;
+  return !score
+    ? false
+    : scoreStringSide1 || scoreStringSide2
+    ? true
+    : sets?.length
+    ? Object.values(sets[0]).some((value) => value)
+    : false;
 }
