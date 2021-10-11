e23ec04957f6e1818debb58f72eb21b91a6ef13e
CourtHive <charles@courthive.com>
2021-10-11 12:08:14 +0200

    add logic to account for derived pair participants in team events to `deleteParticipants` integrity checks

--- src/tournamentEngine/governors/participantGovernor/deleteParticipants.js
+++ src/tournamentEngine/governors/participantGovernor/deleteParticipants.js
@@ -1,21 +1,28 @@
 import { getTournamentParticipants } from '../../getters/participants/getTournamentParticipants';
 import { removeParticipantIdsFromAllTeams } from './groupings/removeIndividualParticipantIds';
+import { allTournamentMatchUps } from '../../getters/matchUpsGetter';
 import { addNotice } from '../../../global/globalState';
+import { intersection } from '../../../utilities';
 
+import { DELETE_PARTICIPANTS } from '../../../constants/topicConstants';
+import { SUCCESS } from '../../../constants/resultConstants';
+import { DOUBLES } from '../../../constants/matchUpTypes';
 import {
   CANNOT_REMOVE_PARTICIPANTS,
   MISSING_PARTICIPANT_IDS,
   MISSING_TOURNAMENT_RECORD,
   PARTICIPANT_ASSIGNED_DRAW_POSITION,
 } from '../../../constants/errorConditionConstants';
-import { SUCCESS } from '../../../constants/resultConstants';
-import { DELETE_PARTICIPANTS } from '../../../constants/topicConstants';
 
-export function deleteParticipants({ tournamentRecord, participantIds }) {
+export function deleteParticipants({
+  teamDrawIds = [],
+  tournamentRecord,
+  participantIds,
+}) {
   if (!tournamentRecord) return { error: MISSING_TOURNAMENT_RECORD };
   if (!participantIds?.length) return { error: MISSING_PARTICIPANT_IDS };
-  const participantsCount = tournamentRecord.participants?.length || 0;
 
+  const participantsCount = tournamentRecord.participants?.length || 0;
   if (!participantsCount) return SUCCESS;
 
   const { tournamentParticipants } = getTournamentParticipants({
@@ -24,10 +31,28 @@ export function deleteParticipants({ tournamentRecord, participantIds }) {
     tournamentRecord,
   });
 
+  const getPlacedParticipantIds = () => {
+    const { matchUps } = allTournamentMatchUps({
+      tournamentRecord,
+      matchUpFilters: { drawIds: teamDrawIds, matchUpTypes: [DOUBLES] },
+    });
+    const placedParticipantIds = matchUps
+      .map(({ sides }) => sides.map(({ participantId }) => participantId))
+      .flat()
+      .filter(Boolean);
+    return intersection(placedParticipantIds, participantIds);
+  };
+
+  // for team draws it is necessary to check matchUps for pair participantIds "discovered" in collectionAssignments
+  const placedParticipantIds = teamDrawIds.length && getPlacedParticipantIds();
+
   const participantsInDraws = tournamentParticipants.filter(
-    (participant) => participant.draws?.length
+    (participant) =>
+      participant.draws?.filter(
+        ({ drawId }) => !teamDrawIds.length || !teamDrawIds.includes(drawId)
+      ).length
   );
-  if (participantsInDraws.length)
+  if (placedParticipantIds.length || participantsInDraws.length)
     return { error: PARTICIPANT_ASSIGNED_DRAW_POSITION };
 
   // If not active in draws, remove participantIds from all entries
