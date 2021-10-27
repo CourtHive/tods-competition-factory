a6f748b479219376fb3207856e6480a1fb717f4b
CourtHive <charles@courthive.com>
2021-10-27 16:09:22 +0200

    update mock teams

--- src/mocksEngine/utilities/teamMocks.js
+++ src/mocksEngine/utilities/teamMocks.js
@@ -1,8 +1,13 @@
-import { shuffleArray } from '../../utilities';
+import { generateRange, shuffleArray } from '../../utilities';
 import teamsData from '../data/teams.json';
 
 export function teamMocks({ count = 1 } = {}) {
-  const shuffledCities = shuffleArray(teamsData);
-  const teams = shuffledCities.slice(0, count);
+  const shuffledTeamNames = shuffleArray(teamsData);
+  const teams = shuffledTeamNames.slice(0, count);
+  if (teams.length < count) {
+    generateRange(0, count - teams.length).forEach((i) =>
+      teams.push(`Team ${i + 1}`)
+    );
+  }
   return { teams };
 }
