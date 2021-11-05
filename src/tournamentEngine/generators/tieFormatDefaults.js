d65644d765ac5e97b3f40599ebc1422e63b45838
CourtHive <charles@courthive.com>
2021-11-05 11:59:20 +0100

    COLLEGE_DEFAULT tieFormat update

--- src/tournamentEngine/generators/tieFormatDefaults.js
+++ src/tournamentEngine/generators/tieFormatDefaults.js
@@ -45,7 +45,7 @@ const namedFormats = {
     singles: {
       matchUpCount: 6,
       matchUpValue: 1,
-      matchUpFormat: 'SET3-S:6NOAD/TB7',
+      matchUpFormat: 'SET3-S:6/TB7',
     },
     tieFormatName: 'COLLEGE_DEFAULT',
     valueGoal: 4,
