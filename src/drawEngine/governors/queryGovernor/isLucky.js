81f99a705a65a208f65a5b3f3c14c43838267d27
CourtHive <charles@courthive.com>
2021-11-17 10:58:24 +0100

    add `isLucky` drawType analysis

---
+++ src/drawEngine/governors/queryGovernor/isLucky.js
@@ -0,0 +1,32 @@
+import { getRoundMatchUps } from '../../accessors/matchUpAccessor/getRoundMatchUps';
+import { isPowerOf2 } from '../../../utilities';
+
+import { LUCKY_DRAW } from '../../../constants/drawDefinitionConstants';
+
+export function isLucky({ drawDefinition, structure }) {
+  if (!structure) return false;
+
+  const { roundProfile, roundMatchUps } = getRoundMatchUps({
+    matchUps: structure.matchUps || [],
+  });
+
+  const hasFirstRoundDrawPositions = !!roundMatchUps[1].find(
+    ({ drawPositions }) => drawPositions
+  );
+
+  const noSecondRoundDrawPositions = !roundMatchUps[2].find(
+    ({ drawPositions }) => drawPositions
+  );
+
+  const hasOddMatchUpsCount = Object.values(roundProfile).find(
+    ({ matchUpsCount }) => !isPowerOf2(matchUpsCount)
+  );
+
+  return (
+    hasOddMatchUpsCount &&
+    !structure?.structures &&
+    hasFirstRoundDrawPositions &&
+    noSecondRoundDrawPositions &&
+    !(drawDefinition?.drawType && drawDefinition.drawType !== LUCKY_DRAW)
+  );
+}
