007d6d026653ef91e780b33635b93ab66b141bbb
CourtHive <charles@courthive.com>
2021-11-23 16:19:50 +0100

    add chained conditionals to avoid team RR error when drawSize 3

---
+++ src/tournamentEngine/tests/team/teamRoundRobin.test.js
@@ -0,0 +1,20 @@
+import tournamentEngine from '../../sync';
+import { mocksEngine } from '../../..';
+
+import { ROUND_ROBIN } from '../../../constants/drawDefinitionConstants';
+import { TEAM } from '../../../constants/eventConstants';
+
+// this test originated because an error was being thrown with this scenario
+test('team ROUND_robin with 3 teams', () => {
+  const drawProfiles = [
+    { drawSize: 3, drawType: ROUND_ROBIN, eventType: TEAM },
+  ];
+
+  const { tournamentRecord } = mocksEngine.generateTournamentRecord({
+    drawProfiles,
+  });
+
+  tournamentEngine.setState(tournamentRecord);
+
+  expect(tournamentRecord).not.toBeUndefined();
+});
