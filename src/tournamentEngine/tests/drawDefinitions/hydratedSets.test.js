38110ba6c30be9a66add5fe686d927058317539c
CourtHive <charles@courthive.com>
2021-12-01 14:10:29 +0100

    extend tests

---
+++ src/tournamentEngine/tests/drawDefinitions/hydratedSets.test.js
@@ -0,0 +1,18 @@
+import { mocksEngine } from '../../..';
+import tournamentEngine from '../../sync';
+
+test('inContext matchUp score.sets include tiebreakSet boolean', () => {
+  const mockProfile = {
+    drawProfiles: [{ drawSize: 4, matchUpFormat: 'SET3-S:T20' }],
+    completeAllMatchUps: true,
+  };
+  const { tournamentRecord } =
+    mocksEngine.generateTournamentRecord(mockProfile);
+
+  tournamentEngine.setState(tournamentRecord);
+
+  const { matchUps } = tournamentEngine.allTournamentMatchUps();
+  matchUps[0].score.sets.forEach((set) =>
+    expect(set.tiebreakSet).toEqual(true)
+  );
+});
