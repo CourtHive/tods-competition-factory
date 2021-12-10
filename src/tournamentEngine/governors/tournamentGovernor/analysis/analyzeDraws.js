fc61ba147d366b4da5d1b008d67c4c9c9915e497
CourtHive <charles@courthive.com>
2021-12-10 14:27:54 -0500

    extend `analyzeDraws` method

--- src/tournamentEngine/governors/tournamentGovernor/analysis/analyzeDraws.js
+++ src/tournamentEngine/governors/tournamentGovernor/analysis/analyzeDraws.js
@@ -4,14 +4,16 @@ import { getPositionAssignments } from '../../../../drawEngine/getters/positions
 import { stageOrder } from '../../../../constants/drawDefinitionConstants';
 
 import { SUCCESS } from '../../../../constants/resultConstants';
+import { getStructureLinks } from '../../../../drawEngine/getters/linkGetter';
 
-export function analyzeDraws(tournamentRecord) {
+export function analyzeDraws({ tournamentRecord }) {
   const drawsAnalysis = {
-    noPositionsAssigned: 0, // no positions assigned
-    positionsNoOutcomes: 0, // all positions assigned and no outcomes
-    matchPlayDraws: 0, // only first round has active matchUps; some unassigned positions
-    canBePruned: 0, // partially assigned positions with outcomes => drawSizes can be reduced
-    draws: [],
+    positionsNoOutcomes: [], // all positions assigned and no outcomes
+    canBePruned: [], // partially assigned positions with outcomes => drawSizes can be reduced
+    matchPlay: [], // only first round has active matchUps; some unassigned positions
+    inactive: [],
+
+    drawAnalysis: {},
   };
 
   const drawDefinitions = tournamentRecord.events
@@ -20,18 +22,29 @@ export function analyzeDraws(tournamentRecord) {
     .filter(Boolean);
 
   drawDefinitions.forEach((drawDefinition) => {
+    let positionsAssignedCount = 0;
+    let matchUpsWithWinningSideCount = 0;
     const structures = drawDefinition?.structures || [];
-    const structureData = structures.map((structure) => {
+    const structuresData = structures.map((structure) => {
       const { stage, stageSequence, structureId } = structure;
       const orderNumber = stageOrder[stage];
       const { inContextStructureMatchUps } = structureActiveDrawPositions({
         drawDefinition,
         structure,
       });
+      const winningSideCount =
+        inContextStructureMatchUps
+          ?.filter(({ winningSide }) => winningSide)
+          .filter(Boolean).length || 0;
+
+      matchUpsWithWinningSideCount += winningSideCount;
+
       const { positionAssignments } = getPositionAssignments({ structure });
       const positionsAssigned = positionAssignments?.filter(
         ({ participantId }) => participantId
       );
+      positionsAssignedCount += positionsAssigned.length;
+
       const unassignedPositionsCount =
         (positionAssignments?.length || 0) - (positionsAssigned?.length || 0);
 
@@ -40,6 +53,9 @@ export function analyzeDraws(tournamentRecord) {
 
       const activeRounds = Object.keys(roundProfile).filter(
         (roundNumber) => !roundProfile[roundNumber].inactiveRound
+      );
+      const inactiveRounds = Object.keys(roundProfile).filter(
+        (roundNumber) => roundProfile[roundNumber].inactiveRound
       );
       const inactiveStructure = Object.values(roundProfile).every(
         (profile) => profile.inactiveRound
@@ -50,6 +66,7 @@ export function analyzeDraws(tournamentRecord) {
         unassignedPositionsCount,
         inactiveStructure,
         maxMatchUpsCount,
+        inactiveRounds,
         roundMatchUps,
         activeRounds,
         roundNumbers,
@@ -62,25 +79,52 @@ export function analyzeDraws(tournamentRecord) {
       };
     });
 
-    const mainStructure = structureData.find(
+    const mainStructure = structuresData.find(
       (data) => data.orderNumber === 2 && data.stageSequence === 1
     );
+
+    const activeStructuresCount = structuresData.filter(
+      ({ inactiveStructure }) => !inactiveStructure
+    ).length;
+
+    const { links } = getStructureLinks({
+      drawDefinition,
+      structureId: mainStructure.structureId,
+    });
+
     const isMatchPlay =
+      parseInt(mainStructure.activeRounds[0]) === 1 &&
       mainStructure.activeRounds.length === 1 &&
-      parseInt(mainStructure.activeRounds[0]) === 1;
+      activeStructuresCount === 1;
+
+    const inactiveDraw = structuresData?.every(
+      ({ inactiveStructure }) => inactiveStructure
+    );
 
     const canBePruned =
+      !links.length &&
       mainStructure.activeRounds.length &&
-      mainStructure.roundProfile[1].inActiveCount;
+      (mainStructure.roundProfile[1].inactiveCount ||
+        mainStructure.inactiveRounds.length);
 
-    if (isMatchPlay) drawsAnalysis.matchPlayDraws += 1;
-    if (canBePruned) drawsAnalysis.canBePruned += 1;
+    const drawId = drawDefinition.drawId;
 
-    drawsAnalysis.draws.push({
-      drawId: drawDefinition.drawId,
-      structureData,
+    if (positionsAssignedCount && !matchUpsWithWinningSideCount)
+      drawsAnalysis.positionsNoOutcomes.push(drawId);
+    if (inactiveDraw) drawsAnalysis.inactive.push(drawId);
+    if (isMatchPlay) drawsAnalysis.matchPlay.push(drawId);
+    if (canBePruned) drawsAnalysis.canBePruned.push(drawId);
+
+    const drawAnalysis = {
+      matchUpsWithWinningSideCount,
+      positionsAssignedCount,
+      structuresData,
+      inactiveDraw,
       isMatchPlay,
-    });
+      drawId,
+    };
+
+    drawsAnalysis.drawAnalysis[drawId] = drawAnalysis;
   });
 
   return { ...SUCCESS, drawsAnalysis };
---
+++ src/tournamentEngine/tests/drawDefinitions/analyzeDraws.test.js
@@ -0,0 +1,96 @@
+import { getDrawStructures } from '../../../drawEngine/getters/findStructure';
+import tournamentEngine from '../../sync';
+import { drawEngine, mocksEngine } from '../../..';
+
+import { MAIN } from '../../../constants/drawDefinitionConstants';
+import { STRUCTURE_SELECTED_STATUSES } from '../../../constants/entryStatusConstants';
+
+test('draw analysis can determine when draws are able to be pruned', () => {
+  const {
+    drawIds: [drawId],
+    tournamentRecord,
+  } = mocksEngine.generateTournamentRecord({
+    // automated false: don't place the participantIds
+    drawProfiles: [
+      {
+        drawSize: 32,
+        participantsCount: 20,
+        automated: false,
+      },
+    ],
+  });
+
+  tournamentEngine.setState(tournamentRecord);
+
+  const { drawDefinition } = tournamentEngine.getEvent({ drawId });
+  const {
+    structures: [mainStructure],
+  } = getDrawStructures({
+    drawDefinition,
+    stage: MAIN,
+    stageSequence: 1,
+  });
+
+  const placedParticipantIds = mainStructure.positionAssignments
+    .map(({ participantId }) => participantId)
+    .filter(Boolean);
+  expect(placedParticipantIds.length).toEqual(0);
+
+  let result = tournamentEngine.analyzeDraws();
+  expect(result.drawsAnalysis.inactive.includes(drawId)).toEqual(true);
+  expect(result.drawsAnalysis.positionsNoOutcomes).toEqual([]);
+  expect(result.drawsAnalysis.canBePruned).toEqual([]);
+  expect(result.drawsAnalysis.matchPlay).toEqual([]);
+
+  const structureSelectedParticipantIds = drawDefinition.entries
+    .filter((entry) => STRUCTURE_SELECTED_STATUSES.includes(entry.entryStatus))
+    .map(({ participantId }) => participantId);
+
+  structureSelectedParticipantIds.forEach((participantId, index) => {
+    const drawPosition = index + 1;
+    tournamentEngine.assignDrawPosition({
+      structureId: mainStructure.structureId,
+      participantId,
+      drawPosition,
+      drawId,
+    });
+  });
+
+  result = tournamentEngine.analyzeDraws();
+  expect(result.drawsAnalysis.positionsNoOutcomes.includes(drawId)).toEqual(
+    true
+  );
+  expect(result.drawsAnalysis.inactive.includes(drawId)).toEqual(true);
+  expect(result.drawsAnalysis.canBePruned).toEqual([]);
+  expect(result.drawsAnalysis.matchPlay).toEqual([]);
+
+  const { matchUps } = tournamentEngine.allTournamentMatchUps();
+
+  const { roundMatchUps } = drawEngine.getRoundMatchUps({ matchUps });
+
+  let { outcome } = mocksEngine.generateOutcome();
+  result = tournamentEngine.setMatchUpStatus({
+    matchUpId: roundMatchUps[1][0].matchUpId,
+    outcome,
+    drawId,
+  });
+  expect(result.success).toEqual(true);
+
+  result = tournamentEngine.analyzeDraws();
+  expect(result.drawsAnalysis.canBePruned.includes(drawId)).toEqual(true);
+  expect(result.drawsAnalysis.matchPlay.includes(drawId)).toEqual(true);
+  expect(result.drawsAnalysis.positionsNoOutcomes).toEqual([]);
+  expect(result.drawsAnalysis.inactive).toEqual([]);
+
+  expect(result.drawsAnalysis.drawAnalysis[drawId].inactiveDraw).toEqual(false);
+  expect(result.drawsAnalysis.drawAnalysis[drawId].isMatchPlay).toEqual(true);
+  expect(
+    result.drawsAnalysis.drawAnalysis[drawId].matchUpsWithWinningSideCount
+  ).toEqual(1);
+
+  const structureData =
+    result.drawsAnalysis.drawAnalysis[drawId].structuresData[0];
+  expect(structureData.unassignedPositionsCount).toEqual(12);
+  expect(structureData.inactiveRounds.length).toEqual(4);
+  expect(structureData.activeRounds.length).toEqual(1);
+});
