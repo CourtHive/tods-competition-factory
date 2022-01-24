260505b6df1b17ef0e0267eaa64abbcae8888e68
CourtHive <charles@courthive.com>
2022-01-24 10:45:12 -0500

    cleanup

--- src/drawEngine/tests/integration/fmlcWinningSide.test.js
+++ src/drawEngine/tests/integration/fmlcWinningSide.test.js
@@ -1,6 +1,9 @@
 import { mocksEngine, tournamentEngine } from '../../..';
 
-import { FIRST_MATCH_LOSER_CONSOLATION } from '../../../constants/drawDefinitionConstants';
+import {
+  FIRST_MATCH_LOSER_CONSOLATION,
+  MAIN,
+} from '../../../constants/drawDefinitionConstants';
 
 it('can complete FMLC consolation with BYE before main', () => {
   const winningSide = 1;
@@ -45,52 +48,9 @@ it('can complete FMLC consolation with BYE before main', () => {
   expect(completedMatchUps.length).toEqual(15);
   expect(upcomingMatchUps.length).toEqual(4);
 
-  const matchUpIds = upcomingMatchUps.map(({ matchUpId }) => matchUpId);
-
-  let result = tournamentEngine.setMatchUpStatus({
-    outcome: { winningSide: 2 },
-    matchUpId: matchUpIds[0],
-    drawId,
-  });
-  expect(result.success).toEqual(true);
-});
-
-it('can complete FMLC consolation with WOWO before main', () => {
-  const winningSide = 1;
-  const roundNumber = 1;
-  const drawProfiles = [
-    {
-      drawSize: 16,
-      drawType: FIRST_MATCH_LOSER_CONSOLATION,
-      outcomes: [
-        { roundNumber, roundPosition: 1, winningSide },
-        { roundNumber, roundPosition: 2, winningSide },
-        { roundNumber, roundPosition: 3, winningSide },
-        { roundNumber, roundPosition: 4, winningSide },
-        { roundNumber, roundPosition: 5, winningSide },
-        { roundNumber, roundPosition: 6, winningSide },
-        { roundNumber, roundPosition: 7, winningSide },
-        { roundNumber, roundPosition: 8, winningSide },
-      ],
-    },
-  ];
-
-  const {
-    drawIds: [drawId],
-    tournamentRecord,
-  } = mocksEngine.generateTournamentRecord({ drawProfiles });
-
-  tournamentEngine.setState(tournamentRecord);
-
-  const { drawDefinition } = tournamentEngine.getEvent({ drawId });
-  expect(drawDefinition.links.length).toEqual(2);
-
-  const { completedMatchUps, upcomingMatchUps } =
-    tournamentEngine.tournamentMatchUps();
-  expect(completedMatchUps.length).toEqual(8);
-  expect(upcomingMatchUps.length).toEqual(8);
-
-  const matchUpIds = upcomingMatchUps.map(({ matchUpId }) => matchUpId);
+  const matchUpIds = upcomingMatchUps
+    .filter((matchUp) => matchUp.stage === MAIN)
+    .map(({ matchUpId }) => matchUpId);
 
   let result = tournamentEngine.setMatchUpStatus({
     outcome: { winningSide: 2 },
