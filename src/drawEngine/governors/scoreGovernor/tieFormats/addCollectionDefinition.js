cf7b6d58fcffdcfd0d8d113e83b6bc226b7945d3
CourtHive <charles@courthive.com>
2022-02-17 08:54:27 -0500

    WIP dynamic tieFormats

---
+++ src/drawEngine/governors/scoreGovernor/tieFormats/addCollectionDefinition.js
@@ -0,0 +1,67 @@
+import { findMatchUp } from '../../../getters/getMatchUps/findMatchUp';
+import { findStructure } from '../../../getters/findStructure';
+import { UUID } from '../../../../utilities';
+import {
+  validateCollectionDefinition,
+  validateTieFormat,
+} from './tieFormatUtilities';
+
+import {
+  DUPLICATE_VALUE,
+  INVALID_VALUES,
+} from '../../../../constants/errorConditionConstants';
+
+/*
+ * collectionDefinition will be added to an event tieFormat (if present)
+ * if a matchUpId is provided, will be added to matchUp.tieFormat
+ * if a structureId is provided, will be added to structure.tieFormat
+ * TODO: determine whether all contained instances of tieFormat should be updated
+ */
+export function addCollectionDefinition({
+  collectionDefinition,
+  drawDefinition,
+  structureId,
+  matchUpId,
+  event,
+}) {
+  const { valid, errors } = validateCollectionDefinition({
+    collectionDefinition,
+  });
+  if (!valid) return { error: INVALID_VALUES, errors };
+
+  let tieFormat;
+
+  if (event?.tieFormat) {
+    tieFormat = event.tieFormat;
+  } else if (matchUpId) {
+    const { error, matchUp, structure } = findMatchUp({
+      drawDefinition,
+      matchUpId,
+    });
+    if (error) return { error };
+
+    tieFormat =
+      matchUp.tieFormat ||
+      (structureId && structure.tieFormat) ||
+      drawDefinition.tieFormat;
+  } else if (structureId) {
+    const { structure } = findStructure({ drawDefinition, structureId });
+    tieFormat = structure.tieFormat;
+  }
+
+  const result = validateTieFormat({ tieFormat });
+  if (!result.valid) return { error: INVALID_VALUES, errors: result.errors };
+
+  if (!collectionDefinition.collectionId) {
+    collectionDefinition.collectionId = UUID();
+  } else {
+    const collectionIds = tieFormat.collectionDefinitions.map(
+      ({ collectionId }) => collectionId
+    );
+    if (collectionIds.includes(collectionDefinition.collectionId))
+      return {
+        error: DUPLICATE_VALUE,
+        collectionId: collectionDefinition.collectionId,
+      };
+  }
+}
