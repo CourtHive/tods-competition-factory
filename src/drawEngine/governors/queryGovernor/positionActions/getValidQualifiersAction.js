24ef047078dcd88fe5172dd22af2267eb1453da1
CourtHive <charles@courthive.com>
2022-03-15 12:59:22 -0400

    rough in valid qualifiers action

---
+++ src/drawEngine/governors/queryGovernor/positionActions/getValidQualifiersAction.js
@@ -0,0 +1,20 @@
+export function getValidQualifiersAction() {
+  /*
+  {
+  positionSourceStructureIds,
+  isWinRatioFedStructure,
+  tournamentParticipants,
+  isQualifierPosition,
+  positionAssignments,
+  activeDrawPositions,
+  policyDefinitions,
+  drawDefinition,
+  drawPosition,
+  structureId,
+  structure,
+  }
+  */
+  const validAssignmentActions = [];
+
+  return { validAssignmentActions };
+}
