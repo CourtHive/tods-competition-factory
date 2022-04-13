6f8ecae3dd41b088b8838b8805ddce9241e31114
CourtHive <charles@courthive.com>
2022-04-13 12:30:37 -0400

    add `voluntaryConsolation` param to `generateDrawDefinition`

--- src/tournamentEngine/generators/generateDrawDefinition.js
+++ src/tournamentEngine/generators/generateDrawDefinition.js
@@ -1,3 +1,4 @@
+import { addVoluntaryConsolationStructure } from '../governors/eventGovernor/generateVoluntaryConsolationStructure';
 import { generateDrawType } from '../../drawEngine/governors/structureGovernor/generateDrawType';
 import { getTournamentParticipants } from '../getters/participants/getTournamentParticipants';
 import { setMatchUpFormat } from '../../drawEngine/governors/matchUpGovernor/matchUpFormat';
@@ -32,6 +33,7 @@ export function generateDrawDefinition(params) {
   const {
     drawType = SINGLE_ELIMINATION,
     ignoreAllowedDrawTypes,
+    voluntaryConsolation,
     policyDefinitions,
     tournamentRecord,
     tieFormatName,
@@ -255,6 +257,13 @@ export function generateDrawDefinition(params) {
   }
 
   drawDefinition.drawName = params.drawName || drawType;
+
+  if (typeof voluntaryConsolation === 'object')
+    addVoluntaryConsolationStructure({
+      ...voluntaryConsolation,
+      drawDefinition,
+      matchUpType,
+    });
 
   return {
     qualifyingConflicts,
