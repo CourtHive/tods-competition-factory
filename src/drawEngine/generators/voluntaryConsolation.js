39c5359f90ef3f25eb0337b2effd83764046a716
CourtHive <charles@courthive.com>
2022-04-13 12:29:59 -0400

    cleanup

--- src/drawEngine/generators/voluntaryConsolation.js
+++ src/drawEngine/generators/voluntaryConsolation.js
@@ -1,7 +1,5 @@
-// import { automatedPositioning } from '../governors/positionGovernor/automatedPositioning';
 import { modifyDrawNotice } from '../notifications/drawNotifications';
 import structureTemplate from './structureTemplate';
-// import { treeMatchUps } from './eliminationTree';
 
 import { MISSING_DRAW_DEFINITION } from '../../constants/errorConditionConstants';
 import { VOLUNTARY_CONSOLATION } from '../../constants/drawDefinitionConstants';
@@ -10,14 +8,9 @@ import { SUCCESS } from '../../constants/resultConstants';
 export function addVoluntaryConsolationStructure({
   structureName = VOLUNTARY_CONSOLATION,
   structureAbbreviation,
-  // tournamentRecord,
   drawDefinition,
-  // participants,
   matchUpType,
   structureId,
-  // automated,
-  // drawSize,
-  // event,
 }) {
   if (!drawDefinition) return { error: MISSING_DRAW_DEFINITION };
 
