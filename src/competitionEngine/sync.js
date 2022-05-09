3e93cd4eb5299942006051ea51ba47fa315795e8
CourtHive <charles@courthive.com>
2022-05-09 13:19:26 -0400

    update perf logging

--- src/competitionEngine/sync.js
+++ src/competitionEngine/sync.js
@@ -103,7 +103,7 @@ export const competitionEngine = (function () {
 
     const log = { methodName };
     if (
-      devContext.perf !== undefined &&
+      ![undefined, false].includes(devContext.perf) &&
       (isNaN(devContext.perf) || elapsed > devContext.perf)
     )
       log.elapsed = elapsed;
