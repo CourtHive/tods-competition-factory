0956f09f402561022e9ef7053b19789043780480
CourtHive <charles@courthive.com>
2022-05-12 10:08:39 -0400

    extend tests

--- src/mocksEngine/tests/anonymizeTournamentRecord.test.js
+++ src/mocksEngine/tests/anonymizeTournamentRecord.test.js
@@ -1,17 +1,25 @@
 import mocksEngine from '..';
 import { intersection } from '../../utilities';
+import { extractDate } from '../../utilities/dateTime';
 
 test('it can anonymize tournamentRecords', () => {
+  const tournamentName = 'Demo Tournament';
   const { tournamentRecord } = mocksEngine.generateTournamentRecord({
     drawProfiles: [{ drawSize: 16 }],
+    tournamentName,
   });
 
   const originalPersons = tournamentRecord.participants.map(
     (participant) => participant.person
   );
 
+  expect(tournamentRecord.tournamentName).toEqual(tournamentName);
+
   let result = mocksEngine.anonymizeTournamentRecord({ tournamentRecord });
   expect(result.success).toEqual(true);
+
+  const currentDate = extractDate(new Date().toISOString());
+  expect(tournamentRecord.tournamentName).toEqual(`Anonymized: ${currentDate}`);
 
   const generatedPersons = tournamentRecord.participants.map(
     (participant) => participant.person
