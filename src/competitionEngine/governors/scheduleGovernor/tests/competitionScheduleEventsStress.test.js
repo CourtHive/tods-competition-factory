8c22a2b24029d979d9f0e699a5a2773477335551
CourtHive <charles@courthive.com>
2022-06-20 10:23:36 -0400

    extend tests

--- src/competitionEngine/governors/scheduleGovernor/tests/competitionScheduleEventsStress.test.js
+++ src/competitionEngine/governors/scheduleGovernor/tests/competitionScheduleEventsStress.test.js
@@ -117,7 +117,6 @@ test.skip('competitionSchedule performance 4 events', () => {
       ],
     },
   ];
-
   const { scheduledRounds, tournamentRecord } =
     mocksEngine.generateTournamentRecord({
       autoSchedule: true,
@@ -137,4 +136,63 @@ test.skip('competitionSchedule performance 4 events', () => {
     matchUpFilters,
   });
   expect(result.dateMatchUps.length).toEqual(256);
+});
+
+test('mocksEngine can autoschedule', () => {
+  const startDate = '2022-02-02';
+  const drawId = 'mockDrawId';
+  const venueProfiles = [
+    {
+      venueId: 'e8e4c0b0-216c-426f-bba2-18e16caa74b8', // ensure consistent venueId for courts shared across tournaments
+      venueName: 'Club Courts',
+      venueAbbreviation: 'CC',
+      startTime: '08:00',
+      endTime: '20:00',
+      courtsCount: 6,
+    },
+  ];
+  const drawProfiles = [
+    {
+      eventName: `WTN 14-19 SINGLES`,
+      category: { ratingType: 'WTN', ratingMin: 14, ratingMax: 19.99 },
+      generate: true,
+      drawSize: 4,
+      drawId,
+    },
+  ];
+  const schedulingProfile = [
+    {
+      scheduleDate: startDate,
+      venues: [
+        {
+          venueId: venueProfiles[0].venueId,
+          rounds: [{ drawId, winnerFinishingPositionRange: '1-2' }],
+        },
+      ],
+    },
+  ];
+  const personExtensions = [
+    { name: 'districtCode', value: 'Z' },
+    { name: 'sectionCode', value: '123' },
+  ];
+  const participantsProfile = { personExtensions };
+
+  const { tournamentRecord } = mocksEngine.generateTournamentRecord({
+    scheduleCompletedMatchUps: true,
+    completeAllMatchUps: true,
+    participantsProfile,
+    autoSchedule: true,
+    schedulingProfile,
+    venueProfiles,
+    drawProfiles,
+    startDate,
+  });
+
+  const {
+    matchUps: [mockedMatchUp],
+  } = tournamentEngine.setState(tournamentRecord).allTournamentMatchUps();
+
+  expect(Object.keys(mockedMatchUp.schedule).length).toBeGreaterThan(0);
+
+  console.log(mockedMatchUp.sides[0]);
 });
