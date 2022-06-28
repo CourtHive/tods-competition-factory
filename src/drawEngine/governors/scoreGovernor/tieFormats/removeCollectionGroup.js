474fc666450ccb885ecf054fe94307712eda9f2b
CourtHive <charles@courthive.com>
2022-06-28 08:55:12 -0400

    refactor for reuse

--- src/drawEngine/governors/scoreGovernor/tieFormats/removeCollectionGroup.js
+++ src/drawEngine/governors/scoreGovernor/tieFormats/removeCollectionGroup.js
@@ -1,19 +1,10 @@
 import { getTieFormat } from '../../../../tournamentEngine/getters/getTieFormat';
-import { updateTieMatchUpScore } from '../../matchUpGovernor/tieMatchUpScore';
-import { definedAttributes } from '../../../../utilities/objects';
-import { getTargetTeamMatchUps } from './getTargetTeamMatchUps';
-import { calculateWinCriteria } from './calculateWinCriteria';
+import { collectionGroupUpdate } from './collectionGroupUpdate';
 import { validateTieFormat } from './tieFormatUtilities';
 import { copyTieFormat } from './copyTieFormat';
-import {
-  modifyDrawNotice,
-  modifyMatchUpNotice,
-} from '../../../notifications/drawNotifications';
 
-import { SUCCESS } from '../../../../constants/resultConstants';
 import {
   INVALID_VALUES,
-  MISSING_DRAW_DEFINITION,
   MISSING_VALUE,
 } from '../../../../constants/errorConditionConstants';
 
@@ -60,75 +51,18 @@ export function removeCollectionGroup({
     ({ groupNumber }) => groupNumber !== collectionGroupNumber
   );
 
-  // calculate new winCriteria for tieFormat
-  // if existing winCriteria is aggregateValue, retain
-  const { aggregateValue, valueGoal } = calculateWinCriteria(tieFormat);
-
-  tieFormat.winCriteria = { aggregateValue, valueGoal };
-
-  // if valueGoal has changed, force renaming of the tieFormat
-  if (originalValueGoal && originalValueGoal !== valueGoal) {
-    if (tieFormatName) {
-      tieFormat.tieFormatName = tieFormatName;
-    } else {
-      delete tieFormat.tieFormatName;
-    }
-  }
-
-  const { targetMatchUps } = getTargetTeamMatchUps({
+  return collectionGroupUpdate({
     updateInProgressMatchUps,
+    originalValueGoal,
+    tournamentRecord,
     drawDefinition,
+    tieFormatName,
     structureId,
     structure,
+    tieFormat,
     matchUpId,
     matchUp,
+    eventId,
+    event,
   });
-
-  for (const targetMatchUp of targetMatchUps) {
-    const hasTieFormat = !!targetMatchUp.tieFormat;
-    if (hasTieFormat) {
-      targetMatchUp.tieFormat = copyTieFormat(tieFormat);
-    }
-
-    let scoreUpdated;
-    if (updateInProgressMatchUps) {
-      // recalculate score
-      const result = updateTieMatchUpScore({
-        matchUpId: targetMatchUp.matchUpId,
-        exitWhenNoValues: true,
-        tournamentRecord,
-        drawDefinition,
-        event,
-      });
-      if (result.error) return result;
-      scoreUpdated = result.score;
-    }
-
-    if (hasTieFormat && !scoreUpdated) {
-      console.log('boo', { scoreUpdated, hasTieFormat });
-
-      modifyMatchUpNotice({
-        tournamentId: tournamentRecord?.tournamentId,
-        eventId: event?.eventId,
-        matchUp: targetMatchUp,
-        drawDefinition,
-      });
-    }
-  }
-
-  const prunedTieFormat = definedAttributes(tieFormat);
-  if (eventId) {
-    event.tieFormat = prunedTieFormat;
-    // NOTE: there is not a modifyEventNotice
-  } else if (structure) {
-    structure.tieFormat = prunedTieFormat;
-  } else if (drawDefinition) {
-    drawDefinition.tieFormat = prunedTieFormat;
-  } else if (!matchUp || !drawDefinition) {
-    return { error: MISSING_DRAW_DEFINITION };
-  }
-
-  modifyDrawNotice({ drawDefinition, eventId: event?.eventId });
-
-  return { ...SUCCESS };
 }
