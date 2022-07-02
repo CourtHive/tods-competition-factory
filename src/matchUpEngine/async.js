8a91d05e5271adac4e676cd7cffd77573af620aa
CourtHive <charles@courthive.com>
2022-07-01 21:16:17 -0400

    initial commit matchUpEngine

---
+++ src/matchUpEngine/async.js
@@ -0,0 +1,124 @@
+import { setState } from './stateMethods';
+import { notifySubscribersAsync } from '../global/state/notifySubscribers';
+import { factoryVersion } from '../global/functions/factoryVersion';
+import { makeDeepCopy } from '../utilities';
+import {
+  setDeepCopy,
+  setDevContext,
+  getDevContext,
+  deleteNotices,
+  createInstanceState,
+} from '../global/state/globalState';
+
+import { SUCCESS } from '../constants/resultConstants';
+
+let matchUp;
+
+export function drawEngineAsync(test) {
+  const result = createInstanceState();
+  if (result.error && !test) return result;
+
+  const engine = {
+    getState: ({ convertExtensions, removeExtensions } = {}) => ({
+      matchUp: makeDeepCopy(
+        matchUp,
+        convertExtensions,
+        false,
+        removeExtensions
+      ),
+    }),
+    version: () => factoryVersion(),
+    reset: () => {
+      matchUp = undefined;
+      return { ...SUCCESS };
+    },
+  };
+
+  function processResult(result) {
+    if (result?.error) {
+      engine.error = result.error;
+      engine.success = false;
+    } else {
+      engine.error = undefined;
+      engine.success = true;
+      matchUp = result;
+      engine.drawId = result.drawId;
+    }
+    return engine;
+  }
+
+  importGovernors([]);
+
+  engine.devContext = (isDev) => {
+    setDevContext(isDev);
+    return engine;
+  };
+  engine.setState = (definition, deepCopyOption, deepCopyAttributes) => {
+    setDeepCopy(deepCopyOption, deepCopyAttributes);
+    const result = setState(definition);
+    return processResult(result);
+  };
+
+  return engine;
+
+  async function importGovernors(governors) {
+    for (const governor of governors) {
+      const governorMethods = Object.keys(governor);
+
+      for (const governorMethod of governorMethods) {
+        engine[governorMethod] = async (params) => {
+          if (getDevContext()) {
+            const result = await invoke({ params, governor, governorMethod });
+
+            return result;
+          } else {
+            try {
+              const result = await invoke({ params, governor, governorMethod });
+
+              return result;
+            } catch (err) {
+              const error = err.toString();
+              console.log('ERROR', {
+                error,
+                method: governorMethod,
+                params: JSON.stringify(params),
+              });
+              console.log(err);
+            }
+          }
+        };
+      }
+    }
+  }
+
+  async function invoke({ params, governor, governorMethod }) {
+    delete engine.success;
+    delete engine.error;
+
+    const snapshot =
+      params?.rollbackOnError && makeDeepCopy(matchUp, false, true);
+
+    params = {
+      ...params,
+      matchUp,
+    };
+
+    const result = governor[governorMethod](params);
+
+    if (result?.error) {
+      if (snapshot) setState(snapshot);
+      return { ...result, rolledBack: !!snapshot };
+    }
+
+    const notify =
+      result?.success &&
+      params?.delayNotify !== true &&
+      params?.doNotNotify !== true;
+    if (notify) await notifySubscribersAsync();
+    if (notify || !result?.success || params?.doNotNotify) deleteNotices();
+
+    return result;
+  }
+}
+
+export default drawEngineAsync;
---
+++ src/matchUpEngine/sync.js
@@ -0,0 +1,114 @@
+import { setState } from './stateMethods';
+import { notifySubscribers } from '../global/state/notifySubscribers';
+import { factoryVersion } from '../global/functions/factoryVersion';
+import { makeDeepCopy } from '../utilities';
+import {
+  setDeepCopy,
+  setDevContext,
+  getDevContext,
+  deleteNotices,
+} from '../global/state/globalState';
+
+import { SUCCESS } from '../constants/resultConstants';
+
+let matchUp;
+
+export const drawEngine = (function () {
+  const engine = {
+    getState: ({ convertExtensions, removeExtensions } = {}) => ({
+      matchUp: makeDeepCopy(
+        matchUp,
+        convertExtensions,
+        false,
+        removeExtensions
+      ),
+    }),
+    version: () => factoryVersion(),
+    reset: () => {
+      matchUp = undefined;
+      return { ...SUCCESS };
+    },
+  };
+
+  function processResult(result) {
+    if (result?.error) {
+      engine.error = result.error;
+      engine.success = false;
+    } else {
+      engine.error = undefined;
+      engine.success = true;
+      matchUp = result;
+      engine.drawId = result.drawId;
+    }
+    return engine;
+  }
+
+  importGovernors([]);
+
+  engine.devContext = (isDev) => {
+    setDevContext(isDev);
+    return engine;
+  };
+  engine.setState = (definition, deepCopyOption, deepCopyAttributes) => {
+    setDeepCopy(deepCopyOption, deepCopyAttributes);
+    const result = setState(definition);
+    return processResult(result);
+  };
+
+  return engine;
+
+  function importGovernors(governors) {
+    governors.forEach((governor) => {
+      Object.keys(governor).forEach((governorMethod) => {
+        engine[governorMethod] = (params) => {
+          if (getDevContext()) {
+            return invoke({ params, governor, governorMethod });
+          } else {
+            try {
+              return invoke({ params, governor, governorMethod });
+            } catch (err) {
+              const error = err.toString();
+              console.log('ERROR', {
+                error,
+                method: governorMethod,
+                params: JSON.stringify(params),
+              });
+              console.log(err);
+            }
+          }
+        };
+      });
+    });
+  }
+
+  function invoke({ params, governor, governorMethod }) {
+    delete engine.success;
+    delete engine.error;
+
+    const snapshot =
+      params?.rollbackOnError && makeDeepCopy(matchUp, false, true);
+
+    params = {
+      ...params,
+      matchUp,
+    };
+
+    const result = governor[governorMethod](params);
+
+    if (result?.error) {
+      if (snapshot) setState(snapshot);
+      return { ...result, rolledBack: !!snapshot };
+    }
+
+    const notify =
+      result?.success &&
+      params?.delayNotify !== true &&
+      params?.doNotNotify !== true;
+    if (notify) notifySubscribers();
+    if (notify || !result?.success || params?.doNotNotify) deleteNotices();
+
+    return result;
+  }
+})();
+
+export default drawEngine;
