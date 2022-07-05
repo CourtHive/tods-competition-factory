9b3e557bf605da90398d600deaa1e6ed4e19a070
CourtHive <charles@courthive.com>
2022-07-05 18:32:52 -0400

    `matchUpEngine` additions

--- src/matchUpEngine/governors/historyGovernor/addGame.js
+++ src/matchUpEngine/governors/historyGovernor/addGame.js
@@ -1,1 +1,15 @@
-export function addGame() {}
+import { saveHistory } from './saveHistory';
+import { getHistory } from './getHistory';
+
+import { MISSING_VALUE } from '../../../constants/errorConditionConstants';
+
+export function addGame({ matchUp, game }) {
+  // TODO: check game validity
+  if (typeof point !== 'object') return { error: MISSING_VALUE };
+
+  // do not destructure undoHistory; it is destroyed when adding to history
+  const { history = [] } = getHistory({ matchUp });
+  history.push(game);
+
+  return saveHistory({ matchUp, history });
+}
--- src/matchUpEngine/governors/historyGovernor/addSet.js
+++ src/matchUpEngine/governors/historyGovernor/addSet.js
@@ -1,1 +1,15 @@
-export function addSet() {}
+import { saveHistory } from './saveHistory';
+import { getHistory } from './getHistory';
+
+import { MISSING_VALUE } from '../../../constants/errorConditionConstants';
+
+export function addSet({ matchUp, set }) {
+  // TODO: check set validity
+  if (typeof point !== 'object') return { error: MISSING_VALUE };
+
+  // do not destructure undoHistory; it is destroyed when adding to history
+  const { history = [] } = getHistory({ matchUp });
+  history.push(set);
+
+  return saveHistory({ matchUp, history });
+}
