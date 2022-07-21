d03e11d2ff91a58e13a5acf86706205756088551
CourtHive <charles@courthive.com>
2022-07-21 13:19:31 -0400

    extend tests

--- src/tournamentEngine/tests/drawDefinitions/qualifyingOnly.test.js
+++ src/tournamentEngine/tests/drawDefinitions/qualifyingOnly.test.js
@@ -2,7 +2,12 @@ import mocksEngine from '../../../mocksEngine';
 import { unique } from '../../../utilities';
 import tournamentEngine from '../../sync';
 
-import { MAIN, QUALIFYING } from '../../../constants/drawDefinitionConstants';
+import {
+  DRAW,
+  MAIN,
+  QUALIFYING,
+  WINNER,
+} from '../../../constants/drawDefinitionConstants';
 
 it('can generate QUALIFYING structures before MAIN structure', () => {
   let result = mocksEngine.generateTournamentRecord({
@@ -43,4 +48,14 @@ it('can generate QUALIFYING structures before MAIN structure', () => {
     ({ stage }) => stage === QUALIFYING
   );
   expect(qualifyingStructure.matchUps.length).toEqual(12);
+
+  const links = drawDefinition.links;
+  expect(links.length).toEqual(1);
+  expect(links[0].linkType).toEqual(WINNER);
+
+  expect(links[0].source.structureId).toEqual(qualifyingStructure.structureId);
+
+  expect(links[0].target.feedProfile).toEqual(DRAW);
+  expect(links[0].target.structureId).toEqual(mainStructure.structureId);
+  expect(links[0].target.roundNumber).toEqual(1);
 });
