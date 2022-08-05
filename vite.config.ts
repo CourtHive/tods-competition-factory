969e684d7d20f524d2f041f33ba6193916bff72d
CourtHive <charles@courthive.com>
2022-08-05 17:40:07 -0400

    vitest exclude glob patterns

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/benchmarks/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
   },
 });
