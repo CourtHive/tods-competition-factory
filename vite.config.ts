ba2311682ee2598006e5f02d58cc264e4e287faf
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2022-08-05 14:34:50 -0300

    replace jest for vitest

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+// eslint-disable-next-line import/no-unresolved
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+});
