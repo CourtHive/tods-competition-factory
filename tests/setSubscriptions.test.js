b7dc4ef7aa43e7297310b27d77f976d86fb26f34
CourtHive <charles@courthive.com>
2022-08-07 16:50:02 -0400

    update tests

--- tests/setSubscriptions.test.js
+++ tests/setSubscriptions.test.js
@@ -1,4 +1,4 @@
-import { setSubscriptions, tournamentEngine, utilities } from '../dist';
+import { setSubscriptions, tournamentEngine, utilities } from '../src/index';
 
 it('can set subscriptions in global state outside of engines', () => {
   const allMatchUps = [];
