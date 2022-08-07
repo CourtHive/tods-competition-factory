9add69d8258cd8411a6cfb44983d40538ba4ae5d
CourtHive <charles@courthive.com>
2022-08-07 14:07:20 -0400

    cleanup

--- src/drawEngine/governors/structureGovernor/getGenerators.js
+++ src/drawEngine/governors/structureGovernor/getGenerators.js
@@ -2,8 +2,8 @@ import { firstRoundLoserConsolation } from '../../generators/firstRoundLoserCons
 import { generateDoubleElimination } from '../../generators/doubleEliminattion';
 import { generateCurtisConsolation } from '../../generators/curtisConsolation';
 import { generatePlayoffStructures } from '../../generators/playoffStructures';
-import { feedInChampionship } from '../../generators/feedInChamp';
 import structureTemplate from '../../generators/structureTemplate';
+import { feedInChampionship } from '../../generators/feedInChamp';
 import { feedInMatchUps } from '../../generators/feedInMatchUps';
 import { treeMatchUps } from '../../generators/eliminationTree';
 import { luckyDraw } from '../../generators/luckyDraw';
