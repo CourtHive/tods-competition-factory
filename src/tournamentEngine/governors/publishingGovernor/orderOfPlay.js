3322c8441b3dc9582dbc4f17f3296b78a5c0fe38
CourtHive <charles@courthive.com>
2022-08-07 16:36:57 -0400

    relocate publishing methods

--- src/tournamentEngine/governors/publishingGovernor/orderOfPlay.js
+++ src/tournamentEngine/governors/publishingGovernor/orderOfPlay.js
@@ -4,8 +4,47 @@ import { addNotice } from '../../../global/state/globalState';
 
 import { MISSING_TOURNAMENT_RECORD } from '../../../constants/errorConditionConstants';
 import { PUBLIC, PUBLISH, STATUS } from '../../../constants/timeItemConstants';
-import { PUBLISH_ORDER_OF_PLAY } from '../../../constants/topicConstants';
 import { SUCCESS } from '../../../constants/resultConstants';
+import {
+  UNPUBLISH_ORDER_OF_PLAY,
+  PUBLISH_ORDER_OF_PLAY,
+} from '../../../constants/topicConstants';
+
+export function unPublishOrderOfPlay({
+  removePriorValues,
+  tournamentRecord,
+  status = PUBLIC,
+}) {
+  if (!tournamentRecord) return { error: MISSING_TOURNAMENT_RECORD };
+
+  const itemType = `${PUBLISH}.${STATUS}`;
+  const { timeItem } = getTournamentTimeItem({
+    itemType,
+  });
+
+  const itemValue = timeItem?.itemValue || { [status]: {} };
+
+  if (itemValue[status]) delete itemValue[status].orderOfPlay;
+
+  const updatedTimeItem = {
+    itemValue,
+    itemType,
+  };
+
+  addTournamentTimeItem({
+    timeItem: updatedTimeItem,
+    removePriorValues,
+    tournamentRecord,
+  });
+  addNotice({
+    topic: UNPUBLISH_ORDER_OF_PLAY,
+    payload: {
+      tournamentId: tournamentRecord.tournamentId,
+    },
+  });
+
+  return { ...SUCCESS };
+}
 
 export function publishOrderOfPlay({
   scheduledDates = [],
