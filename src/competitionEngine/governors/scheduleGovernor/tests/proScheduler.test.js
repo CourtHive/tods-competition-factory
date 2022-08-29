f598abf4f0d8113c1f1a86b58d77f16993b4ea60
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2022-08-29 15:13:35 -0300

    add test covering pro-scheduling + personal request scenario

--- src/competitionEngine/governors/scheduleGovernor/tests/proScheduler.test.js
+++ src/competitionEngine/governors/scheduleGovernor/tests/proScheduler.test.js
@@ -2,6 +2,7 @@ import { addDays, dateRange } from '../../../../utilities/dateTime';
 import { chunkArray } from '../../../../utilities';
 import mocksEngine from '../../../../mocksEngine';
 import competitionEngine from '../../../sync';
+import tournamentEngine from '../../../../tournamentEngine/sync';
 import { expect } from 'vitest';
 
 it('supports pro-scheduling', () => {
@@ -75,5 +76,133 @@ it('supports pro-scheduling', () => {
     [4, '2022-08-28T08:30', 'Court 1'],
     [4, '2022-08-28T08:30', 'Court 2'],
     [5, '2022-08-29T07:00', 'Court 1'], // round 5 is scheduled for the 3rd day
+  ]);
+});
+
+it('pro-scheduling respects DO_NOT_SCHEDULE requests', () => {
+  tournamentEngine.reset();
+  competitionEngine.reset();
+
+  const availableDates = ['2022-01-01'];
+
+  tournamentEngine.newTournamentRecord({
+    tournamentName: 'New Tournament',
+  });
+
+  const {
+    venue: { venueId },
+  } = tournamentEngine.addVenue({
+    venue: { venueName: 'test Tennis', venueAbbreviation: 'Test' },
+    returnDetails: true,
+  });
+
+  const dateAvailability = availableDates.map((date) => ({
+    date: date,
+    startTime: '08:00',
+    endTime: '22:00',
+  }));
+
+  tournamentEngine.addCourts({
+    courtNames: ['1'],
+    courtsCount: 1,
+    venueId,
+    dateAvailability: dateAvailability,
+  });
+
+  const generatePersons = (length) => {
+    return [...new Array(length)].map((_, index) => {
+      return {
+        personId: index,
+        standardFamilyName: 'test',
+        standardGivenName: 'test',
+        nationalityCode: 'BRA',
+        sex: 'MALE',
+      };
+    });
+  };
+
+  const persons = generatePersons(3);
+  tournamentEngine.addPersons({ persons });
+
+  // add a DO_NOT_SCHEDULE for the first person
+
+  const requests = [
+    {
+      date: availableDates[0],
+      startTime: '07:00',
+      endTime: '12:00',
+      requestType: 'DO_NOT_SCHEDULE',
+    },
+  ];
+
+  competitionEngine.addPersonRequests({
+    personId: persons[0].personId,
+    requests,
+  });
+
+  const event = {
+    eventName: 'Test Event',
+    eventType: 'SINGLES',
+    eventGender: 'MALE',
+  };
+
+  let { tournamentRecord } = tournamentEngine.getState();
+  const { participants } = tournamentRecord;
+  const participantsIds = participants.map(
+    (participant) => participant.participantId
+  );
+
+  const {
+    event: { eventId },
+  } = tournamentEngine.addEvent({ event });
+
+  tournamentEngine.setEventDates({
+    eventId,
+    startDate: availableDates[0],
+    endDate: availableDates[availableDates.length - 1],
+  });
+
+  tournamentEngine.addEventEntries({
+    eventId,
+    participantIds: participantsIds,
+  });
+
+  tournamentEngine.generateDrawDefinition({
+    eventId: eventId,
+    addToEvent: true,
+  });
+
+  competitionEngine.clearScheduledMatchUps();
+  const { rounds } = tournamentEngine.getRounds();
+
+  const schedulingProfile = [
+    {
+      scheduleDate: availableDates[0],
+      venues: [{ venueId, rounds }],
+    },
+  ];
+
+  let result = competitionEngine.setSchedulingProfile({ schedulingProfile });
+
+  result = competitionEngine.scheduleProfileRounds({
+    pro: true,
+    scheduleDates: availableDates,
+  });
+
+  expect(result.success).toEqual(true);
+  expect(result.noTimeMatchUpIds[availableDates[0]]).toEqual([]);
+
+  const { matchUps } = competitionEngine.allCompetitionMatchUps();
+  const roundSchedules = matchUps.map(
+    ({ schedule: { scheduledTime, courtName }, roundNumber }) => [
+      roundNumber,
+      scheduledTime,
+      courtName,
+    ]
+  );
+
+  expect(roundSchedules).toStrictEqual([
+    [1, '2022-01-01T08:00', '1'],
+    [2, '2022-01-01T09:30', '1'],
   ]);
 });
