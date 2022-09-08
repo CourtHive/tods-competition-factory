43c2cad2638e593c8c3759f1e3d63c15fd9bf8a5
CourtHive <charles@courthive.com>
2022-09-08 17:46:24 -0400

    tweaks

--- src/matchUpEngine/governors/scoreGovernor/index.js
+++ src/matchUpEngine/governors/scoreGovernor/index.js
@@ -1,15 +1,15 @@
 import { generateTieMatchUpScore } from '../../../drawEngine/generators/generateTieMatchUpScore';
 import { validateTieFormat } from '../tieFormatGovernor/tieFormatUtilities';
+import { generateScoreString } from '../../generators/generateScoreString';
+import { checkSetIsComplete, keyValueScore } from './keyValueScore';
+import { scoreHasValue } from '../queryGovernor/scoreHasValue';
+import { analyzeSet } from '../../getters/analyzeSet';
 import {
   getSetComplement,
   getTiebreakComplement,
 } from '../../getters/getComplement';
-import { checkSetIsComplete, keyValueScore } from './keyValueScore';
-import { generateScoreString } from '../../generators/generateScoreString';
-import { analyzeSet } from '../../getters/analyzeSet';
 
-import { scoreHasValue } from '../queryGovernor/scoreHasValue';
-
+// renamed
 import { stringify } from '../matchUpFormatGovernor/stringify';
 import { isValid } from '../matchUpFormatGovernor/isValid';
 import { parse } from '../matchUpFormatGovernor/parse';
