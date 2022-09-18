c9bf4242568986db26e1965e07e91ddd9b4766bc
CourtHive <charles@courthive.com>
2022-09-18 12:54:46 -0400

    correct `aggregateValue` winning participant direction

--- src/tournamentEngine/tests/team/tieFormatAggregateValue.test.js
+++ src/tournamentEngine/tests/team/tieFormatAggregateValue.test.js
@@ -1,7 +1,8 @@
 import mocksEngine from '../../../mocksEngine';
 import tournamentEngine from '../../sync';
 
-import { DOUBLES } from '../../../constants/matchUpTypes';
+import { FEMALE, MALE, MIXED } from '../../../constants/genderConstants';
+import { DOUBLES, SINGLES } from '../../../constants/matchUpTypes';
 import { TEAM } from '../../../constants/eventConstants';
 import {
   COMPLETED,
@@ -9,6 +10,38 @@ import {
 } from '../../../constants/matchUpStatusConstants';
 
 const tieFormats = {
+  aggregateDuo: {
+    collectionDefinitions: [
+      {
+        collectionName: 'Mixed Doubles',
+        gender: MIXED,
+        matchUpCount: 1,
+        matchUpFormat: 'SET1-S:8/TB7',
+        matchUpType: DOUBLES,
+        matchUpValue: 1,
+      },
+      {
+        collectionName: 'Male Singles',
+        matchUpCount: 1,
+        gender: MALE,
+        matchUpFormat: 'SET3-S:6/TB7-F:TB10',
+        matchUpType: SINGLES,
+        matchUpValue: 1,
+      },
+      {
+        collectionName: 'Female Singles',
+        gender: FEMALE,
+        matchUpCount: 1,
+        matchUpFormat: 'SET3-S:6/TB7-F:TB10',
+        matchUpType: SINGLES,
+        matchUpValue: 1,
+      },
+    ],
+    tieFormatName: 'DOMINANT_DUO_MIXED',
+    winCriteria: {
+      valueGoal: 2,
+    },
+  },
   Shuffle: {
     collectionDefinitions: [
       {
@@ -298,5 +331,160 @@ test.each(scenarios)(
     expect(secondRoundDualMatchUp.drawPositions).toEqual(
       secondRoundDrawPositions
     );
+  }
+);
+
+// prettier-ignore
+const outcomeScenarios = [
+  {
+    expectation: { 
+      scores: ['7-5', '6-3 6-3', '2-6 2-6'],
+    },
+    outcomes: [
+      {
+        score: {
+          sets: [ { setNumber: 1, side1Score: 7, side2Score: 5, winningSide: 1, }, ],
+        },
+        winningSide: 1,
+      },
+      {
+        score: {
+          sets: [
+            { setNumber: 1, side1Score: 6, side2Score: 3, winningSide: 1, },
+            { setNumber: 2, side1Score: 6, side2Score: 3, winningSide: 1, }, ],
+        },
+        winningSide: 1,
+      },
+      {
+        score: {
+          sets: [
+            { setNumber: 1, side1Score: 2, side2Score: 6, winningSide: 2, },
+            { setNumber: 2, side1Score: 2, side2Score: 6, winningSide: 2, },
+          ],
+        },
+        winningSide: 2,
+      },
+    ],
+  },
+  {
+    expectation: { 
+      scores: ['7-5', '6-3 3-6 [10-2]', '2-6 2-6'],
+    },
+    winCriteria: { aggregateValue: true },
+    outcomes: [
+      {
+        score: {
+          sets: [
+            { setNumber: 1, side1Score: 7, side2Score: 5, winningSide: 1, },
+          ],
+        },
+        winningSide: 1,
+      },
+      {
+        score: {
+          sets: [
+            { setNumber: 1, side1Score: 6, side2Score: 3, winningSide: 1, },
+            { setNumber: 2, side1Score: 3, side2Score: 6, winningSide: 2, },
+            { setNumber: 3, side1TiebreakScore: 10, side2TiebreakScore: 2, winningSide: 1, },
+          ],
+        },
+        winningSide: 1,
+      },
+      {
+        score: {
+          sets: [
+            { setNumber: 1, side1Score: 2, side2Score: 6, winningSide: 2, },
+            { setNumber: 2, side1Score: 2, side2Score: 6, winningSide: 2, },
+          ],
+        },
+        winningSide: 2,
+      },
+    ],
+  },
+];
+
+test.each(outcomeScenarios)(
+  'aggregateValue works with matchUpValue',
+  (scenario) => {
+    let tieFormat = tieFormats.aggregateDuo;
+    if (scenario.winCriteria) tieFormat.winCriteria = scenario.winCriteria;
+
+    const {
+      tournamentRecord,
+      drawIds: [drawId],
+    } = mocksEngine.generateTournamentRecord({
+      drawProfiles: [
+        {
+          eventType: TEAM,
+          drawSize: 4,
+          tieFormat,
+        },
+      ],
+    });
+
+    tournamentEngine.setState(tournamentRecord);
+
+    let { matchUps } = tournamentEngine.allTournamentMatchUps();
+    expect(matchUps.length).toEqual(12);
+
+    let teamMatchUp = matchUps.find(
+      (m) => m.matchUpType === TEAM && m.roundNumber === 1
+    );
+    const singlesMatchUps = teamMatchUp.tieMatchUps.filter(
+      (m) => m.matchUpType === SINGLES
+    );
+    const doublesMatchUp = teamMatchUp.tieMatchUps.find(
+      (m) => m.matchUpType === DOUBLES
+    );
+
+    let result = tournamentEngine.setMatchUpStatus({
+      outcome: scenario.outcomes[0],
+      matchUpId: doublesMatchUp.matchUpId,
+      drawId,
+    });
+    expect(result.success).toEqual(true);
+
+    result = tournamentEngine.setMatchUpStatus({
+      outcome: scenario.outcomes[1],
+      matchUpId: singlesMatchUps[0].matchUpId,
+      drawId,
+    });
+    expect(result.success).toEqual(true);
+
+    result = tournamentEngine.setMatchUpStatus({
+      outcome: scenario.outcomes[2],
+      matchUpId: singlesMatchUps[1].matchUpId,
+      drawId,
+    });
+    expect(result.success).toEqual(true);
+
+    matchUps = tournamentEngine.allTournamentMatchUps().matchUps;
+
+    teamMatchUp = matchUps.find(
+      ({ matchUpId }) => matchUpId === teamMatchUp.matchUpId
+    );
+    expect(
+      teamMatchUp.tieMatchUps.map((m) => m.score?.scoreStringSide1)
+    ).toEqual(scenario.expectation.scores);
+    expect(teamMatchUp.winningSide).toEqual(1);
+
+    const winningParticipant = teamMatchUp.sides.find(
+      ({ sideNumber }) => sideNumber === teamMatchUp.winningSide
+    ).participant;
+    const winningParticipantId = winningParticipant.participantId;
+
+    const finalTeamMatchUp = matchUps.find(
+      ({ roundNumber, matchUpType }) =>
+        matchUpType === TEAM && roundNumber === 2
+    );
+    const advancedTeam = finalTeamMatchUp.sides
+      .map(({ participant }) => participant)
+      .filter(Boolean)[0];
+    const advancedParticipantId = advancedTeam.participantId;
+
+    expect(winningParticipant.participantName).toEqual(
+      advancedTeam.participantName
+    );
+    expect(winningParticipantId).toEqual(advancedParticipantId);
   }
 );
