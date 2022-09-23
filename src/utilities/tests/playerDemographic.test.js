a656b222f9895b23a5fcc68e8c344b2e17517672
CourtHive <charles@courthive.com>
2022-09-23 12:24:39 -0400

    extend test

--- src/utilities/tests/playerDemographic.test.js
+++ src/utilities/tests/playerDemographic.test.js
@@ -4,6 +4,7 @@ import mocksEngine from '../../mocksEngine';
 import { INDIVIDUAL } from '../../constants/participantConstants';
 import { DOUBLES } from '../../constants/eventConstants';
 import { PAIR } from '../../constants/participantTypes';
+import { JSON2CSV } from '../json';
 
 it('can export CSV files with player demographic data', () => {
   const personExtensions = [
@@ -12,15 +13,24 @@ it('can export CSV files with player demographic data', () => {
   ];
   const nationalityCodes = ['USA'];
   const participantsCount = 32;
+  const category = { categoryName: 'U18' };
   const participantsProfile = {
     participantType: PAIR,
-    nationalityCodes,
     participantsCount,
+    nationalityCodes,
     personExtensions,
+    category,
   };
 
   let result = mocksEngine.generateTournamentRecord({
-    drawProfiles: [{ drawSize: participantsCount, eventType: DOUBLES }],
+    drawProfiles: [
+      {
+        drawSize: participantsCount,
+        eventType: DOUBLES,
+        seedsCount: 8,
+        category,
+      },
+    ],
     participantsProfile,
   });
 
@@ -34,6 +44,35 @@ it('can export CSV files with player demographic data', () => {
   const { tournamentParticipants: individualParticipants } =
     tournamentEngine.getTournamentParticipants({
       participantFilters: { participantTypes: [INDIVIDUAL] },
+      convertExtensions: true,
+      withScaleValues: true,
+      withEvents: true,
     });
-  expect(individualParticipants.length).toEqual(64);
+  expect(individualParticipants.length).toEqual(128);
+
+  const rowJoiner = '|';
+  const config = {
+    rowJoiner,
+    delimiter: '',
+    includeTransformAccessors: true,
+    columnAccessors: [''],
+    columnTransform: {
+      personId: ['person.personId'],
+      firstName: ['person.standardGivenName'],
+      lastName: ['person.standardFamilyName'],
+      state: ['person.addresses.0.state'],
+      city: ['person.addresses.0.city'],
+      district: ['person._districtCode'],
+      section: ['person._sectionCode'],
+      birthDate: ['person.birthDate'],
+      sex: ['person.sex'],
+    },
+  };
+
+  const eventParticipants = individualParticipants.filter(
+    (participant) => participant.events?.length
+  );
+  console.log(eventParticipants[0]);
+  const csvParticipants = JSON2CSV(eventParticipants, config);
+  console.log(csvParticipants);
 });
