5e22e0c531162551dc031c8a7994cf0b485c9109
CourtHive <charles@courthive.com>
2022-09-27 09:14:17 -0400

    use deepCopy config in `definedAttributes` pruning

--- src/utilities/objects.js
+++ src/utilities/objects.js
@@ -1,20 +1,56 @@
+import { deepCopyEnabled } from '../global/state/globalState';
+import { isDateObject } from './dateTime';
+
 export function definedAttributes(obj, ignoreFalse, ignoreEmptyArrays) {
   if (typeof obj !== 'object' || obj === null) return obj;
+  const deepCopy = deepCopyEnabled();
+
   const ignoreValues = ['', undefined, null];
   if (ignoreFalse) ignoreValues.push(false);
+
   const definedKeys = Object.keys(obj).filter(
     (key) =>
+      !deepCopy?.ignore.includes(key) &&
       !ignoreValues.includes(obj[key]) &&
       (!ignoreEmptyArrays || (Array.isArray(obj[key]) ? obj[key].length : true))
   );
+
+  const targetObject = Array.isArray(obj) ? [] : {};
+  for (const key of definedKeys) {
+    const value = obj[key];
+    if (deepCopy?.stringify.includes(key)) {
+      targetObject[key] =
+        typeof value?.toString === 'function'
+          ? value.toString()
+          : JSON.stringify(value);
+    } else if (
+      deepCopy?.toJSON.includes(key) &&
+      typeof value?.toJSON === 'function'
+    ) {
+      targetObject[key] = value.toJSON();
+    } else if (value === null) {
+      targetObject[key] = undefined;
+    } else if (isDateObject(value)) {
+      targetObject[key] = new Date(value).toISOString();
+    } else if (Array.isArray(obj[key])) {
+      targetObject[key] = obj[key].map((m) => definedAttributes(m));
+    } else {
+      targetObject[key] = definedAttributes(obj[key]);
+    }
+  }
+
+  return targetObject;
+
+  /*
   return Object.assign(
     {},
-    ...definedKeys.map((key) =>
-      Array.isArray(obj[key])
+    ...definedKeys.map((key) => {
+      return Array.isArray(obj[key])
         ? { [key]: obj[key].map((m) => definedAttributes(m)) } // doesn't filter out undefined array elements
-        : { [key]: definedAttributes(obj[key]) }
-    )
+        : { [key]: definedAttributes(obj[key]) };
+    })
   );
+  */
 }
 
 function countKeys(o) {
