ac89903998eaf6557a171d82dbf0e7310c10d4b7
CourtHive <charles@courthive.com>
2022-10-31 13:49:34 -0400

    `personEntryReports`: add `tennisId` and `personOtherId`

--- src/tournamentEngine/governors/reportGovernor/getDetailsWTN.js
+++ src/tournamentEngine/governors/reportGovernor/getDetailsWTN.js
@@ -2,9 +2,11 @@ import { WTN } from '../../../constants/ratingConstants';
 
 export function getDetailsWTN({ participant, eventType }) {
   const personId = participant?.person?.personId;
+  const personOtherId = participant?.person?.otherIds?.[0];
+  const tennisId = participant?.person?.tennisId;
   const personWTN = participant?.ratings?.[eventType]?.find(
     ({ scaleName }) => scaleName === WTN
   )?.scaleValue;
   const { wtnRating, confidence } = personWTN || {};
-  return { personId, wtnRating, confidence };
+  return { personId, personOtherId, tennisId, wtnRating, confidence };
 }
