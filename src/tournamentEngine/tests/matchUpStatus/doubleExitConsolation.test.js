a1b10960022256a4354a2a97d665663a11bed02c
CourtHive <charles@courthive.com>
2022-11-15 17:22:48 -0500

    extend test

--- src/tournamentEngine/tests/matchUpStatus/doubleExitConsolation.test.js
+++ src/tournamentEngine/tests/matchUpStatus/doubleExitConsolation.test.js
@@ -1,6 +1,14 @@
-import { setSubscriptions } from '../../../global/state/globalState';
 import mocksEngine from '../../../mocksEngine';
 import tournamentEngine from '../../sync';
+
+import {
+  printGlobalLog,
+  pushGlobalLog,
+} from '../../../global/functions/globalLog';
+import {
+  setDevContext,
+  setSubscriptions,
+} from '../../../global/state/globalState';
 
 import { toBePlayed } from '../../../fixtures/scoring/outcomes/toBePlayed';
 import { MODIFY_MATCHUP } from '../../../constants/topicConstants';
@@ -39,8 +47,92 @@ const scenarios = [
       },
     ],
     winningSide: 1,
-    drawPositions: [1, 3],
+    preRemovalChecks: [
+      {
+        stage: MAIN,
+        matchUpStatus: DOUBLE_WALKOVER,
+        drawPositions: [3, 4],
+        roundNumber: 1,
+        roundPosition: 2,
+      },
+      {
+        stage: MAIN,
+        matchUpStatus: WALKOVER,
+        drawPositions: [1],
+        roundNumber: 2,
+        roundPosition: 1,
+        winningSide: 1,
+      },
+      {
+        stage: MAIN,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: [1],
+        roundNumber: 3,
+        roundPosition: 1,
+      },
+      {
+        stage: CONSOLATION,
+        matchUpStatus: WALKOVER,
+        drawPositions: [3, 4],
+        losingSideMatchUpStatusCode: DOUBLE_WALKOVER,
+        roundNumber: 1,
+        roundPosition: 1,
+        winningSide: 1,
+      },
+      {
+        stage: CONSOLATION,
+        matchUpStatus: BYE,
+        drawPositions: [1, 3],
+        roundNumber: 2,
+        roundPosition: 1,
+      },
+    ],
+    postRemovalChecks: [
+      {
+        stage: MAIN,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: [3, 4],
+        roundNumber: 1,
+        roundPosition: 2,
+      },
+      {
+        stage: MAIN,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: [1],
+        roundNumber: 2,
+        roundPosition: 1,
+      },
+      {
+        stage: MAIN,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: undefined,
+        roundNumber: 3,
+        roundPosition: 1,
+      },
+      {
+        stage: CONSOLATION,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: undefined,
+        roundNumber: 3,
+        roundPosition: 1,
+      },
+      {
+        stage: CONSOLATION,
+        matchUpStatus: TO_BE_PLAYED,
+        drawPositions: [3, 4],
+        roundNumber: 1,
+        roundPosition: 1,
+      },
+      {
+        stage: CONSOLATION,
+        matchUpStatus: BYE,
+        drawPositions: [1],
+        roundNumber: 2,
+        roundPosition: 1,
+      },
+    ],
   },
+  /*
   {
     outcomes: [
       {
@@ -57,11 +149,15 @@ const scenarios = [
     winningSide: 2,
     drawPositions: [2, 3],
   },
+  */
 ];
 
 test.each(scenarios)(
   'Double Exit produces exit in consolation',
-  ({ outcomes, winningSide, drawPositions }) => {
+  ({ postRemovalChecks, preRemovalChecks, outcomes }) => {
+    // setDevContext({ globalLog: true });
+    setDevContext();
+
     // keep track of notficiations with each setMatchUpStatus event
     let modifiedMatchUpLog = [];
     let result = setSubscriptions({
@@ -101,67 +197,104 @@ test.each(scenarios)(
     expect(modifiedMatchUpLog.length).toEqual(7);
 
     let matchUps = tournamentEngine.allTournamentMatchUps().matchUps;
+    pushGlobalLog('doubleExitConsolation');
+
+    // remove the DOUBLE_WALKOVER
+    if (preRemovalChecks?.length) {
+      for (const check of preRemovalChecks) {
+        const targetMatchUp = getTarget({
+          ...check,
+          matchUps,
+        });
+        expect(targetMatchUp.drawPositions).toEqual(check.drawPositions);
+        expect(targetMatchUp.matchUpStatus).toEqual(check.matchUpStatus);
+        expect(targetMatchUp.winningSide).toEqual(check.winningSide);
+        const { roundNumber, roundPosition } = targetMatchUp;
+        const color =
+          JSON.stringify(targetMatchUp.drawPositions) !==
+          JSON.stringify(check.drawPositions)
+            ? 'brightmagenta'
+            : 'brightgreen';
+        pushGlobalLog({
+          method: 'before',
+          keyColors: {
+            stage: 'brightcyan',
+            round: 'brightcyan',
+            target: color,
+            check: 'bright',
+          },
+          stage: targetMatchUp.stage,
+          round: [roundNumber, roundPosition],
+          target: JSON.stringify(targetMatchUp.drawPositions),
+          check: JSON.stringify(check.drawPositions),
+        });
+
+        if (check.losingSideMatchUpStatusCode) {
+          const losingSideMatchUpStatusCode =
+            targetMatchUp.matchUpStatusCodes.find(
+              (side) => side.sideNumber !== targetMatchUp.winningSide
+            ).previousMatchUpStatus;
+          expect(losingSideMatchUpStatusCode).toEqual(
+            check.losingSideMatchUpStatusCode
+          );
+        }
+      }
+    }
+
+    pushGlobalLog('----------');
 
     let targetMatchUp = getTarget({
-      stage: CONSOLATION,
-      roundPosition: 1,
-      roundNumber: 1,
-      matchUps,
-    });
-
-    expect(targetMatchUp.winningSide).toEqual(winningSide);
-    const losingSideMatchUpStatusCode = targetMatchUp.matchUpStatusCodes.find(
-      (side) => side.sideNumber !== winningSide
-    ).previousMatchUpStatus;
-    expect(losingSideMatchUpStatusCode).toEqual(DOUBLE_WALKOVER);
-    expect(targetMatchUp.matchUpStatus).toEqual(WALKOVER);
-
-    targetMatchUp = getTarget({
-      stage: CONSOLATION,
-      roundPosition: 1,
-      roundNumber: 2,
-      matchUps,
-    });
-    expect(targetMatchUp.matchUpStatus).toEqual(BYE);
-    expect(targetMatchUp.drawPositions).toEqual(drawPositions);
-
-    targetMatchUp = getTarget({
       ...doubleWalkoverOutcome,
       stage: MAIN,
       matchUps,
     });
 
-    console.log('-------------------');
     result = tournamentEngine.setMatchUpStatus({
       matchUpId: targetMatchUp.matchUpId,
       drawId: targetMatchUp.drawId,
       outcome: toBePlayed,
     });
-
     expect(result.success).toEqual(true);
+
+    pushGlobalLog('----------');
 
     matchUps = tournamentEngine.allTournamentMatchUps().matchUps;
 
-    targetMatchUp = getTarget({
-      stage: CONSOLATION,
-      roundPosition: 1,
-      roundNumber: 1,
-      matchUps,
-    });
+    if (postRemovalChecks?.length) {
+      for (const check of postRemovalChecks) {
+        targetMatchUp = getTarget({
+          ...check,
+          matchUps,
+        });
+        const color =
+          JSON.stringify(targetMatchUp.drawPositions) !==
+          JSON.stringify(check.drawPositions)
+            ? 'brightmagenta'
+            : 'brightgreen';
+        const { roundNumber, roundPosition } = targetMatchUp;
+        pushGlobalLog({
+          method: 'after',
+          keyColors: {
+            stage: 'brightcyan',
+            round: 'brightcyan',
+            target: color,
+            check: 'bright',
+          },
+          stage: targetMatchUp.stage,
+          round: [roundNumber, roundPosition],
+          target: JSON.stringify(targetMatchUp.drawPositions),
+          check: JSON.stringify(check.drawPositions),
+        });
+        expect(targetMatchUp.drawPositions?.filter(Boolean)).toEqual(
+          check.drawPositions
+        );
+        expect(targetMatchUp.matchUpStatus).toEqual(check.matchUpStatus);
+        expect(targetMatchUp.winningSide).toEqual(check.winningSide);
+      }
+      printGlobalLog(true);
+    }
 
-    expect(targetMatchUp.matchUpStatus).toEqual(TO_BE_PLAYED);
-    expect(targetMatchUp.winningSide).toEqual(undefined);
-
-    targetMatchUp = getTarget({
-      stage: CONSOLATION,
-      roundPosition: 1,
-      roundNumber: 2,
-      matchUps,
-    });
-
-    console.log(targetMatchUp.drawPositions);
-    expect(targetMatchUp.drawPositions.filter(Boolean)).toEqual([1]);
-    expect(targetMatchUp.matchUpStatus).toEqual(BYE);
-    expect(targetMatchUp.winningSide).toEqual(undefined);
+    // reset
+    setDevContext();
   }
 );
