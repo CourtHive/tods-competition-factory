de0b5345c710f4d0eb43f71288451e0e4895a914
CourtHive <charles@courthive.com>
2022-11-15 09:50:07 -0500

    refactor `getEventSeedAssignment`

--- src/tournamentEngine/getters/participants/processEventEntry.js
+++ src/tournamentEngine/getters/participants/processEventEntry.js
@@ -1,23 +1,18 @@
-import { definedAttributes, intersection } from '../../../utilities';
-import { getEventSeedAssignments } from './getEventSeedAssignments';
-
-import { SCALE, SEEDING } from '../../../constants/scaleConstants';
+import { definedAttributes } from '../../../utilities';
 
 export function processEventEntry({
   extensionConversions,
-  publishedSeeding,
-  usePublishState,
-  participantMap,
-  participantId,
+  seedAssignments,
+  participant,
   withSeeding,
+  seedValue,
+  eventId,
   ranking,
   entry,
-  event,
 }) {
   const { entryStatus, entryStage, entryPosition } = entry;
-  const { eventType, eventId } = event;
 
-  participantMap[participantId].events[eventId] = definedAttributes(
+  participant.events[eventId] = definedAttributes(
     {
       ...extensionConversions, // this should be deprecated and clients should use derivedEventInfo
       entryPosition,
@@ -32,54 +27,8 @@ export function processEventEntry({
   );
 
   if (withSeeding) {
-    const getScaleAccessor = (scaleName) =>
-      [SCALE, SEEDING, eventType, scaleName].join('.');
-
-    const participant = participantMap[participantId].participant;
-    const seedingScales = Object.assign(
-      {},
-      ...(participant.timeItems || [])
-        .filter(({ itemType }) => itemType.split('.')[1] === SEEDING)
-        .map(({ itemType: seedingScaleName, itemValue: seedValue }) => ({
-          [seedingScaleName]: seedValue,
-        }))
-    );
-
-    const eventSeedingScaleNames = (
-      (publishedSeeding?.stageSeedingScaleNames &&
-        Object.values(publishedSeeding?.stageSeedingScaleNames)) ||
-      (Array.isArray(publishedSeeding?.seedingScaleNames) &&
-        publishedSeeding.seedingScaleNames) ||
-      []
-    ).map(getScaleAccessor);
-
-    const publishedEventSeedingScaleNames = intersection(
-      Object.keys(seedingScales),
-      eventSeedingScaleNames
-    );
-
-    const eventSeedingPublished = !!(
-      !usePublishState ||
-      (!Object.keys(seedingScales).length &&
-        !publishedSeeding?.drawIds?.length) ||
-      publishedEventSeedingScaleNames.length
-    );
-
-    const { seedAssignments, seedValue } = getEventSeedAssignments({
-      publishedEventSeedingScaleNames,
-      eventSeedingPublished,
-      publishedSeeding,
-      usePublishState,
-      seedingScales,
-      withSeeding,
-      participant,
-      event,
-    });
-
     if (seedAssignments)
-      participantMap[participantId].events[eventId].seedAssignments =
-        seedAssignments;
-    if (seedValue)
-      participantMap[participantId].events[eventId].seedValue = seedValue;
+      participant.events[eventId].seedAssignments = seedAssignments;
+    if (seedValue) participant.events[eventId].seedValue = seedValue;
   }
 }
