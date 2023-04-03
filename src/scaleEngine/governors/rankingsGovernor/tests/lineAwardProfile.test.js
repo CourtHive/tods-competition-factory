2c28aa46ac4f6112a0ffe9914fb9a3b9bdf8edab
CourtHive <charles@courthive.com>
2023-04-03 08:15:57 -0400

    feat: :construction: increasing coverage for `tournamentPoints`

--- src/scaleEngine/governors/rankingsGovernor/tests/lineAwardProfile.test.js
+++ src/scaleEngine/governors/rankingsGovernor/tests/lineAwardProfile.test.js
@@ -1,8 +1,11 @@
-import tournamentEngine from '../../../../tournamentEngine/sync';
+import { mocksEngine, scaleEngine, tournamentEngine } from '../../../..';
+import { awardProfileLevelLines } from './awardProfileExamples';
 import { finishingPositionSort } from './awardTestUtils';
-import mocksEngine from '../../../../mocksEngine';
+import { getAwardProfile } from '../getAwardProfile';
 import { expect, it } from 'vitest';
 
+import { POLICY_TYPE_RANKING_POINTS } from '../../../../constants/policyConstants';
+import { TEAM_PARTICIPANT } from '../../../../constants/participantConstants';
 import { TEAM_EVENT } from '../../../../constants/eventConstants';
 
 it('generates points for lines in team matchUps', () => {
@@ -21,10 +24,24 @@ it('generates points for lines in team matchUps', () => {
 
   const participants = tournamentEngine
     .getParticipants({
+      participantFilters: { participantTypes: [TEAM_PARTICIPANT] },
       withRankingProfile: true,
-      // withMatchUps: true,
     })
     .participants.sort(finishingPositionSort);
 
-  console.log({ participants });
+  const participation = participants[0].draws[0].structureParticipation;
+
+  const awardCriteria = {
+    eventType: TEAM_EVENT,
+    participation,
+  };
+
+  const awardProfiles = [awardProfileLevelLines];
+  const { awardProfile } = getAwardProfile({ awardProfiles, ...awardCriteria });
+  expect(awardProfile).not.toBeUndefined();
+
+  const policyDefinitions = { [POLICY_TYPE_RANKING_POINTS]: { awardProfiles } };
+
+  result = scaleEngine.getTournamentPoints({ policyDefinitions, level: 1 });
+  expect(result.success).toEqual(true);
 });
