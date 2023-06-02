0817858a07def16e678f87b5776138dff12e635d
CourtHive <charles@courthive.com>
2023-06-02 07:35:24 -0400

    correct SCHEDULE_WARNING constant

--- src/constants/scheduleConstants.js
+++ src/constants/scheduleConstants.js
@@ -3,7 +3,7 @@ export const SINGLES_DOUBLES = 'SINGLES_DOUBLES';
 export const TOTAL = 'total';
 
 export const SCHEDULE_CONFLICT = 'CONFLICT';
-export const SCHEDULE_WARNING = 'ERROR';
+export const SCHEDULE_WARNING = 'WARNING';
 export const SCHEDULE_ERROR = 'ERROR';
 
 export const scheduleConstants = { TOTAL, SINGLES_DOUBLES, DOUBLES_SINGLES };
