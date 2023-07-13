05543816389288d83eba29a87e59e8260fa7884d
courthive <charles@courthive.com>
2023-07-13 12:23:17 -0400

    feat: :sparkles: export `ratingsParameters` via factory assets

--- src/fixtures/index.js
+++ src/fixtures/index.js
@@ -1,9 +1,11 @@
 import { countries, countryToFlag, flagIOC } from './countryData';
+import ratingsParameters from './ratings/ratingsParameters';
 import { matchUpFormats } from './scoring/matchUpFormats';
 import { tieFormats } from './scoring/tieFormats';
 import { policies } from './policies';
 
 export const fixtures = {
+  ratingsParameters,
   matchUpFormats,
   countryToFlag,
   tieFormats,
