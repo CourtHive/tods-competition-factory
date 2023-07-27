16651e9a94e90ee8aad42e21b129867c2756f066
courthive <charles@courthive.com>
2023-07-27 10:07:28 -0400

    fix: :bug: correct point tallies when timed sets are points-based; extend tests;

--- src/matchUpEngine/getters/roundRobinTally/scoreCounters.js
+++ src/matchUpEngine/getters/roundRobinTally/scoreCounters.js
@@ -51,82 +51,125 @@ export function countGames({
 }) {
   // IMPORTANT: recognize finalSetFormat
   const { sets } = score || {};
-  const matchUpWinnerIndex = matchUpWinningSide - 1;
-  const parsedMatchUpFormat = parse(matchUpFormat);
-  const setsToWin = getSetsToWin(parsedMatchUpFormat?.bestOf || 1);
-  const gamesForSet = parsedMatchUpFormat?.setFormat?.setTo || 0;
-  const tiebreakAt = parsedMatchUpFormat?.setFormat?.tiebreakAt || 0;
   if (!sets) return [0, 0];
 
-  const minimumGameWins = setsToWin * gamesForSet;
+  const matchUpWinnerIndex = matchUpWinningSide - 1;
+  const parsedMatchUpFormat = parse(matchUpFormat);
+  const bestOf = parsedMatchUpFormat?.bestOf || 1;
+  const setsToWin = getSetsToWin(bestOf);
+  const tiebreakAt = parsedMatchUpFormat?.setFormat?.tiebreakAt || 0;
+
   const gamesTally = [[], []];
   if (
     (matchUpStatus === DEFAULTED && tallyPolicy?.gamesCreditForDefaults) ||
     (matchUpStatus === WALKOVER && tallyPolicy?.gamesCreditForWalkovers)
   ) {
+    const gamesForSet = parsedMatchUpFormat?.setFormat?.setTo || 0;
+    const minimumGameWins = setsToWin * gamesForSet;
     gamesTally[matchUpWinnerIndex].push(minimumGameWins);
   } else {
-    (sets || []).forEach((set) => {
-      const { side1Score, side2Score } = set;
-      gamesTally[0].push(parseInt(side1Score || 0));
-      gamesTally[1].push(parseInt(side2Score || 0));
+    sets.forEach((set, i) => {
+      const setNumber = set.setNumber || i + 1;
+      const whichFormat =
+        setNumber > setsToWin && parsedMatchUpFormat?.finalSetFormat
+          ? 'finalSetFormat'
+          : 'setFormat';
+      const based = parsedMatchUpFormat?.[whichFormat]?.based;
+      if (isGamesBased(based)) {
+        const { side1Score, side2Score } = set;
+        gamesTally[0].push(parseInt(side1Score || 0));
+        gamesTally[1].push(parseInt(side2Score || 0));
+      }
     });
   }
   if (matchUpStatus === RETIRED) {
-    const setsTally = countSets({
-      winningSide: matchUpWinningSide,
-      matchUpStatus,
-      matchUpFormat,
-      sets,
-    });
-    const loserLeadSet = gamesTally
-      .map((g) => g[matchUpWinnerIndex] <= g[1 - matchUpWinnerIndex])
-      .reduce((a, b) => a + b, 0);
-    // if sets where loser lead > awarded sets, adjust last game to winner
-    if (loserLeadSet > setsTally[1 - matchUpWinnerIndex]) {
-      const talliedGames = gamesTally[matchUpWinnerIndex].length;
-      const complement = getComplement(
-        gamesTally[1 - matchUpWinnerIndex][talliedGames - 1]
-      );
-      if (complement)
-        gamesTally[matchUpWinnerIndex][talliedGames - 1] = complement;
-    }
-    // if the gamesTally[x].length is less than the number of sets to win award gamesForSet to winner
-    // gamesTally[x].length is an array of games won for each set, so length is number of sets
-    if (
-      setsToWin > gamesTally[matchUpWinnerIndex].length &&
-      tallyPolicy?.gamesCreditForRetirements
-    ) {
-      gamesTally[matchUpWinnerIndex].push(gamesForSet);
+    // setFormat must consider whether retirment occurred in a finalSet which has a different format
+    const whichFormat =
+      sets.length > setsToWin && parsedMatchUpFormat?.finalSetFormat
+        ? 'finalSetFormat'
+        : 'setFormat';
+    const format = parsedMatchUpFormat?.[whichFormat];
+    if (isGamesBased(format.based)) {
+      const gamesForSet = format?.setTo || 0;
+
+      const getComplement = (value) => {
+        if (!parsedMatchUpFormat || value === '') return;
+        if (+value === tiebreakAt - 1 || +value === tiebreakAt)
+          return parseInt(tiebreakAt || 0) + 1;
+        if (+value < tiebreakAt) return gamesForSet;
+        return tiebreakAt;
+      };
+
+      const setsTally = countSets({
+        winningSide: matchUpWinningSide,
+        matchUpStatus,
+        matchUpFormat,
+        sets,
+      });
+      const loserLeadSet = gamesTally
+        .map((g) => g[matchUpWinnerIndex] <= g[1 - matchUpWinnerIndex])
+        .reduce((a, b) => a + b, 0);
+      // if sets where loser lead > awarded sets, adjust last game to winner
+      if (loserLeadSet > setsTally[1 - matchUpWinnerIndex]) {
+        const talliedGames = gamesTally[matchUpWinnerIndex].length;
+        const complement = getComplement(
+          gamesTally[1 - matchUpWinnerIndex][talliedGames - 1]
+        );
+        if (complement)
+          gamesTally[matchUpWinnerIndex][talliedGames - 1] = complement;
+      }
+      // if the gamesTally[x].length is less than the number of sets to win award gamesForSet to winner
+      // gamesTally[x].length is an array of games won for each set, so length is number of sets
+      if (
+        setsToWin > gamesTally[matchUpWinnerIndex].length &&
+        tallyPolicy?.gamesCreditForRetirements
+      ) {
+        gamesTally[matchUpWinnerIndex].push(gamesForSet);
+      }
     }
   }
-  const result = [
+
+  return [
     gamesTally[0].reduce((a, b) => a + b, 0),
     gamesTally[1].reduce((a, b) => a + b, 0),
   ];
-
-  return result;
-
-  function getComplement(value) {
-    if (!parsedMatchUpFormat || value === '') return;
-    if (+value === tiebreakAt - 1 || +value === tiebreakAt)
-      return parseInt(tiebreakAt || 0) + 1;
-    if (+value < tiebreakAt) return gamesForSet;
-    return tiebreakAt;
-  }
 }
 
-export function countPoints({ score }) {
+export function countPoints({ matchUpFormat, score }) {
+  const parsedMatchUpFormat = parse(matchUpFormat);
+  const bestOf = parsedMatchUpFormat?.bestOf || 1;
+  const setsToWin = getSetsToWin(bestOf);
   const pointsTally = [0, 0];
 
-  score?.sets?.forEach((set) => {
-    if (set.side1TiebreakScore)
-      pointsTally[0] += parseInt(set.side1TiebreakScore);
-    if (set.side2TiebreakScore)
-      pointsTally[1] += parseInt(set.side2TiebreakScore);
+  score?.sets?.forEach((set, i) => {
+    const setNumber = set.setNumber || i + 1;
+    const whichFormat =
+      setNumber > setsToWin && parsedMatchUpFormat?.finalSetFormat
+        ? 'finalSetFormat'
+        : 'setFormat';
+    const based = parsedMatchUpFormat?.[whichFormat]?.based;
+    if (isPointsBased(based)) {
+      const { side1Score, side2Score } = set;
+      if (side1Score) pointsTally[0] += parseInt(side1Score || 0);
+      if (side2Score) pointsTally[1] += parseInt(side2Score || 0);
+    } else {
+      if (set.side1TiebreakScore)
+        pointsTally[0] += parseInt(set.side1TiebreakScore || 0);
+      if (set.side2TiebreakScore)
+        pointsTally[1] += parseInt(set.side2TiebreakScore || 0);
+    }
   });
   return pointsTally;
 }
+
 function getSetsToWin(bestOfGames) {
   return (bestOfGames && Math.ceil(bestOfGames / 2)) || 1;
+}
+
+function isPointsBased(based) {
+  return based === 'P';
+}
+
+function isGamesBased(based) {
+  return !isPointsBased(based);
 }
--- src/matchUpEngine/getters/roundRobinTally/tests/timeBasedFormatTallies.test.js
+++ src/matchUpEngine/getters/roundRobinTally/tests/timeBasedFormatTallies.test.js
@@ -1,11 +1,12 @@
+import { getParticipantId } from '../../../../global/functions/extractors';
 import tournamentEngine from '../../../../tournamentEngine/sync';
 import mocksEngine from '../../../../mocksEngine';
+import matchUpEngine from '../../../sync';
 import { expect, it } from 'vitest';
 
 import { ROUND_ROBIN } from '../../../../constants/drawDefinitionConstants';
 
 it('round robins with timed formats will default to game based when no indicator', () => {
-  // TODO: investigate whether mocksEngine can generate timed results
   const { tournamentRecord } = mocksEngine.generateTournamentRecord({
     completeAllMatchUps: true,
     randomWinningSide: true,
@@ -33,8 +34,7 @@ it('round robins with timed formats will default to game based when no indicator
   expect(pointsWon + pointsLost).toEqual(0);
 });
 
-it.only('round robins with points based timed formats to tally points not games', () => {
-  // TODO: investigate whether mocksEngine can generate timed results
+it('round robins with points based timed formats to tally points not games', () => {
   const { tournamentRecord } = mocksEngine.generateTournamentRecord({
     completeAllMatchUps: true,
     randomWinningSide: true,
@@ -58,6 +58,72 @@ it.only('round robins with points based timed formats to tally points not games'
 
   const p1Result = positionAssignments[0].extensions[0].value;
   const { gamesWon, gamesLost, pointsWon, pointsLost } = p1Result;
-  expect(gamesWon + gamesLost).toBeGreaterThan(1);
-  expect(pointsWon + pointsLost).toEqual(0);
+  expect(pointsWon + pointsLost).toBeGreaterThan(1);
+  expect(gamesWon + gamesLost).toEqual(0);
+});
+
+it('can tally points when different finalSet format', () => {
+  const matchUpFormat = 'SET3-S:6/TB7-F:T20P';
+  const { tournamentRecord } = mocksEngine.generateTournamentRecord({
+    drawProfiles: [{ drawSize: 4, matchUpFormat }],
+  });
+
+  let result = tournamentEngine.setState(tournamentRecord);
+  expect(result.success).toEqual(true);
+
+  let matchUps = tournamentEngine.allTournamentMatchUps().matchUps;
+  let matchUp = matchUps[0];
+
+  expect(matchUp.matchUpFormat).toEqual(matchUpFormat);
+
+  // verify that square brackets denote points
+  let outcome = mocksEngine.generateOutcomeFromScoreString({
+    scoreString: '6-1 1-6 [23-45]',
+  }).outcome;
+  outcome.winningSide = 2;
+  let thirdSet = outcome.score.sets.find(({ setNumber }) => setNumber === 3);
+  expect(thirdSet.winningSide).toEqual(2);
+  expect(thirdSet.side1TiebreakScore).toEqual(23);
+  expect(thirdSet.side2TiebreakScore).toEqual(45);
+
+  // no square brackets denotes games...
+  // ...but in this case the matchUpFormat will ensure result is calculated as points
+  outcome = mocksEngine.generateOutcomeFromScoreString({
+    scoreString: '6-1 1-6 23-45',
+  }).outcome;
+  outcome.winningSide = 2;
+  thirdSet = outcome.score.sets.find(({ setNumber }) => setNumber === 3);
+  expect(thirdSet.winningSide).toEqual(2);
+  expect(thirdSet.side1Score).toEqual(23);
+  expect(thirdSet.side2Score).toEqual(45);
+
+  const { matchUpId, drawId } = matchUp;
+  result = tournamentEngine.setMatchUpStatus({ matchUpId, outcome, drawId });
+  expect(result.success).toEqual(true);
+
+  matchUp = tournamentEngine.findMatchUp({
+    inContext: true,
+    matchUpId,
+    drawId,
+  }).matchUp;
+
+  let { participantResults } = matchUpEngine.tallyParticipantResults({
+    matchUps: [matchUp],
+  });
+  const sideIds = matchUp.sides
+    .sort((a, b) => a.sideNumber - b.sideNumber) // ensure sides are ordered
+    .map(getParticipantId);
+  const sideTallies = sideIds.map((id) => participantResults[id]);
+
+  let { gamesWon, gamesLost, pointsWon, pointsLost } = sideTallies[0];
+  expect(pointsLost).toEqual(45);
+  expect(pointsWon).toEqual(23);
+  expect(gamesLost).toEqual(7);
+  expect(gamesWon).toEqual(7);
+
+  ({ gamesWon, gamesLost, pointsWon, pointsLost } = sideTallies[1]);
+  expect(pointsLost).toEqual(23);
+  expect(pointsWon).toEqual(45);
+  expect(gamesLost).toEqual(7);
+  expect(gamesWon).toEqual(7);
 });
