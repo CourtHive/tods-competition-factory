3da96177460f968f5d837b30850c0e0e08e39dc9
courthive <charles@courthive.com>
2023-08-03 12:01:24 -0400

    feat: :sparkles: export all flavors of `setMatchUpStatus` from forge:transform

--- src/forge/transform/index.ts
+++ src/forge/transform/index.ts
@@ -1,3 +1,7 @@
+export { setMatchUpStatus as competitionEngineSetMatchUpStatus } from '../../competitionEngine/governors/competitionsGovernor/setMatchUpStatus';
+export { setMatchUpStatus as tournamentEngineSetMatchUpStatus } from '../../tournamentEngine/governors/eventGovernor/setMatchUpStatus';
+export { setMatchUpStatus as drawEnginSetMatchUpStatus } from '../../drawEngine/governors/matchUpGovernor/setMatchUpStatus';
+
 export { modifyEventMatchUpFormatTiming } from '../../competitionEngine/governors/scheduleGovernor/matchUpFormatTiming/modifyEventMatchUpFormatTiming';
 export { bulkRescheduleMatchUps } from '../../competitionEngine/governors/scheduleGovernor/bulkRescheduleMatchUps';
 export { setMatchUpDailyLimits } from '../../competitionEngine/governors/scheduleGovernor/setMatchUpDailyLimits';
