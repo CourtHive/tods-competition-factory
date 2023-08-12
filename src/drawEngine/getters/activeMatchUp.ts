7fa9d4ad92177f6f7ed8bdfeda1bb7a1b5d27393
courthive <charles@courthive.com>
2023-08-12 15:46:22 -0400

    refactor: :recycle: typescript conversion;

--- src/drawEngine/getters/activeMatchUp.ts
+++ src/drawEngine/getters/activeMatchUp.ts
@@ -7,16 +7,26 @@ import {
   WALKOVER,
 } from '../../constants/matchUpStatusConstants';
 
+import { Score } from '../../types/tournamentFromSchema';
+
 // an active matchUp is one that has a winningSide, more than one set, or a single set with any score value greater than zero
 // when { matchUpType: TEAM } the child tieMatchUps must be checked as well
 // scoreStrings are not reliable because TEAM matchUps can have scoreString '0-0'
+
+type IsActiveMatchUpArgs = {
+  matchUpStatus?: string;
+  winningSide?: number;
+  tieMatchUps?: any[];
+  sides?: any[];
+  score: Score;
+};
 export function isActiveMatchUp({
   matchUpStatus,
   winningSide,
   tieMatchUps,
   sides,
   score,
-}) {
+}: IsActiveMatchUpArgs) {
   const participantAssigned = sides?.find(({ participantId }) => participantId);
   const activeTieMatchUps = tieMatchUps?.filter(isActiveMatchUp)?.length;
   const scoreExists = scoreHasValue({ score });
@@ -27,7 +37,8 @@ export function isActiveMatchUp({
     (winningSide && participantAssigned) || // if winningSide and no participant assigned => "produced" WALKOVER
     // must exclude IN_PROGRESS as this is automatically set by updateTieMatchUpScore
     // must exclude WALKOVER and DEFAULTED as "produced" scenarios do not imply a winningSide
-    (isActiveMatchUpStatus({ matchUpStatus }) &&
+    (matchUpStatus &&
+      isActiveMatchUpStatus({ matchUpStatus }) &&
       ![DEFAULTED, WALKOVER, IN_PROGRESS].includes(matchUpStatus))
   );
 }
