6fa952812f85d711c178390d45b6d4f7c37d22e7
courthive <charles@courthive.com>
2023-08-12 21:28:28 -0400

    refactor: :recycle: typescript conversion

--- src/drawEngine/generators/addFinishingRounds.ts
+++ src/drawEngine/generators/addFinishingRounds.ts
@@ -1,6 +1,17 @@
 import { getRoundMatchUps } from '../accessors/matchUpAccessor/getRoundMatchUps';
+import { extractAttributes, generateRange } from '../../utilities';
 import { getDevContext } from '../../global/state/globalState';
-import { generateRange } from '../../utilities';
+
+type AddFinishingRoundsArgs = {
+  finishingPositionOffset?: number;
+  finishingPositionLimit?: number;
+  positionsFed?: number;
+  roundsCount: number;
+  roundLimit?: number;
+  matchUps: any[];
+  lucky?: boolean;
+  fmlc?: boolean;
+};
 
 export function addFinishingRounds({
   finishingPositionOffset = 0,
@@ -11,15 +22,15 @@ export function addFinishingRounds({
   matchUps,
   lucky,
   fmlc,
-}) {
+}: AddFinishingRoundsArgs) {
   if (!Array.isArray(matchUps)) return [];
 
-  const { roundProfile, roundNumbers } = getRoundMatchUps({
+  const { roundProfile, roundNumbers = [] } = getRoundMatchUps({
     interpolate: true, // for structures which do not contain a final round of one matchUps (structure winner)
     matchUps,
   });
 
-  roundsCount = roundsCount || Math.max(...roundNumbers);
+  roundsCount = roundsCount || Math.max(...roundNumbers, 0);
 
   // for qualifying, offset the final round so that qualifyinground is finishingRound
   const finishingRoundOffset = roundLimit ? roundsCount - roundLimit : 0;
@@ -29,13 +40,12 @@ export function addFinishingRounds({
     finishingRoundOffset &&
     roundProfile[roundsCount - finishingRoundOffset]?.matchUpsCount;
 
-  // array of # of matchUps (value) for eaach round (index)
   const roundMatchUpsCountArray = Object.values(roundProfile).map(
-    ({ matchUpsCount }) => matchUpsCount
+    extractAttributes('matchUpsCount')
   );
 
   // returns a range for array of possible finishing drawPositions
-  const finishingRange = (positionRange, winner) => {
+  const finishingRange = (positionRange, winner?) => {
     let minFinishingPosition = Math.min(...positionRange);
 
     // only modify for qualifying when the minFinishingPosition is 1
@@ -44,7 +54,7 @@ export function addFinishingRounds({
       minFinishingPosition = minQualifyingPosition;
     }
     let maxFinishingPosition = Math.max(...positionRange);
-    if (maxFinishingPosition > finishingPositionLimit)
+    if (finishingPositionLimit && maxFinishingPosition > finishingPositionLimit)
       maxFinishingPosition = finishingPositionLimit;
     return [minFinishingPosition, maxFinishingPosition];
   };
@@ -54,16 +64,19 @@ export function addFinishingRounds({
     ...roundNumbers.map((roundNumber) => {
       const finishingRound =
         roundsCount + 1 - roundNumber - finishingRoundOffset;
-      const matchUpsCount = roundProfile[roundNumber].matchUpsCount;
-      const finishingData = { finishingRound };
+      const matchUpsCount: number = roundProfile[roundNumber].matchUpsCount;
+      const finishingData = {
+        finishingPositionRange: {},
+        finishingRound,
+      };
 
       const upcomingMatchUps = roundMatchUpsCountArray
         .slice(roundNumber - 1)
-        .reduce((a, b) => a + b, 0);
+        .reduce((a, b) => a + (b || 0), 0);
       // in the case of FMLC the finishingPositionRange in consolation is not modified after first fed round
       const fmlcException = fmlc && roundNumber !== 1;
       const rangeOffset =
-        1 + finishingPositionOffset + (fmlcException ? positionsFed : 0);
+        1 + finishingPositionOffset + (fmlcException ? positionsFed || 0 : 0);
       const finalPosition = 1;
       const positionRange = generateRange(
         rangeOffset,
