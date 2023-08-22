7eb9217aa2c87482ac94fdb2d8a44e0d533e29a0
courthive <charles@courthive.com>
2023-08-22 10:56:07 -0400

    refactor: :recycle: typescript conversion

--- src/tournamentEngine/governors/eventGovernor/drawDefinitions/updateTeamLineUp.ts
+++ src/tournamentEngine/governors/eventGovernor/drawDefinitions/updateTeamLineUp.ts
@@ -7,27 +7,32 @@ import { validateLineUp } from './validateTeamLineUp';
 import { LINEUPS } from '../../../../constants/extensionConstants';
 import { SUCCESS } from '../../../../constants/resultConstants';
 import {
+  ErrorType,
   MISSING_DRAW_DEFINITION,
   MISSING_PARTICIPANT_ID,
 } from '../../../../constants/errorConditionConstants';
+import {
+  DrawDefinition,
+  TieFormat,
+} from '../../../../types/tournamentFromSchema';
 
 // update an extension on the drawDefinition that keeps track of the latest lineUp for all team participantIds
 // each matchUp in the draw will use this as the template on first load and then write lineUp to the matchUp
 
-/**
- *
- * @param {string} drawId - drawDefinition is resolved by tournamentEngine
- * @param {string} participantId - id of the team for which lineUp is being updated
- * @param {object} tieFormat - optional - valid tieFormat definition; used to validate collectionIds
- * @param {object[]} lineUp - modified lineUp [{ participantId, collectionAssignments }]
- * @returns
- */
+type UpdateTeamLineUpArgs = {
+  drawDefinition: DrawDefinition;
+  participantId: string;
+  tieFormat: TieFormat;
+  drawId?: string;
+  lineUp: any;
+};
+
 export function updateTeamLineUp({
   drawDefinition,
   participantId,
   tieFormat,
   lineUp,
-}) {
+}: UpdateTeamLineUpArgs): { success?: boolean; error?: ErrorType } {
   if (typeof drawDefinition !== 'object')
     return { error: MISSING_DRAW_DEFINITION };
   if (typeof participantId !== 'string')
