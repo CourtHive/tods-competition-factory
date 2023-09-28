1b87036ca25ed5e3f09c223b1829c861363c7ce0
courthive <charles@courthive.com>
2023-09-28 16:24:20 -0400

    feat: :sparkles: extend use cases for `isAdHoc` (support hydrated structures with `roundMatchUps`)

--- src/drawEngine/governors/queryGovernor/isAdHoc.ts
+++ src/drawEngine/governors/queryGovernor/isAdHoc.ts
@@ -1,17 +1,19 @@
-import { DrawDefinition, Structure } from '../../../types/tournamentFromSchema';
+import { DrawDefinition } from '../../../types/tournamentFromSchema';
 import { AD_HOC } from '../../../constants/drawDefinitionConstants';
 
 type IsAdHocArgs = {
   drawDefinition?: DrawDefinition;
-  structure?: Structure;
+  structure?: any; // in this case support hydrated structures as well
 };
 export function isAdHoc({ drawDefinition, structure }: IsAdHocArgs): boolean {
   if (!structure) return false;
 
-  const hasRoundPosition = structure?.matchUps?.find(
-    (matchUp) => matchUp?.roundPosition
-  );
-  const hasDrawPosition = structure?.matchUps?.find(
+  const matchUps =
+    structure.matchUps ||
+    (structure.roundMatchUps && Object.values(structure.roundMatchUps).flat());
+
+  const hasRoundPosition = matchUps?.find((matchUp) => matchUp?.roundPosition);
+  const hasDrawPosition = matchUps?.find(
     (matchUp) => matchUp?.drawPositions?.length
   );
 
