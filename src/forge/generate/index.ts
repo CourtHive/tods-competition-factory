99c933987a9e48544e5dc401ceabd176be8c7b4d
courthive <charles@courthive.com>
2023-09-29 17:09:57 -0400

    feat: :sparkles: `generateDrawMaticRound`: export via forge and tournamentEngine

--- src/forge/generate/index.ts
+++ src/forge/generate/index.ts
@@ -1,6 +1,7 @@
 export { generateAndPopulatePlayoffStructures } from '../../drawEngine/governors/structureGovernor/generateAndPopulatePlayoffStructures';
 export { createGroupParticipant } from '../../tournamentEngine/governors/participantGovernor/groupings/createGroupParticipant';
 export { generateVoluntaryConsolation } from '../../tournamentEngine/governors/eventGovernor/generateVoluntaryConsolation';
+export { generateDrawMaticRound } from '../../drawEngine/generators/drawMatic/generateDrawMaticRound';
 export { generateDrawDefinition } from '../../tournamentEngine/generators/generateDrawDefinition';
 export { generateFlightProfile } from '../../tournamentEngine/generators/generateFlightProfile';
 export {
