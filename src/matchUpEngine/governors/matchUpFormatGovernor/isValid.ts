b049e2d13f750b72483d552fe2f23a3859866f90
courthive <charles@courthive.com>
2023-10-08 19:30:16 -0400

    refactor: :recycle: refactor regular expression: CodeQ

--- src/matchUpEngine/governors/matchUpFormatGovernor/isValid.ts
+++ src/matchUpEngine/governors/matchUpFormatGovernor/isValid.ts
@@ -5,12 +5,16 @@ export function isValid(matchUpFormat: string): boolean {
   if (typeof matchUpFormat !== 'string') return false;
   const parsedFormat = parse(matchUpFormat);
 
-  const setParts = matchUpFormat.match(/-S:(\d+)+\/TB(\d+)@?(\d*)*/);
+  const setParts = matchUpFormat.match(
+    /-S:([1-9])+\/TB([0-9]{1,2})@?([1-9]?)*/
+  );
   const setsTo = setParts?.[1];
   const tiebreakTo = setParts?.[2];
   const tiebreakAt = setParts?.[3];
 
-  const finalSetParts = matchUpFormat.match(/-F:(\d+)+\/TB(\d+)@?(\d*)*/);
+  const finalSetParts = matchUpFormat.match(
+    /-F:([1-9])+\/TB([0-9]{1,2})@?([1-9]?)*/
+  );
   const finalSetTo = finalSetParts?.[1];
   const finalSetTiebreakTo = finalSetParts?.[2];
   const finalTiebreakAt = finalSetParts?.[3];
