6f2a1f03ef9622b11a438338933cfde492d7893e
courthive <charles@courthive.com>
2023-10-24 16:52:32 -0400

    fix: :bug: `getParticipantEntries`: add default value for `extensions` to avoid error

--- src/tournamentEngine/getters/participants/getParticipantEntries.ts
+++ src/tournamentEngine/getters/participants/getParticipantEntries.ts
@@ -105,7 +105,7 @@ export function getParticipantEntries(params) {
 
     const {
       drawDefinitions = [],
-      extensions,
+      extensions = [],
       eventType,
       eventName,
       category,
