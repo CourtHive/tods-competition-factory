140bbe3aa7cf9c9c3e500cd4b91cfc4c547a0eb9
courthive <charles@courthive.com>
2023-11-20 06:38:04 -0500

    test: :white_check_mark: exted test coverage

---
+++ src/tournamentEngine/tests/reports/getParticipantStats.test.ts
@@ -0,0 +1,57 @@
+import { extractAttributes } from '../../../utilities';
+import mocksEngine from '../../../mocksEngine';
+import tournamentEngine from '../../sync';
+import { expect, it } from 'vitest';
+
+import { MISSING_MATCHUPS } from '../../../constants/errorConditionConstants';
+import { TEAM_EVENT } from '../../../constants/eventConstants';
+import { TEAM_MATCHUP } from '../../../constants/matchUpTypes';
+
+it('can return team statistics', () => {
+  const drawSize = 8;
+  const tournamentRecord = mocksEngine.generateTournamentRecord({
+    drawProfiles: [{ eventType: TEAM_EVENT, drawSize }],
+    completeAllMatchUps: true,
+  }).tournamentRecord;
+
+  tournamentEngine.setState(tournamentRecord);
+
+  let result = tournamentEngine.getParticipantStats({ matchUps: [] });
+  expect(result.error).toEqual(MISSING_MATCHUPS);
+
+  result = tournamentEngine.getParticipantStats({ withScaleValues: true });
+
+  expect(result.participatingTeamsCount).toEqual(drawSize);
+  expect(result.allParticipantStats.length).toEqual(drawSize);
+
+  const matchUps = tournamentEngine.allTournamentMatchUps({
+    matchUpFilters: { matchUpTypes: [TEAM_MATCHUP] },
+  }).matchUps;
+  const sideParticipantIds = matchUps[0].sides.map(
+    extractAttributes('participantId')
+  );
+
+  const [teamParticipantId, opponentParticipantId] = sideParticipantIds;
+  const teamParticiapntName = matchUps[0].sides[0].participant.participantName;
+
+  result = tournamentEngine.getParticipantStats({
+    withIndividualStats: true,
+    teamParticipantId,
+  });
+  expect(result.allParticipantStats[0].participantName).toEqual(
+    teamParticiapntName
+  );
+  result.allParticipantStats.forEach((stats) =>
+    expect(stats.participantName).toBeDefined()
+  );
+
+  /**
+  result = tournamentEngine.getParticipantStats({
+    withIndividualStats: true,
+    opponentParticipantId,
+    teamParticipantId,
+  });
+
+  console.log(result.allParticipantStats);
+   */
+});
