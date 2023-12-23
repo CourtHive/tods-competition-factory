27f4a7f8b01bee3df437b4855ba7654eee865df0
courthive <charles@courthive.com>
2023-12-23 06:53:35 -0500

    refactor: :truck: relocate files

---
+++ src/test/mutations/matchUps/positions/getPatternSubBlock.test.ts
@@ -0,0 +1,30 @@
+import { getSubBlock } from '../../../../assemblies/generators/drawDefinitions/generateBlockPattern';
+import { generateRange } from '../../../../utilities';
+import { expect, test } from 'vitest';
+
+const scenarios = [
+  {
+    blockPattern: [[1], [5], [3, 7], [2, 4, 6, 8]],
+    expectation: [
+      [0, [1]],
+      [1, [5]],
+      [2, [3, 7]],
+      [3, [3, 7]],
+      [4, [2, 4, 6, 8]],
+      [5, [2, 4, 6, 8]],
+      [6, [2, 4, 6, 8]],
+      [7, [2, 4, 6, 8]],
+    ],
+  },
+];
+
+test.each(scenarios)('extract sub-block from block pattern', (scenario) => {
+  const { blockPattern } = scenario;
+  const totalLength = blockPattern.flat(Infinity).length;
+  const result: any[] = [];
+  for (const index of generateRange(0, totalLength)) {
+    const block = getSubBlock({ blockPattern, index });
+    result.push([index, block]);
+  }
+  expect(scenario.expectation).toEqual(result);
+});
---
+++ src/test/mutations/matchUps/positions/positionAdvancement.test.ts
@@ -0,0 +1,330 @@
+import { generateDrawTypeAndModifyDrawDefinition } from '../../../../assemblies/generators/drawDefinitions/generateDrawTypeAndModifyDrawDefinition';
+import { positionTargets } from '../../../../mutate/matchUps/drawPositions/positionTargets';
+import { getStructureMatchUps } from '../../../../query/structure/getStructureMatchUps';
+import { setStageDrawSize } from '../../../../drawEngine/governors/entryGovernor/stageEntryCounts';
+import { getAllDrawMatchUps } from '../../../../query/drawMatchUps';
+import { getDrawStructures } from '../../../../acquire/findStructure';
+import { feedInChampionship } from '../../../../drawEngine/tests/primitives/feedIn';
+import { newDrawDefinition } from '../../../../assemblies/generators/drawDefinitions/newDrawDefinition';
+import { expect, it } from 'vitest';
+
+import { DrawDefinition } from '../../../../types/tournamentTypes';
+import { ERROR } from '../../../../constants/resultConstants';
+import {
+  MAIN,
+  FEED_IN_CHAMPIONSHIP,
+  FIRST_MATCH_LOSER_CONSOLATION,
+  COMPASS,
+} from '../../../../constants/drawDefinitionConstants';
+
+it('can direct participants in First Match Consolation (FIRST_MATCH_LOSER_CONSOLATION)', () => {
+  const drawSize = 32;
+  const drawDefinition: DrawDefinition = newDrawDefinition();
+  setStageDrawSize({ drawDefinition, stage: MAIN, drawSize });
+  const result = generateDrawTypeAndModifyDrawDefinition({
+    drawType: FIRST_MATCH_LOSER_CONSOLATION,
+    drawDefinition,
+  });
+  expect(result).not.toHaveProperty(ERROR);
+  expect(drawDefinition.links?.length).toEqual(2);
+  const {
+    structures: [structure],
+  } = getDrawStructures({ drawDefinition, stage: MAIN });
+
+  // not requiring participants, just checking round Positions
+  const { upcomingMatchUps, pendingMatchUps } = getStructureMatchUps({
+    requireParticipants: false,
+    structure,
+  });
+
+  const { matchUps: inContextDrawMatchUps } = getAllDrawMatchUps({
+    inContext: true,
+    drawDefinition,
+  });
+
+  // test first matchUp in roundNumber: 1
+  let matchUpId = upcomingMatchUps?.reduce((matchUpId: any, matchUp) => {
+    return matchUp.roundNumber === 1 && matchUp.roundPosition === 1
+      ? matchUp.matchUpId
+      : matchUpId;
+  }, undefined);
+
+  let {
+    matchUp,
+    targetMatchUps: { winnerMatchUp, loserMatchUp },
+  } = positionTargets({
+    drawDefinition,
+    inContextDrawMatchUps,
+    matchUpId,
+  });
+
+  expect(matchUp.roundNumber).toEqual(1);
+  expect(matchUp.roundPosition).toEqual(1);
+  expect(winnerMatchUp.roundNumber).toEqual(2);
+  expect(winnerMatchUp.roundPosition).toEqual(1);
+  expect(loserMatchUp.roundNumber).toEqual(1);
+  expect(loserMatchUp.roundPosition).toEqual(1);
+
+  expect(matchUp.structureId).toEqual(winnerMatchUp.structureId);
+  expect(matchUp.structureId).not.toEqual(loserMatchUp.structureId);
+
+  // test last matchUp in roundNumber: 1
+  matchUpId = upcomingMatchUps?.reduce((matchUpId: any, matchUp) => {
+    return matchUp.roundNumber === 1 && matchUp.roundPosition === drawSize / 2
+      ? matchUp.matchUpId
+      : matchUpId;
+  }, undefined);
+
+  ({
+    matchUp,
+    targetMatchUps: { winnerMatchUp, loserMatchUp },
+  } = positionTargets({
+    drawDefinition,
+    inContextDrawMatchUps,
+    matchUpId,
+  }));
+
+  expect(matchUp.roundNumber).toEqual(1);
+  expect(matchUp.roundPosition).toEqual(drawSize / 2);
+  expect(winnerMatchUp.roundNumber).toEqual(2);
+  expect(winnerMatchUp.roundPosition).toEqual(drawSize / 4);
+  expect(loserMatchUp.roundNumber).toEqual(1);
+  expect(loserMatchUp.roundPosition).toEqual(drawSize / 4);
+
+  expect(matchUp.structureId).toEqual(winnerMatchUp.structureId);
+  expect(matchUp.structureId).not.toEqual(loserMatchUp.structureId);
+
+  matchUpId = pendingMatchUps?.[0]?.matchUpId;
+  const {
+    matchUp: matchUp2ndRound,
+    targetMatchUps: {
+      winnerMatchUp: winnerMatchUp2ndRound,
+      loserMatchUp: loserMatchUp2ndRound,
+    },
+  } = positionTargets({
+    drawDefinition,
+    inContextDrawMatchUps,
+    matchUpId,
+  });
+
+  expect(matchUp2ndRound.roundNumber).toEqual(2);
+  expect(winnerMatchUp2ndRound.roundNumber).toEqual(3);
+
+  expect(matchUp2ndRound.structureId).toEqual(
+    winnerMatchUp2ndRound.structureId
+  );
+  expect(loserMatchUp2ndRound.roundNumber).toEqual(2);
+  expect(loserMatchUp2ndRound.roundPosition).toEqual(1);
+});
+
+it('can direct participants in FEED_IN_CHAMPIONSHIP structure', () => {
+  const drawSize = 16;
+  const drawDefinition: any =
+    feedInChampionship({
+      drawType: FEED_IN_CHAMPIONSHIP,
+      feedPolicy: { roundGroupedOrder: [] },
+      drawSize,
+    }).drawDefinition ?? {};
+  expect(drawDefinition?.links?.length).toEqual(4);
+  const {
+    structures: [structure],
+  } = getDrawStructures({ drawDefinition, stage: MAIN });
+
+  // not requiring participants for this round of testing
+  const { upcomingMatchUps, pendingMatchUps } = getStructureMatchUps({
+    structure,
+    requireParticipants: false,
+  });
+  expect(upcomingMatchUps?.length).toEqual(drawSize / 2);
+
+  const { matchUps: inContextDrawMatchUps } = getAllDrawMatchUps({
+    inContext: true,
+    drawDefinition,
+  });
+
+  // test first matchUp in roundNumber: 1
+  let matchUpId = upcomingMatchUps?.reduce((matchUpId: any, matchUp) => {
+    return matchUp.roundNumber === 1 && matchUp.roundPosition === 1
+      ? matchUp.matchUpId
+      : matchUpId;
+  }, undefined);
+
+  let {
+    matchUp,
+    targetMatchUps: { winnerMatchUp, loserMatchUp },
+  } = positionTargets({
+    inContextDrawMatchUps,
+    drawDefinition,
+    matchUpId,
+  });
+
+  expect(matchUp.structureId).toEqual(winnerMatchUp.structureId);
+  expect(matchUp.structureId).not.toEqual(loserMatchUp.structureId);
+
+  expect(matchUp.roundNumber).toEqual(1);
+  expect(winnerMatchUp.roundNumber).toEqual(2);
+  expect(loserMatchUp.roundNumber).toEqual(1);
+
+  expect(matchUp.roundPosition).toEqual(1);
+  expect(winnerMatchUp.roundPosition).toEqual(1);
+  expect(loserMatchUp.roundPosition).toEqual(1);
+
+  // test last matchUp in roundNumber: 1
+  matchUpId = upcomingMatchUps?.reduce((matchUpId: any, matchUp) => {
+    return matchUp.roundNumber === 1 && matchUp.roundPosition === drawSize / 2
+      ? matchUp.matchUpId
+      : matchUpId;
+  }, undefined);
+
+  ({
+    matchUp,
+    targetMatchUps: { winnerMatchUp, loserMatchUp },
+  } = positionTargets({
+    drawDefinition,
+    inContextDrawMatchUps,
+    matchUpId,
+  }));
+
+  expect(matchUp.structureId).toEqual(winnerMatchUp.structureId);
+  expect(matchUp.structureId).not.toEqual(loserMatchUp.structureId);
+
+  expect(matchUp.roundNumber).toEqual(1);
+  expect(winnerMatchUp.roundNumber).toEqual(2);
+  expect(loserMatchUp.roundNumber).toEqual(1);
+
+  expect(matchUp.roundPosition).toEqual(drawSize / 2);
+  expect(winnerMatchUp.roundPosition).toEqual(drawSize / 4);
+  expect(loserMatchUp.roundPosition).toEqual(drawSize / 4);
+
+  matchUpId = pendingMatchUps?.[0].matchUpId;
+  const {
+    matchUp: matchUp2ndRound,
+    targetMatchUps: {
+      winnerMatchUp: winnerMatchUp2ndRound,
+      loserMatchUp: loserMatchUp2ndRound,
+    },
+  } = positionTargets({
+    drawDefinition,
+    inContextDrawMatchUps,
+    matchUpId,
+  });
+
+  expect(matchUp2ndRound.roundNumber).toEqual(2);
+  expect(winnerMatchUp2ndRound.roundNumber).toEqual(3);
+  expect(loserMatchUp2ndRound.roundNumber).toEqual(2);
+
+  expect(matchUp2ndRound.roundPosition).toEqual(1);
+  expect(winnerMatchUp2ndRound.roundPosition).toEqual(1);
+  // 2nd round should be fed BOTTOM_UP
+  expect(loserMatchUp2ndRound.roundPosition).toEqual(4);
+
+  expect(matchUp2ndRound.structureId).toEqual(
+    winnerMatchUp2ndRound.structureId
+  );
+  expect(matchUp2ndRound.structureId).not.toEqual(
+    loserMatchUp2ndRound.structureId
+  );
+  expect(matchUp2ndRound.structureId).not.toEqual(
+    loserMatchUp2ndRound.structureId
+  );
+
+  expect(loserMatchUp.structureId).toEqual(loserMatchUp2ndRound.structureId);
+});
+
+it('can direct participants in COMPASS', () => {
+  const drawDefinition: DrawDefinition = newDrawDefinition() ?? {};
+  setStageDrawSize({ drawDefinition, stage: MAIN, drawSize: 32 });
+  const result = generateDrawTypeAndModifyDrawDefinition({
+    drawType: COMPASS,
+    drawDefinition,
+  });
+  expect(result).not.toHaveProperty(ERROR);
+  expect(drawDefinition.links?.length).toEqual(7);
+  const {
+    structures: [structure],
+  } = getDrawStructures({ drawDefinition, stage: MAIN });
+
+  // not requiring participants, just checking round Positions
+  const { upcomingMatchUps, pendingMatchUps } = getStructureMatchUps({
+    requireParticipants: false,
+    structure,
+  });
+
+  const { matchUps: inContextDrawMatchUps } = getAllDrawMatchUps({
+    inContext: true,
+    drawDefinition,
+  });
+
+  let matchUpId = upcomingMatchUps?.[0].matchUpId ?? ''; // keep TypeScript happy!
+  const {
+    matchUp,
+    targetLinks: { loserTargetLink },
+    targetMatchUps: { winnerMatchUp, loserMatchUp },
+  } = positionTargets({
+    inContextDrawMatchUps,
+    drawDefinition,
+    matchUpId,
+  });
+
+  const structureNameMap = Object.assign(
+    {},
+    ...(drawDefinition.structures ?? []).map(
+      ({ structureId, structureName = '' }) => ({
+        [structureName]: structureId,
+      })
+    )
+  );
+
+  expect(matchUp.roundNumber).toEqual(1);
+  expect(winnerMatchUp.roundNumber).toEqual(2);
+  expect(loserMatchUp.roundNumber).toEqual(1);
+
+  expect(matchUp.roundPosition).toEqual(1);
+  expect(winnerMatchUp.roundPosition).toEqual(1);
+  expect(loserMatchUp.roundPosition).toEqual(1);
+
+  expect(loserTargetLink.source.structureId).toEqual(structureNameMap['East']);
+  expect(loserTargetLink.target.structureId).toEqual(structureNameMap['West']);
+
+  expect(matchUp.structureId).toEqual(winnerMatchUp.structureId);
+  expect(matchUp.structureId).not.toEqual(loserMatchUp.structureId);
+
+  matchUpId = pendingMatchUps?.[0].matchUpId ?? ''; // keep TypeScript happy
+  const {
+    matchUp: matchUp2ndRound,
+    targetLinks: { loserTargetLink: round2loserTargetLink },
+    targetMatchUps: {
+      winnerMatchUp: winnerMatchUp2ndRound,
+      loserMatchUp: loserMatchUp2ndRound,
+    },
+  } = positionTargets({
+    inContextDrawMatchUps,
+    drawDefinition,
+    matchUpId,
+  });
+
+  expect(matchUp2ndRound.roundNumber).toEqual(2);
+  expect(winnerMatchUp2ndRound.roundNumber).toEqual(3);
+  expect(loserMatchUp2ndRound.roundNumber).toEqual(1);
+
+  expect(matchUp.roundPosition).toEqual(1);
+  expect(winnerMatchUp.roundPosition).toEqual(1);
+  expect(loserMatchUp.roundPosition).toEqual(1);
+
+  expect(round2loserTargetLink.source.structureId).toEqual(
+    structureNameMap['East']
+  );
+  expect(round2loserTargetLink.target.structureId).toEqual(
+    structureNameMap['North']
+  );
+
+  expect(matchUp2ndRound.structureId).toEqual(
+    winnerMatchUp2ndRound.structureId
+  );
+  expect(matchUp2ndRound.structureId).not.toEqual(
+    loserMatchUp2ndRound.structureId
+  );
+  expect(loserMatchUp.structureId).not.toEqual(
+    loserMatchUp2ndRound.structureId
+  );
+});
