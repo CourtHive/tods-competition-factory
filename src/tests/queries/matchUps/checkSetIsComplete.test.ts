e28ef177df345ac797a4c43a6ba260fb3edb247d
courthive <charles@courthive.com>
2024-01-16 09:03:48 -0500

    fix: :bug: `checkSetIsComplete`: correct check for No-Advantage sets

---
+++ src/tests/queries/matchUps/checkSetIsComplete.test.ts
@@ -0,0 +1,26 @@
+import { checkSetIsComplete } from '../../../query/matchUp/checkSetIsComplete';
+import { expect, it } from 'vitest';
+
+it('properly determines when sets are complete', () => {
+  let matchUpFormat = 'SET3-S:4/TB7-F:TB7';
+  let params: any = {
+    matchUpFormat,
+    set: {
+      side1Score: 4,
+      side2Score: 3,
+    },
+  };
+  let result = checkSetIsComplete(params);
+  expect(result).toEqual(false);
+
+  matchUpFormat = 'SET3-S:4NOAD-F:TB7';
+  params = {
+    matchUpFormat,
+    set: {
+      side1Score: 4,
+      side2Score: 3,
+    },
+  };
+  result = checkSetIsComplete(params);
+  expect(result).toEqual(true);
+});
