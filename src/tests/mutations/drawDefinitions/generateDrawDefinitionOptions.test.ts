c6d0b7ab4cc7cb4e676d88f61add0587c9f99e7a
courthive <charles@courthive.com>
2024-01-18 18:04:10 -0500

    refactor: :recycle: `generateDrawDefinition`: refactor parameter derivation and validity checking

--- src/tests/mutations/drawDefinitions/generateDrawDefinitionOptions.test.ts
+++ src/tests/mutations/drawDefinitions/generateDrawDefinitionOptions.test.ts
@@ -1,15 +1,18 @@
 import mocksEngine from '../../../assemblies/engines/mock';
 import tournamentEngine from '../../engines/syncEngine';
-import { expect, it } from 'vitest';
+import { expect, it, test } from 'vitest';
 
-import { CURTIS_CONSOLATION } from '../../../constants/drawDefinitionConstants';
+import {
+  AD_HOC,
+  CURTIS_CONSOLATION,
+  ROUND_ROBIN_WITH_PLAYOFF,
+  SINGLE_ELIMINATION,
+} from '../../../constants/drawDefinitionConstants';
 import { INVALID_DRAW_SIZE } from '../../../constants/errorConditionConstants';
 
 it('can be configured to not enforce minimum drawSize for multi-structure draws', () => {
   const result = mocksEngine.generateTournamentRecord({
-    drawProfiles: [
-      { drawSize: 2, drawType: CURTIS_CONSOLATION, drawTypeCoercion: false },
-    ],
+    drawProfiles: [{ drawSize: 2, drawType: CURTIS_CONSOLATION, drawTypeCoercion: false }],
   });
 
   expect(result.error).toEqual(INVALID_DRAW_SIZE);
@@ -35,4 +38,39 @@ it('can be configured to not enforce minimum drawSize for multi-structure draws'
   const { drawDefinition } = tournamentEngine.getEvent({ drawId });
   expect(drawDefinition.structures.length).toEqual(1);
   expect(drawDefinition.drawType).toEqual(CURTIS_CONSOLATION);
+});
+
+const scenarios = [
+  {
+    drawProfile: { drawType: AD_HOC, drawSize: 2 },
+    drawTypeCoercion: { AD_HOC: false },
+    expectation: { drawType: AD_HOC },
+  },
+  {
+    drawProfile: { drawType: AD_HOC, drawSize: 2 },
+    expectation: { drawType: SINGLE_ELIMINATION },
+    drawTypeCoercion: { AD_HOC: true },
+  },
+  {
+    drawProfile: { drawType: ROUND_ROBIN_WITH_PLAYOFF, drawSize: 4 },
+    drawTypeCoercion: { ROUND_ROBIN_WITH_PLAYOFF: false },
+    expectation: { drawType: ROUND_ROBIN_WITH_PLAYOFF },
+  },
+  {
+    drawProfile: { drawType: ROUND_ROBIN_WITH_PLAYOFF, drawSize: 4 },
+    drawTypeCoercion: { ROUND_ROBIN_WITH_PLAYOFF: 5 },
+    expectation: { drawType: SINGLE_ELIMINATION },
+  },
+];
+
+test.skip.each(scenarios)('drawTypeCoercion can be configured to coerce all drawTypes', (scenario) => {
+  const result = mocksEngine.generateTournamentRecord({
+    policyDefinitions: { POLICY_TYPE_DRAWS: { drawTypeCoercion: scenario.drawTypeCoercion } },
+    drawProfiles: [{ ...scenario.drawProfile, drawId: 'drawId' }],
+    setState: true,
+  });
+  expect(result.success).toEqual(true);
+
+  const { drawDefinition } = tournamentEngine.getEvent({ drawId: 'drawId' });
+  expect(drawDefinition.drawType).toEqual(scenario.expectation.drawType);
 });
