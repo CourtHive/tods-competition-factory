3d25506d36a8548bafb2dfc958b1eebc7c729b0d
courthive <charles@courthive.com>
2024-01-21 19:28:31 -0500

    ci: :construction_worker: tsconfig

--- src/assemblies/generators/drawDefinitions/generateDrawDefinition/generateOrGetExisting.ts
+++ src/assemblies/generators/drawDefinitions/generateDrawDefinition/generateOrGetExisting.ts
@@ -74,7 +74,6 @@ export function generateOrGetExisting(params: GenerateOrGetExisting): ResultType
   const {
     ignoreStageSpace,
     tournamentRecord,
-    policyDefinitions,
     appliedPolicies,
     qualifyingOnly,
     matchUpFormat,
@@ -96,49 +95,15 @@ export function generateOrGetExisting(params: GenerateOrGetExisting): ResultType
   const positioningReports: any[] = [];
   let conflicts: any[] = [];
 
-  const existingDrawDefinition = params.drawId
-    ? (event?.drawDefinitions?.find((d) => d.drawId === params.drawId) as DrawDefinition)
-    : undefined;
+  const setUpResult = setUp(params);
+  if (setUpResult.error) return decorateResult({ result: setUpResult, stack });
+  const existingDrawDefinition = setUpResult.existingDrawDefinition;
+  let drawDefinition: any = setUpResult.drawDefinition ?? {};
+  let structureId = setUpResult.structureId;
+  const existingQualifyingPlaceholderStructureId = setUpResult.existingQualifyingPlaceholderStructureId;
 
-  if (existingDrawDefinition && drawType !== existingDrawDefinition.drawType)
-    existingDrawDefinition.drawType = drawType as DrawTypeUnion;
-
-  let drawDefinition: any =
-    existingDrawDefinition ??
-    newDrawDefinition({
-      processCodes: params.processCodes,
-      drawId: params.drawId,
-      drawType,
-    });
-
-  const equivalenceResult = checkFormatScopeEquivalence({
-    tournamentRecord,
-    drawDefinition,
-    matchUpFormat,
-    matchUpType,
-    tieFormat,
-    event,
-  });
-  if (equivalenceResult.error) return decorateResult({ result: equivalenceResult, stack });
-
-  const attachmentResult = policyAttachment({ appliedPolicies, policyDefinitions, drawDefinition, stack });
-  if (attachmentResult.error) return attachmentResult;
-  // ---------------------------------------------------------------------------
-
-  // find existing MAIN structureId if existingDrawDefinition
-  let structureId = existingDrawDefinition?.structures?.find(
-    (structure) => structure.stage === MAIN && structure.stageSequence === 1,
-  )?.structureId;
   const entries = drawEntries ?? eventEntries ?? [];
   let drawTypeResult;
-
-  const existingQualifyingStructures = existingDrawDefinition
-    ? existingDrawDefinition.structures?.filter((structure) => structure.stage === QUALIFYING)
-    : [];
-  const existingQualifyingPlaceholderStructureId =
-    existingQualifyingStructures?.length === 1 &&
-    !existingQualifyingStructures[0].matchUps?.length &&
-    existingQualifyingStructures[0].structureId;
 
   if (existingQualifyingPlaceholderStructureId) {
     const qualifyingProfiles = params.qualifyingProfiles;
@@ -373,4 +338,67 @@ export function generateOrGetExisting(params: GenerateOrGetExisting): ResultType
     structureId,
     entries,
   };
+}
+
+function setUp(params): ResultType & {
+  existingQualifyingPlaceholderStructureId?: string | boolean;
+  existingDrawDefinition?: DrawDefinition;
+  drawDefinition?: DrawDefinition;
+  structureId?: string;
+} {
+  const {
+    tournamentRecord,
+    matchUpFormat,
+    appliedPolicies,
+    policyDefinitions,
+    matchUpType,
+    tieFormat,
+    drawType,
+    stack,
+    event,
+  } = params;
+
+  const existingDrawDefinition = params.drawId
+    ? (event?.drawDefinitions?.find((d) => d.drawId === params.drawId) as DrawDefinition)
+    : undefined;
+
+  if (existingDrawDefinition && drawType !== existingDrawDefinition.drawType)
+    existingDrawDefinition.drawType = drawType as DrawTypeUnion;
+
+  const drawDefinition: any =
+    existingDrawDefinition ??
+    newDrawDefinition({
+      processCodes: params.processCodes,
+      drawId: params.drawId,
+      drawType,
+    });
+
+  const equivalenceResult = checkFormatScopeEquivalence({
+    tournamentRecord,
+    drawDefinition,
+    matchUpFormat,
+    matchUpType,
+    tieFormat,
+    event,
+  });
+  if (equivalenceResult.error) return decorateResult({ result: equivalenceResult, stack });
+
+  const attachmentResult = policyAttachment({ appliedPolicies, policyDefinitions, drawDefinition, stack });
+  if (attachmentResult.error) return attachmentResult;
+  // ---------------------------------------------------------------------------
+
+  // find existing MAIN structureId if existingDrawDefinition
+  const structureId = existingDrawDefinition?.structures?.find(
+    (structure) => structure.stage === MAIN && structure.stageSequence === 1,
+  )?.structureId;
+
+  const existingQualifyingStructures = existingDrawDefinition
+    ? existingDrawDefinition.structures?.filter((structure) => structure.stage === QUALIFYING)
+    : [];
+  const existingQualifyingPlaceholderStructureId =
+    existingQualifyingStructures?.length === 1 &&
+    !existingQualifyingStructures[0].matchUps?.length &&
+    existingQualifyingStructures[0].structureId;
+
+  return { drawDefinition, structureId, existingDrawDefinition, existingQualifyingPlaceholderStructureId };
 }
