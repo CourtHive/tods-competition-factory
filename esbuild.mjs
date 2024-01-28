96c6131537e9a3db3968d87f7eb6f7aff35abe23
courthive <charles@courthive.com>
2024-01-28 08:56:43 -0500

    ci: :construction_worker: cleanup CI pipeline

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,9 +1,6 @@
-#!/usr/bin/env node
-// import tsPaths from 'esbuild-ts-paths';
 import * as esbuild from 'esbuild';
 
 esbuild.build({
-  // plugins: [tsPaths('./tsconfig.base.json')],
   entryPoints: ['./src/index.ts'],
   outfile: './dist/index.mjs',
   platform: 'node',
