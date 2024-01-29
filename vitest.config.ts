2ab0e2d7bd1a26b5dd3c96cd3929b36a06601363
courthive <charles@courthive.com>
2024-01-29 15:16:55 -0500

    ci: :construction_worker: config for no-threshold testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -44,6 +44,7 @@ export default defineConfig({
       '@Validators': new URL('./src/validators', import.meta.url).pathname,
       '@Constants': new URL('./src/constants', import.meta.url).pathname,
       '@Functions': new URL('./src/functions', import.meta.url).pathname,
+      '@Fixtures': new URL('./src/fixtures', import.meta.url).pathname,
       '@Acquire': new URL('./src/acquire', import.meta.url).pathname,
       '@Helpers': new URL('./src/helpers', import.meta.url).pathname,
       '@Global': new URL('./src/global', import.meta.url).pathname,
