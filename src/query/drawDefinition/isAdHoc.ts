c8376d977b0414bb452aa740641663104ed245fd
courthive <charles@courthive.com>
2024-01-29 21:03:48 -0500

    refactor: :recycle: refactor isAdhoc logic

--- src/query/drawDefinition/isAdHoc.ts
+++ src/query/drawDefinition/isAdHoc.ts
@@ -1,23 +1,21 @@
-import { DrawDefinition } from '../../types/tournamentTypes';
-import { AD_HOC, VOLUNTARY_CONSOLATION } from '../../constants/drawDefinitionConstants';
+import { VOLUNTARY_CONSOLATION } from '@Constants/drawDefinitionConstants';
 
 type IsAdHocArgs = {
-  drawDefinition?: DrawDefinition;
   structure?: any; // in this case support hydrated structures as well
 };
-export function isAdHoc({ drawDefinition, structure }: IsAdHocArgs): boolean {
+export function isAdHoc({ structure }: IsAdHocArgs): boolean {
   if (!structure) return false;
 
   const matchUps = structure.matchUps || (structure.roundMatchUps && Object.values(structure.roundMatchUps).flat());
 
-  const hasRoundPosition = matchUps?.find((matchUp) => matchUp?.roundPosition);
-  const hasDrawPosition = matchUps?.find((matchUp) => matchUp?.drawPositions?.length);
+  const hasRoundPosition = !!matchUps?.find((matchUp) => matchUp?.roundPosition);
+  const hasDrawPosition = !!matchUps?.find((matchUp) => matchUp?.drawPositions?.length);
 
-  return (
+  const adHoc =
     !structure?.structures &&
     structure?.stage !== VOLUNTARY_CONSOLATION &&
-    !(drawDefinition?.drawType && drawDefinition.drawType !== AD_HOC) &&
-    !hasRoundPosition &&
-    !hasDrawPosition
-  );
+    // !(drawDefinition?.drawType && drawDefinition.drawType !== AD_HOC) &&
+    (!matchUps.length || (!hasRoundPosition && !hasDrawPosition));
+
+  return adHoc;
 }
