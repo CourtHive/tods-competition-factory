d760bb330a4d60abab0fe78b92a8d0c63c37f86a
courthive <charles@courthive.com>
2024-02-07 14:35:07 -0500

    fix: :recycle: update example `mutationEngine` to use `createInstanceState`

--- src/server/providers/factory/engines/mutationEngine.ts
+++ src/server/providers/factory/engines/mutationEngine.ts
@@ -1,26 +1,14 @@
-import * as governors from '@Assemblies/governors';
 import { asyncEngine, globalState } from '../../../..';
+import * as governors from '@Assemblies/governors';
 import asyncGlobalState from './asyncGlobalState';
 
 globalState.setStateProvider(asyncGlobalState);
-const engineAsync = asyncEngine(true);
+asyncGlobalState.createInstanceState();
 
-const methods = {
-  ...governors.competitionGovernor,
-  ...governors.participantGovernor,
-  ...governors.generationGovernor,
-  ...governors.publishingGovernor,
-  ...governors.tournamentGovernor,
-  ...governors.scheduleGovernor,
-  ...governors.policyGovernor,
-  ...governors.reportGovernor,
-  ...governors.mocksGovernor,
-  ...governors.eventGovernor,
-  ...governors.scoreGovernor,
-  ...governors.venueGovernor,
-};
+export function getMutationEngine() {
+  const engineAsync = asyncEngine();
+  engineAsync.importMethods(governors, true, 1);
+  globalState.setSubscriptions({ subscriptions: {} });
 
-engineAsync.importMethods(methods);
-
-export const mutationEngine = engineAsync;
-export default engineAsync;
+  return engineAsync;
+}
