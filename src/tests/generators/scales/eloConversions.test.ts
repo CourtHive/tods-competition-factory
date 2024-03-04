c336757c38b9bbf133d122dbb2a06e36ba4a6d24
courthive <charles@courthive.com>
2024-03-04 14:20:58 -0500

    test: :white_check_mark: extend test coverage

--- src/tests/generators/scales/eloConversions.test.ts
+++ src/tests/generators/scales/eloConversions.test.ts
@@ -4,7 +4,7 @@ import { expect, it } from 'vitest';
 // constants
 import { UTR, WTN } from '@Constants/ratingConstants';
 
-const scenarios = [
+let scenarios: any[] = [
   { ratingType: WTN, ratingValue: 10 },
   { ratingType: UTR, ratingValue: 10 },
 ];
@@ -19,4 +19,37 @@ it.each(scenarios)('can reliably convert to and from elo', (scenario) => {
     sourceRating: convertedRating,
   });
   expect(originalRating).toEqual(scenario.ratingValue);
+});
+
+scenarios = [
+  { ratingType: WTN, highValue: 16.0, lowValue: 10.0, inverted: true },
+  { ratingType: UTR, highValue: 16.0, lowValue: 10.0 },
+];
+
+it.each(scenarios)('propertly handles inverted scales when converting to ELO', (scenario) => {
+  const convertedHighRating = getRatingConvertedToELO({
+    sourceRatingType: scenario.ratingType,
+    sourceRating: scenario.highValue,
+  });
+  const originalRating = getRatingConvertedFromELO({
+    targetRatingType: scenario.ratingType,
+    sourceRating: convertedHighRating,
+  });
+  expect(originalRating).toEqual(scenario.highValue);
+
+  const convertedLowRating = getRatingConvertedToELO({
+    sourceRatingType: scenario.ratingType,
+    sourceRating: scenario.lowValue,
+  });
+  const originalRating2 = getRatingConvertedFromELO({
+    targetRatingType: scenario.ratingType,
+    sourceRating: convertedLowRating,
+  });
+  expect(originalRating2).toEqual(scenario.lowValue);
+
+  if (scenario.inverted) {
+    expect(convertedHighRating).toBeLessThan(convertedLowRating);
+  } else {
+    expect(convertedHighRating).toBeGreaterThan(convertedLowRating);
+  }
 });
