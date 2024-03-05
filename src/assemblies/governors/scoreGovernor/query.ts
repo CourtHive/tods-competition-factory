8bf5b5bd76e7293e680ad4b5514a7911adec9662
courthive <charles@courthive.com>
2024-03-05 17:47:21 -0500

    feat: :sparkles: export `analyzeScore` from `scoreGovernor`

--- src/assemblies/governors/scoreGovernor/query.ts
+++ src/assemblies/governors/scoreGovernor/query.ts
@@ -5,5 +5,6 @@ export { validateScore } from '@Validators/validateScore';
 export { getSetComplement, getTiebreakComplement } from '@Query/matchUp/getComplement';
 export { checkScoreHasValue } from '@Query/matchUp/checkScoreHasValue';
 export { checkSetIsComplete } from '@Query/matchUp/checkSetIsComplete';
+export { analyzeScore } from '@Query/matchUp/analyzeScore';
 export { parseScoreString } from '@Tools/parseScoreString';
 export { analyzeSet } from '@Query/matchUp/analyzeSet';
