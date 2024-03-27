6a016cb32607c3fb885b7392a2cdaf60c302087c
courthive <charles@courthive.com>
2024-03-27 08:35:04 -0400

    feat: :sparkles: export `publishParticipants`, `unPublishParticipants`

--- src/assemblies/governors/publishingGovernor/mutate.ts
+++ src/assemblies/governors/publishingGovernor/mutate.ts
@@ -1,6 +1,8 @@
 export { publishEventSeeding, unPublishEventSeeding } from '@Mutate/publishing/eventSeeding';
+export { unPublishParticipants } from '@Mutate/timeItems/unPublishParticipants';
 export { unPublishOrderOfPlay } from '@Mutate/timeItems/unPublishOrderOfPlay';
+export { publishParticipants } from '@Mutate/timeItems/publishParticipants';
 export { publishOrderOfPlay } from '@Mutate/timeItems/publishOrderOfPlay';
-export { setEventDisplay } from '@Mutate/events/setEventDisplay';
 export { unPublishEvent } from '@Mutate/publishing/unPublishEvent';
+export { setEventDisplay } from '@Mutate/events/setEventDisplay';
 export { publishEvent } from '@Mutate/publishing/publishEvent';
