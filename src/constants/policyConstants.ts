f22ec0e8f617745623e51e8836f538cfb3c6612a
courthive <charles@courthive.com>
2024-03-27 12:37:16 -0400

    docs: :memo: dev notes

--- src/constants/policyConstants.ts
+++ src/constants/policyConstants.ts
@@ -9,6 +9,7 @@ export const POLICY_TYPE_PARTICIPANT = 'participant';
 export const POLICY_TYPE_PROGRESSION = 'progression';
 export const POLICY_TYPE_SCHEDULING = 'scheduling';
 export const POLICY_TYPE_AVOIDANCE = 'avoidance';
+export const POLICY_TYPE_PRIVACY = 'participant'; // TODO: this should be 'privacy'
 export const POLICY_TYPE_DISPLAY = 'display'; // storage for client type displays, e.g. { public: {}, admin: {} }
 export const POLICY_TYPE_SCORING = 'scoring';
 export const POLICY_TYPE_SEEDING = 'seeding';
@@ -29,6 +30,7 @@ export type ValidPolicyTypes =
   | typeof POLICY_TYPE_SCHEDULING
   | typeof POLICY_TYPE_AVOIDANCE
   | typeof POLICY_TYPE_DISPLAY
+  | typeof POLICY_TYPE_PRIVACY
   | typeof POLICY_TYPE_FEED_IN
   | typeof POLICY_TYPE_SCORING
   | typeof POLICY_TYPE_SEEDING
@@ -48,6 +50,7 @@ export const policyConstants = {
   POLICY_TYPE_SCHEDULING,
   POLICY_TYPE_AVOIDANCE,
   POLICY_TYPE_DISPLAY,
+  POLICY_TYPE_PRIVACY,
   POLICY_TYPE_FEED_IN,
   POLICY_TYPE_SCORING,
   POLICY_TYPE_SEEDING,
