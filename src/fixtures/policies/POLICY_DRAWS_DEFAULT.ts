8586f9d3136ac26ba0ca2bb53544c3aabe4a067f
courthive <charles@courthive.com>
2024-04-16 07:08:37 -0400

    docs: :memo: update inline documentation for POLICY_TYPE_DRAWS

--- src/fixtures/policies/POLICY_DRAWS_DEFAULT.ts
+++ src/fixtures/policies/POLICY_DRAWS_DEFAULT.ts
@@ -3,7 +3,7 @@ import { POLICY_TYPE_DRAWS } from '@Constants/policyConstants';
 export const POLICY_DRAWS_DEFAULT = {
   [POLICY_TYPE_DRAWS]: {
     // drawTypeCoercion: { ROUND_ROBIN_WITH_PLAYOFF: 5 }, // example of how to set a minimum # of participants for a specific drawType
-    // drawTypeCoercion: true, // example of how to set a default for all drawTypes
+    // drawTypeCoercion: false, // example of how to set a default for all drawTypes; without a policy defaults to true
   },
 };
 
