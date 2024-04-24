220847d0a6ada6b4909e4a0f8f3568b30eaa604f
courthive <charles@courthive.com>
2024-04-24 08:23:58 -0400

    feat: :sparkles: `getAggregateTeamResults`: calculate team individual results as well

--- src/query/scales/getAggregateTeamResults.ts
+++ src/query/scales/getAggregateTeamResults.ts
@@ -3,6 +3,7 @@ import { getParticipants } from '@Query/participants/getParticipants';
 import { parse } from '@Helpers/matchUpFormatCode/parse';
 
 // Constants
+import { INDIVIDUAL, TEAM_PARTICIPANT } from '@Constants/participantConstants';
 import { TOURNAMENT_RECORD } from '@Constants/attributeConstants';
 import { SUCCESS } from '@Constants/resultConstants';
 import { Tournament } from '@Types/tournamentTypes';
@@ -28,12 +29,18 @@ export function getAggregateTeamResults(params: GetAggregateTeamResultsArgs) {
   };
 
   const { matchUps, participants } = getParticipants({
+    participantFilters: { participantTypes: [INDIVIDUAL] },
     withRankingProfile: true,
     withStatistics: true,
     ...params,
   });
 
+  // use finishingPositionRange for each draw to determine bonus points
   for (const participant of participants ?? []) {
+    if (participant.participantType === TEAM_PARTICIPANT) continue;
+    if (participant.draws?.[0]?.drawId === 'draw-0-3') {
+      console.log(participant.participantType, participant.draws[0].finishingPositionRange);
+    }
     const teamParticipant = getTeamParticipant(participant);
     if (!teamParticipant) continue;
     for (const draw of participant?.draws ?? []) {
@@ -46,6 +53,25 @@ export function getAggregateTeamResults(params: GetAggregateTeamResultsArgs) {
       }
     }
   }
+
+  const initializeResults = (teamName) => ({
+    teamName,
+    standingPoints: 0,
+    pointsPlayed: 0,
+    pointsPct: 0,
+    points: 0,
+    bonus: 0,
+    diff: 0,
+    loss: 0,
+    win: 0,
+  });
+
+  const increment = (results, key, value) => {
+    if (results[key] === undefined) results[key] = 0;
+    results[key] += value;
+  };
+
+  const individualResults = {};
   const teamResults = {};
 
   for (const matchUp of matchUps ?? []) {
@@ -61,29 +87,31 @@ export function getAggregateTeamResults(params: GetAggregateTeamResultsArgs) {
       for (const side of sides) {
         const teamParticipant = getTeamParticipant(side.participant);
         const teamParticipantId = teamParticipant?.participantId;
+        const participantId = side.participant?.participantId;
+        const teamName = teamParticipant?.participantName;
         const sideNumber = side.sideNumber;
+        const individualParticipantIds =
+          side.participant?.individualParticipantIds || (participantId && [participantId]) || [];
+
+        individualParticipantIds.forEach((individualParticipantId) => {
+          if (individualParticipantId && !individualResults[individualParticipantId]) {
+            individualResults[individualParticipantId] = initializeResults(teamName);
+          }
+        });
 
         if (teamParticipantId && sideNumber) {
-          if (!teamResults[teamParticipantId]) {
-            const teamName = teamParticipant.participantName;
-            teamResults[teamParticipantId] = {
-              standingPoints: 0,
-              pointsPlayed: 0,
-              pointsPct: 0,
-              points: 0,
-              teamName,
-              bonus: 0,
-              diff: 0,
-              loss: 0,
-              win: 0,
-            };
-          }
+          if (!teamResults[teamParticipantId]) teamResults[teamParticipantId] = initializeResults(teamName);
+
+          const resultObjects = [
+            ...individualParticipantIds.map((id) => individualResults[id]),
+            teamResults[teamParticipantId],
+          ];
 
           if (matchUp.winningSide) {
             if (sideNumber === matchUp.winningSide) {
-              teamResults[teamParticipantId].win += 1;
+              resultObjects.forEach((resultObject) => increment(resultObject, 'win', 1));
             } else {
-              teamResults[teamParticipantId].loss += 1;
+              resultObjects.forEach((resultObject) => increment(resultObject, 'loss', 1));
             }
           }
 
@@ -93,9 +121,9 @@ export function getAggregateTeamResults(params: GetAggregateTeamResultsArgs) {
             const points = set?.[`side${sideNumber}Score`] || 0;
             const diff = points - opponentPoints;
 
-            teamResults[teamParticipantId].pointsPlayed += points + opponentPoints;
-            teamResults[teamParticipantId].points += points;
-            teamResults[teamParticipantId].diff += diff;
+            resultObjects.forEach((resultObject) => increment(resultObject, 'pointsPlayed', points + opponentPoints));
+            resultObjects.forEach((resultObject) => increment(resultObject, 'points', points));
+            resultObjects.forEach((resultObject) => increment(resultObject, 'diff', diff));
           }
         }
       }
@@ -114,5 +142,13 @@ export function getAggregateTeamResults(params: GetAggregateTeamResultsArgs) {
     }
   }
 
-  return { ...SUCCESS, teamResults };
+  for (const individualParticipantId of Object.keys(individualResults)) {
+    individualResults[individualParticipantId].pointsPct = parseFloat(
+      (
+        individualResults[individualParticipantId].points / individualResults[individualParticipantId].pointsPlayed
+      ).toFixed(2),
+    );
+  }
+
+  return { ...SUCCESS, individualResults, teamResults };
 }
