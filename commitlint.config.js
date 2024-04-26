1b60fa12b1b02ae78f38afe251a8c3d242395cee
courthive <charles@courthive.com>
2024-04-26 08:54:17 -0400

    refactor: :construction_worker: attempt to fix commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  prompt: { useEmoji: true },
-};
+export default { extends: ['@commitlint/config-conventional'], useEmoji: true };
