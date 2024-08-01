8c5a946120307f488e07ca0d5dbba4eb02c4a317
tmharber-cs <tom.harber@clubspark.com>
2024-08-01 14:10:15 +0100

    fix: export hydrated type declarations and set file for types

--- src/types/hydrated.ts
+++ src/types/hydrated.ts
@@ -8,13 +8,6 @@ export type HydratedVenue = {
   [key: string]: any;
 } & Venue;
 
-/*
-export type HydratedMatchUp = MatchUp & {
-  [key: string | number]: any;
-  sides?: HydratedSide[];
-};
-*/
-
 export interface HydratedMatchUp extends MatchUp {
   [key: string | number]: any;
   sides?: HydratedSide[];
