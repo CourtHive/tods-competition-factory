ed798179299f7c7dde0ec24317e6543f61317648
courthive <charles@courthive.com>
2025-10-12 16:44:47 -0400

    test: :test_tube: Check for propagated exit status on first connected structure

---
+++ src/tests/mutations/drawDefinitions/setMatchUpStatus/propagateExitStatus.test.ts
@@ -0,0 +1,67 @@
+import competitionEngine from '@Tests/engines/syncEngine';
+import tournamentEngine from '@Tests/engines/syncEngine';
+import mocksEngine from '@Assemblies/engines/mock';
+import { expect, test } from 'vitest';
+
+// constants
+import { COMPASS, FIRST_MATCH_LOSER_CONSOLATION } from '@Constants/drawDefinitionConstants';
+import { WALKOVER } from '@Constants/matchUpStatusConstants';
+
+const factory = {
+  competitionEngine,
+  tournamentEngine,
+};
+
+test('can propagate an exit status', () => {
+  const idPrefix = 'matchUp';
+  const drawId = 'drawId';
+  mocksEngine.generateTournamentRecord({
+    drawProfiles: [{ drawId, drawSize: 32, drawType: FIRST_MATCH_LOSER_CONSOLATION, idPrefix }],
+    setState: true,
+  });
+
+  let matchUpId = 'matchUp-1-1';
+  let result = tournamentEngine.setMatchUpStatus({
+    outcome: { matchUpStatus: WALKOVER, winningSide: 2 },
+    propagateExitStatus: true,
+    matchUpId,
+    drawId,
+  });
+  expect(result.success).toEqual(true);
+
+  const matchUps = factory.tournamentEngine.allDrawMatchUps({ drawId }).matchUps;
+  const matchUp = matchUps?.find((matchUp) => matchUp.matchUpId === matchUpId);
+  expect(matchUp?.matchUpStatus).toEqual('WALKOVER');
+  const loserMatchUp = matchUps?.find((mU) => mU.matchUpId === matchUp?.loserMatchUpId);
+  expect(loserMatchUp?.matchUpStatus).toEqual('WALKOVER');
+});
+
+test('can propagate an exit status', () => {
+  const idPrefix = 'matchUp';
+  const drawId = 'drawId';
+  mocksEngine.generateTournamentRecord({
+    drawProfiles: [{ drawId, drawSize: 32, drawType: COMPASS, idPrefix }],
+    setState: true,
+  });
+
+  let matchUpId = 'matchUp-East-RP-1-1';
+  let result = tournamentEngine.setMatchUpStatus({
+    outcome: { matchUpStatus: WALKOVER, winningSide: 2 },
+    propagateExitStatus: true,
+    matchUpId,
+    drawId,
+  });
+  expect(result.success).toEqual(true);
+
+  const matchUps = factory.tournamentEngine.allDrawMatchUps({ drawId }).matchUps;
+  const matchUp = matchUps?.find((matchUp) => matchUp.matchUpId === matchUpId);
+  expect(matchUp?.matchUpStatus).toEqual('WALKOVER');
+  const westLoserMatchUp = matchUps?.find((mU) => mU.matchUpId === matchUp?.loserMatchUpId);
+  expect(westLoserMatchUp?.matchUpStatus).toEqual('WALKOVER');
+
+  /*
+  // NOTE: not yet propagating beyond first connected structure; likely propgateExitStatus not propagating in this scenario
+  const southLoserMatchUp = matchUps?.find((mU) => mU.matchUpId === westLoserMatchUp?.loserMatchUpId);
+  expect(southLoserMatchUp?.matchUpStatus).toEqual('WALKOVER');
+  */
+});
