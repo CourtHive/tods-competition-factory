87d14724b1b19f8123d366f99718f0470c186a2f
Simone Vicentini <simone.vicentini@clubspark.com>
2025-11-07 12:12:28 +0000

    feat: stop clearing status of match if downstream matches have propagated status

--- src/query/drawDefinition/isActiveDownstream.ts
+++ src/query/drawDefinition/isActiveDownstream.ts
@@ -1,12 +1,13 @@
 import { positionTargets } from '@Query/matchUp/positionTargets';
 
 // constants
-import { BYE, DEFAULTED, WALKOVER } from '@Constants/matchUpStatusConstants';
+import { BYE, DEFAULTED, DOUBLE_WALKOVER, TO_BE_PLAYED, WALKOVER } from '@Constants/matchUpStatusConstants';
 import { FIRST_MATCHUP } from '@Constants/drawDefinitionConstants';
+import { WITHDRAWN } from '../../constants/entryStatusConstants';
 
 export function isActiveDownstream(params) {
   // relevantLink is passed in iterative calls (see below)
-  const { inContextDrawMatchUps, targetData, drawDefinition, relevantLink } = params;
+  const { inContextDrawMatchUps, targetData, drawDefinition, relevantLink, matchUpStatus, score, winningSide, matchUpsMap } = params;
 
   const fmlcBYE = relevantLink?.linkCondition === FIRST_MATCHUP && targetData?.matchUp?.matchUpStatus === BYE;
   if (fmlcBYE) return false;
@@ -26,11 +27,29 @@ export function isActiveDownstream(params) {
 
   // NOTE: produced WALKOVER, DEFAULTEED fed into consolation structures should NOT be considered active
   // IF: the loserMatchUp has no further downstream matchUps or there is no propagated loserParticipant (e.g. DOUBLE_EXIT)
-
   const loserExitPropagation = loserTargetData?.targetMatchUps?.loserMatchUp;
   const loserIndex = loserTargetData?.targetMatchUps?.loserMatchUpDrawPositionIndex;
   const propagatedLoserParticipant = loserExitPropagation?.sides[loserIndex]?.participant;
   const loserMatchUpExit = [DEFAULTED, WALKOVER].includes(loserMatchUp?.matchUpStatus) && !propagatedLoserParticipant;
+  
+
+  
+  const isLoserMatchUpWO = [DEFAULTED, WALKOVER].includes(loserMatchUp?.matchUpStatus);
+  const hasLoserMatchUpUpstreamWOMatches = !!matchUpsMap?.drawMatchUps.find(
+    (m) => m.loserMatchUpId === loserMatchUp?.matchUpId && [DEFAULTED, WALKOVER, WITHDRAWN].includes(m.matchUpStatus),
+  ); 
+  //to identify a propagated exit (WO/DEFAULT) for matches that are WO/DEFAULT, have a winning side,
+  //and have only one participant (the WO/DF player).
+  const loserMatchUpParticipantsCount =
+    loserMatchUp?.sides?.reduce((acc, current) => (current?.participant ? ++acc : acc), 0) ?? 0;
+  const isLoserMatchUpWalkoverWithOnePlayer =
+    //this catches downstream matches marked as WO with only one participant
+    loserMatchUp?.winningSide && isLoserMatchUpWO && loserMatchUpParticipantsCount === 1;
+
+  //we want to figure out if the command is for clearing the score as this
+  //can have downstream effects.
+  const isClearScore =
+    matchUpStatus === TO_BE_PLAYED && score?.scoreStringSide1 === '' && score?.scoreStringSide2 === '' && !winningSide;
 
   const winnerDrawPositionsCount = winnerMatchUp?.drawPositions?.filter(Boolean).length || 0;
 
@@ -38,7 +57,17 @@ export function isActiveDownstream(params) {
   // unless one of its downstream matchUps is active
 
   if (
-    (loserMatchUp?.winningSide && !loserMatchUpExit) ||
+    //if there is a downstream match with two propagated exits we mark it as active
+    (hasLoserMatchUpUpstreamWOMatches && loserMatchUp?.matchUpStatus === DOUBLE_WALKOVER && isClearScore) ||
+    //if there is a downstream propagated exit and we are trying to clear the score we stop the user
+    //by marking the downstream as active
+    (hasLoserMatchUpUpstreamWOMatches && isLoserMatchUpWO && isClearScore) ||
+    //if the downstream has not a propagated exit we run the existing checks,
+    //otherwise we let them set the score. This is to counter act the fact
+    //that propagated exit status matches can be marked as WALKOVER with only one
+    //participant. This would have been marked as an active downstream before.
+    (!isLoserMatchUpWalkoverWithOnePlayer &&
+      ((loserMatchUp?.winningSide && !loserMatchUpExit) ||
     (winnerMatchUp?.winningSide &&
       winnerDrawPositionsCount === 2 &&
       (!winnerMatchUp.feedRound || ![WALKOVER, DEFAULTED].includes(winnerMatchUp?.matchUpStatus)))
