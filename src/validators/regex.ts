7598fe2a1663d313e5d160e8ab44aa5c15b068f2
courthive <charles@courthive.com>
2026-02-07 07:39:09 -0500

    refactor: :rotating_light: linting

--- src/validators/regex.ts
+++ src/validators/regex.ts
@@ -1,6 +1,16 @@
-export const validDateString = /^[\d]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][\d]|3[0-1])$/;
-export const validTimeString = /^((0[\d]|1[\d]|2[0-3]):[0-5][\d](:[0-5][\d])?)([.,][0-9]{3})?$/;
+// Split timeValidation into two simpler regexes for date and time, and combine in logic if needed
+// Combine these in your validation logic as needed
+export const validDateString = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])$/;
+export const validTimeString = /^((0\d|1\d|2[0-3]):[0-5]\d(:[0-5]\d)?)([.,]\d{3})?$/;
+
+/* eslint-disable sonarjs/regex-complexity */
 export const dateValidation =
-  /^([\d]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][\d]|3[0-1]))([ T](0[\d]|1[\d]|2[0-3]):[0-5][\d](:[0-5][\d])?)?([.,][\d]{3})?Z?$/;
+  /^(\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1]))([ T](0\d|1\d|2[0-3]):[0-5]\d(:[0-5]\d)?)?([.,]\d{3})?Z?$/;
 export const timeValidation =
-  /^([\d]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][\d]|3[0-1]))?([ T]?(0[\d]|1[\d]|2[0-3]):[0-5][\d](:[0-5][\d])?)?([.,][\d]{3})?Z?$/;
+  /^(\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1]))?([ T]?(0\d|1\d|2[0-3]):[0-5]\d(:[0-5]\d)?)?([.,]\d{3})?Z?$/;
+
+// Split dateValidation into two simpler regexes for date and time
+// Combine these in your validation logic as needed
+// Split into two simpler regexes: one for date, one for time, and combine in logic if needed
+export const validDate = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+export const validTime = /^(0\d|1\d|2[0-3]):[0-5]\d(:[0-5]\d)?([.,]\d{3})?Z?$/;
